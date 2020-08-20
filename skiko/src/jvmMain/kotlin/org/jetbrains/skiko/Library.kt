38370178699488dbc12f07d561fb19360907ce02
Nikolay Igotti <olonho@users.noreply.github.com>
2020-08-20 09:20:20 +0300

    Unpack native lib only once. (#6)

--- skiko/src/jvmMain/kotlin/org/jetbrains/skiko/Library.kt
+++ skiko/src/jvmMain/kotlin/org/jetbrains/skiko/Library.kt
@@ -1,45 +1,45 @@
 package org.jetbrains.skiko
 
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import java.io.File
 import java.nio.file.Files
 import java.nio.file.StandardCopyOption
 
-// based on https://github.com/JetBrains/skija/blob/bc6e0531021e4ff839c07196e0336d6456ed7520/src/main/java/org/jetbrains/skija/Library.java
 object Library {
     private var loaded = false
 
-    // https://github.com/adamheinrich/native-utils/blob/e6a39489662846a77504634b6fafa4995ede3b1d/src/main/java/cz/adamh/utils/NativeUtils.java
+    private val cacheDir = "${System.getProperty("user.home")}/.skiko/"
+
+    private fun loadOrGet(path: String, resource: String, isLibrary: Boolean) {
+        File(cacheDir).mkdirs()
+        val resourceName = if (isLibrary) System.mapLibraryName(resource) else resource
+        val hash = Library::class.java.getResourceAsStream("$path$resourceName.sha256").use {
+            BufferedReader(InputStreamReader(it)).lines().toArray()[0] as String
+        }
+
+        val fileName = if (isLibrary) System.mapLibraryName(hash) else hash
+        val file = File(cacheDir, fileName)
+        // TODO: small race change when multiple Compose apps are started first time, can handle with atomic rename.
+        if (!file.exists()) {
+            Library::class.java.getResourceAsStream("$path$resourceName").use { input ->
+                Files.copy(input, file.toPath(), StandardCopyOption.REPLACE_EXISTING)
+            }
+        }
+        if (isLibrary) {
+            System.load(file.absolutePath)
+        }
+    }
+
     @Synchronized
     fun load(resourcePath: String, name: String) {
         if (loaded) return
 
-        val libFileName = System.mapLibraryName(name)
-        val url = Library::class.java.getResource(resourcePath + libFileName)
-
-        val tempRoot = File(System.getProperty("java.io.tmpdir"))
-        val tempDir = tempRoot.resolve("skiko_" + System.nanoTime()).apply { mkdirs() }
-        val libFile = when {
-            url == null -> error("Library $libFileName is not found in $resourcePath")
-            url.protocol == "file" -> File(url.toURI())
-            else -> url.openStream().use { input ->
-                File(tempDir, libFileName).also {
-                    it.deleteOnExit()
-                    Files.copy(input, it.toPath(), StandardCopyOption.REPLACE_EXISTING)
-                }
-            }
-        }
+        loadOrGet(resourcePath, name, true)
         val loadIcu = System.getProperty("os.name").toLowerCase().startsWith("win")
-        val icuData = "icudtl.dat"
         if (loadIcu) {
-            val icuUrl = Library::class.java.getResource(resourcePath + icuData)
-            icuUrl.openStream().use { input ->
-                File(tempDir, icuData).also {
-                    it.deleteOnExit()
-                    Files.copy(input, it.toPath(), StandardCopyOption.REPLACE_EXISTING)
-                }
-            }
+            loadOrGet(resourcePath, "icudtl.dat", false)
         }
-        System.load(libFile.absolutePath)
         loaded = true
 
         // we have to set this property to avoid render flickering.
