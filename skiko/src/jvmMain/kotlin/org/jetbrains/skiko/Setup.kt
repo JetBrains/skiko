9bf0d33dea5eb87926c85a861909ae45e69c106f
Roman Sedaikin <roman.sedaikin@jetbrains.com>
2021-01-20 14:22:03 +0300

    Added useScreenMenuBar argument for Setup.init

--- skiko/src/jvmMain/kotlin/org/jetbrains/skiko/Setup.kt
+++ skiko/src/jvmMain/kotlin/org/jetbrains/skiko/Setup.kt
@@ -3,7 +3,11 @@ package org.jetbrains.skiko
 import javax.swing.UIManager
 
 object Setup {
-    fun init(globalLAF: Boolean = false, noEraseBackground: Boolean = true) {
+    fun init(
+        noEraseBackground: Boolean = true,
+        globalLAF: Boolean = false,
+        useScreenMenuBar: Boolean = true
+    ) {
         if (noEraseBackground) {
             // we have to set this property to avoid render flickering.
             System.setProperty("sun.awt.noerasebackground", "true")
@@ -13,11 +17,11 @@ object Setup {
                 // Setup menu look and feel.
                 UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
             }
+            if (useScreenMenuBar) {
+                System.setProperty("apple.laf.useScreenMenuBar", "true")
+            }
         } catch (e: UnsupportedOperationException) {
             // Not all platforms allow this.
-        }
-        if (hostOs == OS.MacOS) {
-            System.setProperty("apple.laf.useScreenMenuBar", "true")
         }
     }
 }
