8ff83215e9c9f2ef11225c1b1eae503174f4cef7
Roman Sedaikin <roman.sedaikin@jetbrains.com>
2021-01-20 14:17:25 +0300

    Fixed Setup.init

--- skiko/src/jvmMain/kotlin/org/jetbrains/skiko/Setup.kt
+++ skiko/src/jvmMain/kotlin/org/jetbrains/skiko/Setup.kt
@@ -2,39 +2,22 @@ package org.jetbrains.skiko
 
 import javax.swing.UIManager
 
-// object Setup {
-//     fun init(globalLAF: Boolean = false, noEraseBackground: Boolean = true) {
-//         if (noEraseBackground) {
-//             // we have to set this property to avoid render flickering.
-//             System.setProperty("sun.awt.noerasebackground", "true")
-//         }
-//         try {
-//             if (globalLAF) {
-//                 // Setup menu look and feel.
-//                 UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
-//                 System.setProperty("apple.laf.useScreenMenuBar", "true")
-//             }
-//         } catch (e: UnsupportedOperationException) {
-//             // Not all platforms allow this.
-//         }
-//     }
-// }
-
 object Setup {
     fun init(globalLAF: Boolean = false, noEraseBackground: Boolean = true) {
         if (noEraseBackground) {
             // we have to set this property to avoid render flickering.
             System.setProperty("sun.awt.noerasebackground", "true")
         }
-        when(hostOs) {
-            OS.MacOS -> {
-                System.setProperty("apple.laf.useScreenMenuBar", "true")
+        try {
+            if (globalLAF) {
+                // Setup menu look and feel.
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
             }
-            else -> {
-                if (globalLAF) {
-                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
-                }
-            }
+        } catch (e: UnsupportedOperationException) {
+            // Not all platforms allow this.
+        }
+        if (hostOs == OS.MacOS) {
+            System.setProperty("apple.laf.useScreenMenuBar", "true")
         }
     }
 }
