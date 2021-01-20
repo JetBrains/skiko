8a345b27a89a5bd268d405f6927eb05a5c52c459
Roman Sedaikin <roman.sedaikin@jetbrains.com>
2021-01-20 12:09:51 +0300

    Fixed MenuBar for Mac OS

--- skiko/src/jvmMain/kotlin/org/jetbrains/skiko/Setup.kt
+++ skiko/src/jvmMain/kotlin/org/jetbrains/skiko/Setup.kt
@@ -2,20 +2,39 @@ package org.jetbrains.skiko
 
 import javax.swing.UIManager
 
+// object Setup {
+//     fun init(globalLAF: Boolean = false, noEraseBackground: Boolean = true) {
+//         if (noEraseBackground) {
+//             // we have to set this property to avoid render flickering.
+//             System.setProperty("sun.awt.noerasebackground", "true")
+//         }
+//         try {
+//             if (globalLAF) {
+//                 // Setup menu look and feel.
+//                 UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+//                 System.setProperty("apple.laf.useScreenMenuBar", "true")
+//             }
+//         } catch (e: UnsupportedOperationException) {
+//             // Not all platforms allow this.
+//         }
+//     }
+// }
+
 object Setup {
     fun init(globalLAF: Boolean = false, noEraseBackground: Boolean = true) {
         if (noEraseBackground) {
             // we have to set this property to avoid render flickering.
             System.setProperty("sun.awt.noerasebackground", "true")
         }
-        try {
-            if (globalLAF) {
-                // Setup menu look and feel.
-                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+        when(hostOs) {
+            OS.MacOS -> {
                 System.setProperty("apple.laf.useScreenMenuBar", "true")
             }
-        } catch (e: UnsupportedOperationException) {
-            // Not all platforms allow this.
+            else -> {
+                if (globalLAF) {
+                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+                }
+            }
         }
     }
 }
