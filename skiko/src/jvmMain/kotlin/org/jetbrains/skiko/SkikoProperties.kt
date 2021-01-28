54c0c302d08cb3c07365a1683f8fa1a1ef94f7c6
Igor Demin <igor.demin@jetbrains.com>
2021-01-28 16:13:59 +0300

    Refactor all properties into SkikoProperties.kt

---
+++ skiko/src/jvmMain/kotlin/org/jetbrains/skiko/SkikoProperties.kt
@@ -0,0 +1,20 @@
+package org.jetbrains.skiko
+
+@Suppress("SameParameterValue")
+internal object SkikoProperties {
+    val fpsEnabled: Boolean by property("skiko.fps.enabled", default = false)
+    val fpsCount: Int by property("skiko.fps.count", default = 300)
+    val fpsProbability: Double by property("skiko.fps.probability", default = 0.97)
+
+    private fun property(name: String, default: Boolean) = lazy {
+        System.getProperty(name)?.toBoolean() ?: default
+    }
+
+    private fun property(name: String, default: Int) = lazy {
+        System.getProperty(name)?.toInt() ?: default
+    }
+
+    private fun property(name: String, default: Double) = lazy {
+        System.getProperty(name)?.toDouble() ?: default
+    }
+}
