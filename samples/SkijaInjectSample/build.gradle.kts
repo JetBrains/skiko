9d621e5cc7982e4903cd21dd4a9407eec6dd2eb6
Igor Demin <igor.demin@jetbrains.com>
2021-01-29 12:11:15 +0300

    Convert SkijaInjectSample to Gradle Kotlin DSL

---
+++ samples/SkijaInjectSample/build.gradle.kts
@@ -0,0 +1,67 @@
+plugins {
+    kotlin("jvm") version "1.3.72"
+    application
+}
+
+repositories {
+    mavenLocal()
+    jcenter()
+    maven("https://dl.bintray.com/kotlin/kotlin-eap")
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+}
+
+val osName = System.getProperty("os.name")
+val targetOs = when {
+    osName == "Mac OS X" -> "macos"
+    osName.startsWith("Win") -> "windows"
+    osName.startsWith("Linux") -> "linux"
+    else -> error("Unsupported OS: $osName")
+}
+
+val osArch = System.getProperty("os.arch")
+var targetArch = when (osArch) {
+    "x86_64", "amd64" -> "x64"
+    "aarch64" -> "arm64"
+    else -> error("Unsupported arch: $osArch")
+}
+
+val target = "${targetOs}-${targetArch}"
+
+var version = "0.0.0-SNAPSHOT"
+if (project.hasProperty("skiko.version")) {
+  version = project.properties["skiko.version"] as String
+}
+
+dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.4.1")
+    implementation("org.jetbrains.skiko:skiko-jvm-runtime-$target:$version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
+
+application {
+    mainClass.set("SkijaInjectSample.AppKt")
+}
+
+tasks.named<JavaExec>("run") {
+    systemProperty("skiko.fps.enabled", "true")
+    System.getProperties().entries
+        .associate {
+            (it.key as String) to (it.value as String)
+        }
+        .filterKeys { it.startsWith("skiko.") }
+        .forEach { systemProperty(it.key, it.value) }
+}
+
+tasks.withType<Test> {
+    systemProperty("skiko.test.screenshots.dir", File(project.projectDir, "src/test/screenshots").absolutePath)
+
+    // Tests should be deterministic, so disable scaling.
+    // On MacOs we need the actual scale, otherwise we will have aliased screenshots because of scaling.
+    if (System.getProperty("os.name") != "Mac OS X") {
+        systemProperty("sun.java2d.dpiaware", "false")
+        systemProperty("sun.java2d.uiScale", "1")
+    }
+}
