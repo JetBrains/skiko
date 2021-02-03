3b530c046ba0d6fec64f95489b1c65a280c478ac
Igor Demin <igor.demin@jetbrains.com>
2021-02-03 19:02:09 +0300

    JNI refactoring. Make fromJavaPointer/toJavaPointer inline

--- skiko/src/jvmMain/cpp/common/jni_helpers.h
+++ skiko/src/jvmMain/cpp/common/jni_helpers.h
@@ -1,7 +1,7 @@
 #pragma once
 
 template<typename T>
-T fromJavaPointer(jlong ptr) { return reinterpret_cast<T>(static_cast<uintptr_t>(ptr)); }
+T inline fromJavaPointer(jlong ptr) { return reinterpret_cast<T>(static_cast<uintptr_t>(ptr)); }
 
 template<typename T>
-jlong toJavaPointer(T ptr) { return static_cast<jlong>(reinterpret_cast<uintptr_t>(ptr)); }
+jlong inline toJavaPointer(T ptr) { return static_cast<jlong>(reinterpret_cast<uintptr_t>(ptr)); }
