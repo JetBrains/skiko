2c853c3201e3f603cf07fe1c6e927690d7b6c93c
Nikolay Igotti <igotti@gmail.com>
2021-05-13 13:31:28 +0300

    Test to create bitmap.

--- skiko/src/jvmTest/kotlin/org/jetbrains/skiko/BaseTest.kt
+++ skiko/src/jvmTest/kotlin/org/jetbrains/skiko/BaseTest.kt
@@ -1,10 +1,17 @@
 package org.jetbrains.skiko
 
+import org.jetbrains.skija.Bitmap
 import org.junit.Test
 
 internal class BaseTest {
     @Test
-    fun loadLibrary() {
+    fun `load library`() {
         Library.load()
+    }
+
+    @Test
+    fun `make bitmap`() {
+        val bitmap = Bitmap()
+        assert(bitmap.isNull)
     }
 }
