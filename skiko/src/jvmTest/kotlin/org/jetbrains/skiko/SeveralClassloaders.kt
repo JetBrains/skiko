cf177ff21741ed23150e3dde1b3219819f14608a
Nikolay Igotti <igotti@gmail.com>
2021-06-17 12:37:14 +0300

    Typo

--- skiko/src/jvmTest/kotlin/org/jetbrains/skiko/SeveralClassloaders.kt
+++ skiko/src/jvmTest/kotlin/org/jetbrains/skiko/SeveralClassloaders.kt
@@ -62,7 +62,7 @@ class SeveralClassloaders {
         val jar = System.getProperty("skiko.jar.path")
         val stdlibClass = Class.forName("kotlin.jvm.internal.Intrinsics")
         val stdLibJar = stdlibClass.protectionDomain.codeSource.location
-        val urls = listOf(URL("file:/$jar"), stdLibJar)
+        val urls = listOf(URL("file:/${if (hostOs.isWindows) "" else "/"}$jar"), stdLibJar)
         val loaders = arrayOf(PlatformAndURLClassLoader(urls), PlatformAndURLClassLoader(urls))
         if (threaded) {
             val threads = mutableListOf<Thread>()
