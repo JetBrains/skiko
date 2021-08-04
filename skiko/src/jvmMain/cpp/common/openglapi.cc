8f603b6b8af6da134d1028b597c4674b41ccc36d
Roman Sedaikin <rsedaikin@gmail.com>
2021-08-04 17:30:44 +0300

    Added llvmpipe to adapter blacklist for OpenGL

--- skiko/src/jvmMain/cpp/common/openglapi.cc
+++ skiko/src/jvmMain/cpp/common/openglapi.cc
@@ -14,7 +14,8 @@
 const std::vector<std::string> vendorBlacklist{ "VMware, Inc." };
 const std::vector<std::string> adapterBlacklist{
     "llvmpipe (LLVM 5.0, 256 bits)",
-    "Intel(R) HD Graphics 2000",
+    "llvmpipe (LLVM 11.0.1, 256 bits)",
+    "Intel(R) HD Graphics 2000"
 };
 
 extern "C" {
