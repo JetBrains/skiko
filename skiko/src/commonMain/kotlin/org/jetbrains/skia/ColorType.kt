42ec4aaf057cf813fced2f589dfba017c68e9278
Nikolay Igotti <igotti@gmail.com>
2021-09-03 09:40:59 +0300

    JS compilation

--- skiko/src/commonMain/kotlin/org/jetbrains/skia/ColorType.kt
+++ skiko/src/commonMain/kotlin/org/jetbrains/skia/ColorType.kt
@@ -1,3 +1,4 @@
+@file:Suppress("NESTED_EXTERNAL_DECLARATION")
 package org.jetbrains.skia
 
 import kotlin.jvm.JvmStatic
---
+++ skiko/src/jsMain/kotlin/org/jetbrains/skia/impl/Library.js.kt
@@ -0,0 +1,9 @@
+package org.jetbrains.skia.impl
+
+actual class Library {
+    actual companion object {
+        actual fun staticLoad() {
+            TODO()
+        }
+    }
+}
