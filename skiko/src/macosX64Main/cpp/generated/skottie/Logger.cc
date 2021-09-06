6902880e5643e9223fbd135e9ca6afcccc6ef7d7
Alexander Gorshenev <alexander.gorshenev@jetbrains.com>
2021-09-06 14:36:01 +0300

    Added skottie native bindings

---
+++ skiko/src/macosX64Main/cpp/generated/skottie/Logger.cc
@@ -0,0 +1,36 @@
+
+// This file has been auto generated.
+
+#include "Skottie.h"
+using namespace skottie;
+#include "common.h"
+
+
+extern "C" jlong org_jetbrains_skia_skottie_Logger__1nMake
+  (kref __Kinstance) {
+    throw std::runtime_error("TODO: implement org_jetbrains_skia_skottie_Logger__1nMake");
+}
+     
+#if 0 
+extern "C" jlong org_jetbrains_skia_skottie_Logger__1nMake
+  (kref __Kinstance) {
+    SkijaLoggerImpl* instance = new SkijaLoggerImpl();
+    return reinterpret_cast<jlong>(instance);
+}
+#endif
+
+
+
+extern "C" void org_jetbrains_skia_skottie_Logger__1nInit
+  (kref __Kinstance, jlong ptr) {
+    throw std::runtime_error("TODO: implement org_jetbrains_skia_skottie_Logger__1nInit");
+}
+     
+#if 0 
+extern "C" void org_jetbrains_skia_skottie_Logger__1nInit
+  (kref __Kinstance, jlong ptr) {
+    SkijaLoggerImpl* instance = reinterpret_cast<SkijaLoggerImpl*>(static_cast<uintptr_t>(ptr));
+    instance->init(env, jthis);
+}
+#endif
+
