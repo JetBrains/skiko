16e2cc1727cce1c61cadef3f94732ee2e6a44758
Nikolay Igotti <igotti@gmail.com>
2021-09-09 19:39:26 +0300

    Unneeded imports.

--- skiko/src/jvmTest/kotlin/org/jetbrains/skiko/LibraryTest.kt
+++ skiko/src/jvmTest/kotlin/org/jetbrains/skiko/LibraryTest.kt
@@ -1,9 +1,6 @@
 package org.jetbrains.skiko
 
 import org.jetbrains.skia.Bitmap
-import org.jetbrains.skia.Color4f
-import org.jetbrains.skia.ColorFilter
-import org.jetbrains.skia.ColorSpace
 import org.junit.Test
 
 internal class LibraryTest {
