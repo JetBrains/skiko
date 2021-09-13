00491f4434f78ef045865ab6a842c3b8f610ee3d
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2021-09-13 18:19:58 +0200

    BufferUtil with top-level native API exported declarations

--- skiko/src/commonMain/kotlin/org/jetbrains/skia/impl/BufferUtil.kt
+++ skiko/src/commonMain/kotlin/org/jetbrains/skia/impl/BufferUtil.kt
@@ -3,7 +3,6 @@ package org.jetbrains.skia.impl
 
 import org.jetbrains.skia.ByteBuffer
 import org.jetbrains.skia.ExternalSymbolName
-import kotlin.jvm.JvmStatic
 
 object BufferUtil {
     fun getByteBufferFromPointer(ptr: NativePointer, size: Int): ByteBuffer {
@@ -16,11 +15,11 @@ object BufferUtil {
         require(result != Native.NullPointer) { "The given buffer " + buffer + "is not a direct buffer or current JVM doesn't support JNI direct buffer access!" }
         return result
     }
-
-    @JvmStatic
-    @ExternalSymbolName("org_jetbrains_skia_BufferUtil__1nGetByteBufferFromPointer")
-    external fun _nGetByteBufferFromPointer(ptr: NativePointer, size: Int): ByteBuffer?
-    @JvmStatic
-    @ExternalSymbolName("org_jetbrains_skia_BufferUtil__1nGetPointerFromByteBuffer")
-    external fun _nGetPointerFromByteBuffer(buffer: ByteBuffer?): NativePointer
 }
+
+
+@ExternalSymbolName("org_jetbrains_skia_BufferUtil__1nGetByteBufferFromPointer")
+private external fun _nGetByteBufferFromPointer(ptr: NativePointer, size: Int): ByteBuffer?
+
+@ExternalSymbolName("org_jetbrains_skia_BufferUtil__1nGetPointerFromByteBuffer")
+private external fun _nGetPointerFromByteBuffer(buffer: ByteBuffer?): NativePointer
