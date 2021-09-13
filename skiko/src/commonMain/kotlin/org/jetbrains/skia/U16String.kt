beffd9c9c42bf99566d2c46350211ef16985bc26
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2021-09-13 18:14:17 +0200

    U16String with top-level native API exported declarations

--- skiko/src/commonMain/kotlin/org/jetbrains/skia/U16String.kt
+++ skiko/src/commonMain/kotlin/org/jetbrains/skia/U16String.kt
@@ -5,25 +5,13 @@ import org.jetbrains.skia.impl.Library.Companion.staticLoad
 import org.jetbrains.skia.impl.Managed
 import org.jetbrains.skia.impl.Stats
 import org.jetbrains.skia.impl.reachabilityBarrier
-import org.jetbrains.skia.ExternalSymbolName
 import org.jetbrains.skia.impl.NativePointer
-import kotlin.jvm.JvmStatic
 
 /**
  * Kotlin mirror of std::vector&lt;jchar&gt; (UTF-16)
  */
 class U16String internal constructor(ptr: NativePointer) : Managed(ptr, _FinalizerHolder.PTR) {
     companion object {
-        @JvmStatic
-        @ExternalSymbolName("org_jetbrains_skia_U16String__1nMake")
-        external fun _nMake(s: String?): NativePointer
-        @JvmStatic
-        @ExternalSymbolName("org_jetbrains_skia_U16String__1nGetFinalizer")
-        external fun _nGetFinalizer(): NativePointer
-        @JvmStatic
-        @ExternalSymbolName("org_jetbrains_skia_U16String__1nToString")
-        external fun _nToString(ptr: NativePointer): String
-
         init {
             staticLoad()
         }
@@ -46,3 +34,13 @@ class U16String internal constructor(ptr: NativePointer) : Managed(ptr, _Finaliz
         val PTR = _nGetFinalizer()
     }
 }
+
+
+@ExternalSymbolName("org_jetbrains_skia_U16String__1nMake")
+private external fun _nMake(s: String?): NativePointer
+
+@ExternalSymbolName("org_jetbrains_skia_U16String__1nGetFinalizer")
+private external fun _nGetFinalizer(): NativePointer
+
+@ExternalSymbolName("org_jetbrains_skia_U16String__1nToString")
+private external fun _nToString(ptr: NativePointer): String
