e8bb68acf8a5d804aea8fcd4e283bffaa984af22
Rsedaikin <rsedaikin@gmail.com>
2021-09-14 13:47:10 +0300

    Windows handle exceptions on DirectX and OpenGL

--- skiko/src/jvmMain/cpp/common/impl/Managed.cc
+++ skiko/src/jvmMain/cpp/common/impl/Managed.cc
@@ -1,11 +1,16 @@
 #include <iostream>
 #include <jni.h>
+#include "jni_helpers.h"
 
 typedef void (*FreeFunction)(void*);
 
 extern "C" JNIEXPORT void JNICALL Java_org_jetbrains_skia_impl_Managed__1nInvokeFinalizer
   (JNIEnv* env, jclass jclass, jlong finalizerPtr, jlong ptr) {
-    void* instance = reinterpret_cast<void*>(static_cast<uintptr_t>(ptr));
-    FreeFunction finalizer = reinterpret_cast<FreeFunction>(static_cast<uintptr_t>(finalizerPtr));
-    finalizer(instance);
+    try {
+      void* instance = reinterpret_cast<void*>(static_cast<uintptr_t>(ptr));
+      FreeFunction finalizer = reinterpret_cast<FreeFunction>(static_cast<uintptr_t>(finalizerPtr));
+      finalizer(instance);
+    } catch(...) {
+      logJavaException(env, handleException(__FUNCTION__));
+    }
 }
