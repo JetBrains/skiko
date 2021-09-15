5dc8f9fe493f6f883283be934e8a2b361012eec9
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2021-09-15 21:42:16 +0200

    simplify setup.js

--- skiko/src/jsMain/resources/setup.js
+++ skiko/src/jsMain/resources/setup.js
@@ -1,17 +1,7 @@
 var wasmSetup = new Promise(function(resolve, reject) {
     Module['onRuntimeInitialized'] = _ => {
-        onInit(window);
         resolve(Module);
     };
 });
 
 function onWasmReady(onReady) { wasmSetup.then(onReady); }
-
-function onInit(context) {
-    Object.keys(Module).forEach((key) => {
-        if (key.startsWith("org_jetbrains_skia")) {
-            console.log(`BINGO ${key}`);
-        }
-    })
-    //context["org_jetbrains_skia_Paint__1nMake"] = Module["org_jetbrains_skia_Paint__1nMake"];
-}
