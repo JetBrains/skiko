775990a5bc595cadeeb55ef0b331c6f215c70e47
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2021-09-15 22:49:47 +0200

    Remove redundant todo and helper func

--- skiko/src/jsMain/kotlin/org/jetbrains/skia/impl/Library.js.kt
+++ skiko/src/jsMain/kotlin/org/jetbrains/skia/impl/Library.js.kt
@@ -3,8 +3,6 @@ package org.jetbrains.skia.impl
 actual class Library {
     actual companion object {
         actual fun staticLoad() {
-            // TODO: load wasm and js modules, maybe just js, as it will load Wasm.
-            TODO()
         }
     }
 }
