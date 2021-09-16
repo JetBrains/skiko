099bb79d542102d3dc6cf55cf4d74e1164c60099
Alexander Gorshenev <alexander.gorshenev@jetbrains.com>
2021-09-16 12:03:56 +0300

    Provided Canvas::drawVertices so that compose doesn't have to call _nDrawVertices explicitly

--- skiko/src/commonMain/kotlin/org/jetbrains/skia/Canvas.kt
+++ skiko/src/commonMain/kotlin/org/jetbrains/skia/Canvas.kt
@@ -808,6 +808,57 @@ open class Canvas internal constructor(ptr: NativePointer, managed: Boolean, int
 
     /**
      *
+     * Draws a triangle, triangle strip or triangle fan mesh, using clip and Matrix.
+     *
+     *
+     * If paint contains an Shader and vertices does not contain texCoords, the shader
+     * is mapped using the vertices' positions.
+     *
+     *
+     * If vertices colors are defined in vertices, and Paint paint contains Shader,
+     * BlendMode mode combines vertices colors with Shader.
+     *
+     * @param vertexMode 0 - triangles, 1 - triangle strip, 2 - triangle fan
+     * @param positions  the mesh to draw
+     * @param colors     color array, one for each corner; may be null
+     * @param texCoords  Point array of texture coordinates, mapping Shader to corners; may be null
+     * @param indices    with which indices points should be drawn; may be null
+     * @param mode       combines vertices colors with Shader, if both are present
+     * @param paint      specifies the Shader, used as Vertices texture
+     *
+     * @see [https://fiddle.skia.org/c/@Canvas_drawVertices](https://fiddle.skia.org/c/@Canvas_drawVertices)
+     *
+     * @see [https://fiddle.skia.org/c/@Canvas_drawVertices_2](https://fiddle.skia.org/c/@Canvas_drawVertices_2)
+     */
+
+    fun drawVertices(
+        vertexMode: Int,
+        positions: Array<Point>,
+        colors: IntArray?,
+        texCoords: Array<Point>?,
+        indices: ShortArray?,
+        mode: BlendMode,
+        paint: Paint
+    ): Canvas {
+        require(colors == null || colors.size == positions.size) { "Expected colors.length == positions.length, got: " + colors!!.size + " != " + positions.size }
+        require(texCoords == null || texCoords.size == positions.size) { "Expected texCoords.length == positions.length, got: " + texCoords!!.size + " != " + positions.size }
+        Stats.onNativeCall()
+        _nDrawVertices(
+            _ptr,
+            vertexMode,
+            Point.flattenArray(positions),
+            colors,
+            Point.flattenArray(texCoords),
+            indices,
+            mode.ordinal,
+            getPtr(paint)
+        )
+        reachabilityBarrier(paint)
+        return this
+    }
+
+    /**
+     *
      * Draws a Coons patch: the interpolation of four cubics with shared corners,
      * associating a color, and optionally a texture SkPoint, with each corner.
      *
