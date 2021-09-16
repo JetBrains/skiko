ce6032e3ef068b5e79e1c12664d97b2a23e0dd98
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2021-09-17 00:40:58 +0200

    Minimal API for webgl interaction

--- skiko/src/jsMain/cpp/wrapper.cc
+++ skiko/src/jsMain/cpp/wrapper.cc
@@ -4,7 +4,12 @@
 #include <emscripten.h>
 #include <emscripten/html5.h>
 #include "include/gpu/GrDirectContext.h"
-#include "include/gpu/gl/GrGLInterface.h"
+#include "include/core/SkSurface.h"
+#include "gpu/gl/GrGLInterface.h"
+#include "gpu/gl/GrGLTypes.h"
+#include "src/gpu/gl/GrGLDefines.h"
+#include <GLES3/gl3.h>
+#include "webgl/webgl1.h"
 
 using namespace emscripten;
 
@@ -14,6 +19,61 @@ extern "C" void* init_surface() {
 
 bool ping() {
     return true;
+}
+
+
+// Set the pixel format based on the colortype.
+// These degrees of freedom are removed from canvaskit only to keep the interface simpler.
+struct ColorSettings {
+    ColorSettings(sk_sp<SkColorSpace> colorSpace) {
+        if (colorSpace == nullptr || colorSpace->isSRGB()) {
+            colorType = kRGBA_8888_SkColorType;
+            pixFormat = GR_GL_RGBA8;
+        } else {
+            colorType = kRGBA_F16_SkColorType;
+            pixFormat = GR_GL_RGBA16F;
+        }
+    };
+    SkColorType colorType;
+    GrGLenum pixFormat;
+};
+
+sk_sp<SkSurface> MakeOnScreenGLSurface(sk_sp<GrDirectContext> dContext, int width, int height,
+                                       sk_sp<SkColorSpace> colorSpace) {
+    // WebGL should already be clearing the color and stencil buffers, but do it again here to
+    // ensure Skia receives them in the expected state.
+    emscripten_glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    emscripten_glClearColor(0, 0, 0, 0);
+    emscripten_glClearStencil(0);
+    emscripten_glClear(GL_COLOR_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+    dContext->resetContext(kRenderTarget_GrGLBackendState | kMisc_GrGLBackendState);
+
+    // The on-screen canvas is FBO 0. Wrap it in a Skia render target so Skia can render to it.
+    GrGLFramebufferInfo info;
+    info.fFBOID = 0;
+
+    GrGLint sampleCnt;
+    emscripten_glGetIntegerv(GL_SAMPLES, &sampleCnt);
+
+    GrGLint stencil;
+    emscripten_glGetIntegerv(GL_STENCIL_BITS, &stencil);
+
+    const auto colorSettings = ColorSettings(colorSpace);
+    info.fFormat = colorSettings.pixFormat;
+    GrBackendRenderTarget target(width, height, sampleCnt, stencil, info);
+    sk_sp<SkSurface> surface(SkSurface::MakeFromBackendRenderTarget(dContext.get(), target,
+        kBottomLeft_GrSurfaceOrigin, colorSettings.colorType, colorSpace, nullptr));
+    return surface;
+}
+
+EMSCRIPTEN_WEBGL_CONTEXT_HANDLE createContext(std::string id) {
+    EmscriptenWebGLContextAttributes attr;
+    emscripten_webgl_init_context_attributes(&attr);
+    attr.majorVersion = 2;
+    EMSCRIPTEN_WEBGL_CONTEXT_HANDLE ctx = emscripten_webgl_create_context(id.c_str(), &attr);
+    assert(ctx && "Failed to create WebGL2 context");
+    emscripten_webgl_make_context_current(ctx);
+    return ctx;
 }
 
 /**
@@ -38,5 +98,7 @@ static sk_sp<GrDirectContext> MakeGrContext(EMSCRIPTEN_WEBGL_CONTEXT_HANDLE cont
 EMSCRIPTEN_BINDINGS(Skiko) {
     function("ping", &ping);
     function("MakeGrContext", &MakeGrContext);
+    function("createContext", &createContext);
+    function("MakeOnScreenGLSurface", &MakeOnScreenGLSurface);
 };
 
--- skiko/src/jsMain/resources/setup.js
+++ skiko/src/jsMain/resources/setup.js
@@ -5,3 +5,50 @@ var wasmSetup = new Promise(function(resolve, reject) {
 });
 
 function onWasmReady(onReady) { wasmSetup.then(onReady); }
+
+var GetWebGLContext = function(canvas, attrs) {
+    function get(obj, attr, defaultValue) {
+        if (obj && obj.hasOwnProperty(attr)) {
+            return obj[attr];
+        }
+        return defaultValue;
+    }
+
+    if (!canvas) {
+        throw 'null canvas passed into makeWebGLContext';
+    }
+    var contextAttributes = {
+        'alpha': get(attrs, 'alpha', 1),
+        'depth': get(attrs, 'depth', 1),
+        'stencil': get(attrs, 'stencil', 8),
+        'antialias': get(attrs, 'antialias', 0),
+        'premultipliedAlpha': get(attrs, 'premultipliedAlpha', 1),
+        'preserveDrawingBuffer': get(attrs, 'preserveDrawingBuffer', 0),
+        'preferLowPowerToHighPerformance': get(attrs, 'preferLowPowerToHighPerformance', 0),
+        'failIfMajorPerformanceCaveat': get(attrs, 'failIfMajorPerformanceCaveat', 0),
+        'enableExtensionsByDefault': get(attrs, 'enableExtensionsByDefault', 1),
+        'explicitSwapControl': get(attrs, 'explicitSwapControl', 0),
+        'renderViaOffscreenBackBuffer': get(attrs, 'renderViaOffscreenBackBuffer', 0),
+    };
+
+    if (attrs && attrs['majorVersion']) {
+        contextAttributes['majorVersion'] = attrs['majorVersion']
+    } else {
+        // Default to WebGL 2 if available and not specified.
+        contextAttributes['majorVersion'] = (typeof WebGL2RenderingContext !== 'undefined') ? 2 : 1;
+    }
+
+    // This check is from the emscripten version
+    if (contextAttributes['explicitSwapControl']) {
+        throw 'explicitSwapControl is not supported';
+    }
+
+    // Creates a WebGL context and sets it to be the current context.
+    // These functions are defined in emscripten's library_webgl.js
+    var handle = GL.createContext(canvas, contextAttributes);
+    if (!handle) {
+        return 0;
+    }
+    GL.makeContextCurrent(handle);
+    return handle;
+}
