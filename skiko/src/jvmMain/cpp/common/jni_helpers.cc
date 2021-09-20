5b065ac95142225c0509561296f43d46a8c5c51f
Roman Sedaikin <roman.sedaikin@jetbrains.com>
2021-09-20 19:05:23 +0300

    Fix after rebase.

--- skiko/src/jvmMain/cpp/common/jni_helpers.cc
+++ skiko/src/jvmMain/cpp/common/jni_helpers.cc
@@ -77,10 +77,6 @@ std::string getDescription(DWORD code)
     default:
         return "UNKNOWN EXCEPTION";
     }
-    const char *property = env->GetStringUTFChars(propertyString, 0);
-    bool result = !strcmp(property, "true");
-    env->ReleaseStringUTFChars(propertyString, property);
-    return result;
 }
 
 void logJavaException(JNIEnv *env, const char *function, DWORD sehCode)
@@ -91,56 +87,8 @@ void logJavaException(JNIEnv *env, const char *function, DWORD sehCode)
 
     if (jvm == NULL)
     {
-    case EXCEPTION_ACCESS_VIOLATION:
-        return "EXCEPTION_ACCESS_VIOLATION";
-    case EXCEPTION_ARRAY_BOUNDS_EXCEEDED:
-        return "EXCEPTION_ARRAY_BOUNDS_EXCEEDED";
-    case EXCEPTION_BREAKPOINT:
-        return "EXCEPTION_BREAKPOINT";
-    case EXCEPTION_DATATYPE_MISALIGNMENT:
-        return "EXCEPTION_DATATYPE_MISALIGNMENT";
-    case EXCEPTION_FLT_DENORMAL_OPERAND:
-        return "EXCEPTION_FLT_DENORMAL_OPERAND";
-    case EXCEPTION_FLT_DIVIDE_BY_ZERO:
-        return "EXCEPTION_FLT_DIVIDE_BY_ZERO";
-    case EXCEPTION_FLT_INEXACT_RESULT:
-        return "EXCEPTION_FLT_INEXACT_RESULT";
-    case EXCEPTION_FLT_INVALID_OPERATION:
-        return "EXCEPTION_FLT_INVALID_OPERATION";
-    case EXCEPTION_FLT_OVERFLOW:
-        return "EXCEPTION_FLT_OVERFLOW";
-    case EXCEPTION_FLT_STACK_CHECK:
-        return "EXCEPTION_FLT_STACK_CHECK";
-    case EXCEPTION_FLT_UNDERFLOW:
-        return "EXCEPTION_FLT_UNDERFLOW";
-    case EXCEPTION_ILLEGAL_INSTRUCTION:
-        return "EXCEPTION_ILLEGAL_INSTRUCTION";
-    case EXCEPTION_IN_PAGE_ERROR:
-        return "EXCEPTION_IN_PAGE_ERROR";
-    case EXCEPTION_INT_DIVIDE_BY_ZERO:
-        return "EXCEPTION_INT_DIVIDE_BY_ZERO";
-    case EXCEPTION_INT_OVERFLOW:
-        return "EXCEPTION_INT_OVERFLOW";
-    case EXCEPTION_INVALID_DISPOSITION:
-        return "EXCEPTION_INVALID_DISPOSITION";
-    case EXCEPTION_NONCONTINUABLE_EXCEPTION:
-        return "EXCEPTION_NONCONTINUABLE_EXCEPTION";
-    case EXCEPTION_PRIV_INSTRUCTION:
-        return "EXCEPTION_PRIV_INSTRUCTION";
-    case EXCEPTION_SINGLE_STEP:
-        return "EXCEPTION_SINGLE_STEP";
-    case EXCEPTION_STACK_OVERFLOW:
-        return "EXCEPTION_STACK_OVERFLOW";
-    default:
-        return "UNKNOWN EXCEPTION";
+        env->GetJavaVM(&jvm);
     }
-}
-
-void logJavaException(JNIEnv *env, const char *function, DWORD sehCode)
-{
-    std::ostringstream oss;
-    oss << "Native exception in [" << function << "]:" << std::endl;
-    oss << "SEH description: " << getDescription(sehCode) << std::endl;
 
     if (isHandleException(env)) {
         static jclass logClass = NULL;
