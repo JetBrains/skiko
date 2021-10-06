e61b0db6baee7973b79ba2619cda7d7ba00aef50
Nikolay Igotti <igotti@gmail.com>
2021-10-06 09:19:50 +0300

    Revert "added libskunicode.a to the native klib (#233)"
    This reverts commit c9bfd868b595d4c634a164d8e03ebb85dc977300.

--- skiko/build.gradle.kts
+++ skiko/build.gradle.kts
@@ -307,16 +307,18 @@ kotlin {
             val skiaDir = unpackedSkia.absolutePath
             val skiaBinSubdir = "out/${buildType.id}-$targetString"
 
-            val skiaLibraries = project.fileTree("$skiaDir/$skiaBinSubdir") {
-                include("**/*.a")
-            }.files.map { it.absolutePath }
-            val bridgesLibrary = "$buildDir/nativeBridges/static/$targetString/skiko-native-bridges-$targetString.a"
-            val libraries = skiaLibraries + bridgesLibrary
-            val libraryFlags = libraries.map { listOf("-include-binary", it) }.flatten()
-
             compilation.target.compilations.all {
                 kotlinOptions {
-                    freeCompilerArgs = libraryFlags
+                    freeCompilerArgs += listOf(
+                            "-include-binary",
+                            "$skiaDir/$skiaBinSubdir/libskia.a",
+                            "-include-binary",
+                            "$skiaDir/$skiaBinSubdir/libskshaper.a",
+                            "-include-binary",
+                            "$skiaDir/$skiaBinSubdir/libskparagraph.a",
+                            "-include-binary",
+                            "$buildDir/nativeBridges/static/$targetString/skiko-native-bridges-$targetString.a"
+                    )
                 }
             }
 
