9b0c3637c6c22b0c95ff827305c9359ab52685ff
Nikolay Igotti <igotti@gmail.com>
2021-10-08 17:09:20 +0300

    One more

--- skiko/src/linuxMain/kotlin/org/jetbrains/skiko/Actuals.linux.kt
+++ skiko/src/linuxMain/kotlin/org/jetbrains/skiko/Actuals.linux.kt
@@ -1,6 +1,12 @@
 package org.jetbrains.skiko
 
 import org.jetbrains.skiko.redrawer.Redrawer
+import org.jetbrains.skiko.context.ContextHandler
+
+internal actual fun createNativeContextHandler(layer: SkiaLayer, renderApi: GraphicsApi): ContextHandler {
+    TODO()
+}
+
 
 internal actual fun createNativeRedrawer(
     layer: SkiaLayer,
