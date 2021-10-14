42f1d7d63e4dfaab586982102df2442161ba8706
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2021-10-14 11:26:33 +0300

    Unify small utils files

--- skiko/buildSrc/src/main/kotlin/fileExtUtils.kt
+++
@@ -1,7 +0,0 @@
-val OS.dynamicLibExt: String
-    get() = when (this) {
-        OS.Linux -> ".so"
-        OS.Windows -> ".dll"
-        OS.MacOS, OS.IOS -> ".dylib"
-        OS.Wasm -> ".wasm"
-    }
--- skiko/buildSrc/src/main/kotlin/utils.kt
+++ skiko/buildSrc/src/main/kotlin/utils.kt
@@ -3,7 +3,7 @@ import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.TaskContainer
 import org.gradle.kotlin.dsl.register
 
-// Utils, that are not needed in scripts should go to internal/utils
+// Utils, that are not needed in scripts can be placed to internal/utils
 
 inline fun <reified T : Task> TaskContainer.registerOrGetTask(
     name: String, crossinline fn: T.() -> Unit
@@ -13,3 +13,12 @@ inline fun <reified T : Task> TaskContainer.registerOrGetTask(
         else register(name, T::class) { fn(this) }
     return taskProvider.map { it as T }
 }
+
+fun joinToTitleCamelCase(vararg parts: String): String =
+    parts.joinToString(separator = "") { part ->
+        if (part.isEmpty()) part
+        else buildString {
+            append(part.first().toTitleCase())
+            append(part.substring(1))
+        }
+    }
