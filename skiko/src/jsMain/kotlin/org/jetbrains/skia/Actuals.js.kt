af41b1f7613ac635c73440b7ed0781e3d9f95460
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2021-10-14 17:56:39 +0200

    Minimal String.intCodePoints implementation on all platforms

--- skiko/src/jsMain/kotlin/org/jetbrains/skia/Actuals.js.kt
+++ skiko/src/jsMain/kotlin/org/jetbrains/skia/Actuals.js.kt
@@ -7,7 +7,7 @@ actual fun <R> commonSynchronized(lock: Any, block: () -> R) {
     block()
 }
 
-actual fun String.intCodePoints(): IntArray = TODO()
+actual fun String.intCodePoints(): IntArray = toList().map { it.code }.toIntArray()
 
 actual class Pattern {
     actual fun split(input: CharSequence): Array<String?>? = TODO()
