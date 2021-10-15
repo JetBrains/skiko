fe86e74d31f1520a082a136411b26e72cd706113
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2021-10-15 15:43:25 +0200

    Use better assertion in CodePointsTest

--- skiko/src/commonTest/kotlin/org/jetbrains/skia/CodePointsTest.kt
+++ skiko/src/commonTest/kotlin/org/jetbrains/skia/CodePointsTest.kt
@@ -1,18 +1,14 @@
 package org.jetbrains.skia
 
 import kotlin.test.Test
+import kotlin.test.assertContentEquals
 import kotlin.test.assertEquals
 
 class CodePointsTest {
-
-    private fun assertEqualsIntArray(a: IntArray, s: String) {
-        assertEquals(a.asList(), s.intCodePoints().asList())
-    }
-
     @Test
     fun codePointsTest() {
-        assertEqualsIntArray(intArrayOf(97, 98, 99), "abc")
-        assertEqualsIntArray(intArrayOf(774, 97, 774, 97, 97, 807, 776, 776), "̆ăaä̧̈")
-        assertEqualsIntArray(intArrayOf(774, 9786), "̆☺")
+        assertContentEquals(intArrayOf(97, 98, 99), "abc".intCodePoints())
+        assertContentEquals(intArrayOf(774, 97, 774, 97, 97, 807, 776, 776), "̆ăaä̧̈".intCodePoints())
+        assertContentEquals(intArrayOf(774, 9786), "̆☺".intCodePoints())
     }
 }
