13d931397d5968993d8a1e7e98fa72e3365ccf75
Aleksandr Veselov <aleksandr.veselov@jetbrains.com>
2021-10-22 18:53:01 +0300

    Use PATH_MAX for linux implementation

--- skiko/src/linuxTest/kotlin/org/jetbrains/skia/util/makeFromResource.kt
+++ skiko/src/linuxTest/kotlin/org/jetbrains/skia/util/makeFromResource.kt
@@ -3,6 +3,7 @@ package org.jetbrains.skia.tests
 import org.jetbrains.skia.Data
 import org.jetbrains.skia.Typeface
 import platform.posix.realpath
+import platform.posix.PATH_MAX
 import kotlinx.cinterop.addressOf
 import kotlinx.cinterop.toKString
 import kotlinx.cinterop.usePinned
@@ -12,7 +13,7 @@ private const val RESOURCES_PATH = "src/commonTest/resources"
 fun makePath(resourceId: String) = run {
     val filePath = "$RESOURCES_PATH/$resourceId"
     // Remove all '..' and '.'
-    var buffer = ByteArray(512)
+    var buffer = ByteArray(PATH_MAX)
     val standardized = buffer.usePinned {
         realpath(filePath, it.addressOf(0))?.toKString()
     }
