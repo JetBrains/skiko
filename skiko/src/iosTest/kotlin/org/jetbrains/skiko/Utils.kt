801aaa4a07db473487b90643e95ae1e6abd090a7
Nikolay Igotti <igotti@gmail.com>
2021-10-22 13:48:48 +0300

    Refactoring

--- skiko/src/iosTest/kotlin/org/jetbrains/skiko/Utils.kt
+++ skiko/src/iosTest/kotlin/org/jetbrains/skiko/Utils.kt
@@ -9,22 +9,23 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
-fun execPath(): String {
-    return NSBundle.mainBundle.bundlePath
+fun resourcePath(): String {
+    val kexeDir = NSBundle.mainBundle.bundlePath
+    // Binary currently compiled into `build/bin/iosX64/debugTest`.
+    return "$kexeDir/../../../../src/iosTest/resources"
 }
 
 class BasicIosTests {
     @Test
     fun resourceAccess() {
-        // Binary currently compiled into `build/bin/iosX64/debugTest`.
-        val resPath = "${execPath()}/../../../../src/iosTest/resources"
-        val path = "$resPath/data.txt"
-        val f = fopen(path, "r") ?: throw Error("cannot open $path: ${strerror(errno)}")
+        val path = "${resourcePath()}/data.txt"
+        val f = fopen(path, "r") ?: throw Error("cannot open $path: ${strerror(errno)?.toKString()}")
         val buf = ByteArray(200)
         buf.usePinned {
             val rv = fread(it.addressOf(0), 1, buf.size.toULong(), f)
             assertTrue { rv != 0UL }
         }
         assertEquals("This is test data.\n", buf.toKString())
+        fclose(f)
     }
 }
