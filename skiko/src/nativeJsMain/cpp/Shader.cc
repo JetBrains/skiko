81cbf367d516bc15ee65b037f28cc02a4fc3488a
Aleksandr Veselov <aleksandr.veselov@jetbrains.com>
2021-10-27 19:56:06 +0300

    Implement native Shader methods, add creation test (#317)

--- skiko/src/nativeJsMain/cpp/Shader.cc
+++ skiko/src/nativeJsMain/cpp/Shader.cc
@@ -5,6 +5,8 @@
 #include "SkColorFilter.h"
 #include "SkShader.h"
 #include "SkGradientShader.h"
+#include "SkPerlinNoiseShader.h"
+#include "SkSize.h"
 #include "common.h"
 
 SKIKO_EXPORT KNativePointer org_jetbrains_skia_Shader__1nMakeWithColorFilter
@@ -17,165 +19,96 @@ SKIKO_EXPORT KNativePointer org_jetbrains_skia_Shader__1nMakeWithColorFilter
 
 
 SKIKO_EXPORT KNativePointer org_jetbrains_skia_Shader__1nMakeLinearGradient
-  (KFloat x0, KFloat y0, KFloat x1, KFloat y1, KInt* colorsArray, KFloat* posArray, KInt tileModeInt, KInt flags, KFloat* matrixArray) {
-    TODO("implement org_jetbrains_skia_Shader__1nMakeLinearGradient");
-}
-     
-#if 0 
-SKIKO_EXPORT KNativePointer org_jetbrains_skia_Shader__1nMakeLinearGradient
-  (KFloat x0, KFloat y0, KFloat x1, KFloat y1, KInt* colorsArray, KFloat* posArray, KInt tileModeInt, KInt flags, KFloat* matrixArray) {
+  (KFloat x0, KFloat y0, KFloat x1, KFloat y1, KInt* colorsArray, KFloat* posArray, KInt count, KInt tileModeInt, KInt flags, KFloat* matrixArray) {
     SkPoint pts[2] {SkPoint::Make(x0, y0), SkPoint::Make(x1, y1)};
-    KInt* colors = env->GetIntArrayElements(colorsArray, nullptr);
-    float* pos = posArray == nullptr ? nullptr : env->GetFloatArrayElements(posArray, nullptr);
+    SkColor* colors = reinterpret_cast<SkColor*>(colorsArray);
+    float* pos = reinterpret_cast<float*>(posArray);
     SkTileMode tileMode = static_cast<SkTileMode>(tileModeInt);
-    std::unique_ptr<SkMatrix> localMatrix = skMatrix(env, matrixArray);
-    SkShader* ptr = SkGradientShader::MakeLinear(pts, reinterpret_cast<SkColor*>(colors), pos, env->GetArrayLength(colorsArray), tileMode, static_cast<uint32_t>(flags), localMatrix.get()).release();
-    env->ReleaseIntArrayElements(colorsArray, colors, 0);
-    if (posArray != nullptr)
-        env->ReleaseFloatArrayElements(posArray, pos, 0);
+    std::unique_ptr<SkMatrix> localMatrix = skMatrix(matrixArray);
+    SkShader* ptr = SkGradientShader::MakeLinear(pts, colors, pos, count, tileMode, static_cast<uint32_t>(flags), localMatrix.get()).release();
     return reinterpret_cast<KNativePointer>(ptr);
 }
-#endif
-
 
 
 SKIKO_EXPORT KNativePointer org_jetbrains_skia_Shader__1nMakeLinearGradientCS
-  (KFloat x0, KFloat y0, KFloat x1, KFloat y1, KFloat* colorsArray, KNativePointer colorSpacePtr, KFloat* posArray, KInt tileModeInt, KInt flags, KFloat* matrixArray) {
-    TODO("implement org_jetbrains_skia_Shader__1nMakeLinearGradientCS");
-}
-     
-#if 0 
-SKIKO_EXPORT KNativePointer org_jetbrains_skia_Shader__1nMakeLinearGradientCS
-  (KFloat x0, KFloat y0, KFloat x1, KFloat y1, KFloat* colorsArray, KNativePointer colorSpacePtr, KFloat* posArray, KInt tileModeInt, KInt flags, KFloat* matrixArray) {
+  (KFloat x0, KFloat y0, KFloat x1, KFloat y1, KFloat* colorsArray, KNativePointer colorSpacePtr, KFloat* posArray, KInt count, KInt tileModeInt, KInt flags, KFloat* matrixArray) {
     SkPoint pts[2] {SkPoint::Make(x0, y0), SkPoint::Make(x1, y1)};
-    float* colors = env->GetFloatArrayElements(colorsArray, nullptr);
+    SkColor4f* colors = reinterpret_cast<SkColor4f*>(colorsArray);
     sk_sp<SkColorSpace> colorSpace = sk_ref_sp<SkColorSpace>(reinterpret_cast<SkColorSpace*>((colorSpacePtr)));
-    float* pos = env->GetFloatArrayElements(posArray, nullptr);
+    float* pos = reinterpret_cast<float*>(posArray);
     SkTileMode tileMode = static_cast<SkTileMode>(tileModeInt);
-    std::unique_ptr<SkMatrix> localMatrix = skMatrix(env, matrixArray);
-    SkShader* ptr = SkGradientShader::MakeLinear(pts, reinterpret_cast<SkColor4f*>(colors), colorSpace, pos, env->GetArrayLength(posArray), tileMode, static_cast<uint32_t>(flags), localMatrix.get()).release();
-    env->ReleaseFloatArrayElements(colorsArray, colors, 0);
-    env->ReleaseFloatArrayElements(posArray, pos, 0);
+    std::unique_ptr<SkMatrix> localMatrix = skMatrix(matrixArray);
+    SkShader* ptr = SkGradientShader::MakeLinear(pts, colors, colorSpace, pos, count, tileMode, static_cast<uint32_t>(flags), localMatrix.get()).release();
     return reinterpret_cast<KNativePointer>(ptr);
 }
-#endif
-
 
 
 SKIKO_EXPORT KNativePointer org_jetbrains_skia_Shader__1nMakeRadialGradient
-  (KFloat x, KFloat y, KFloat r, KInt* colorsArray, KFloat* posArray, KInt tileModeInt, KInt flags, KFloat* matrixArray) {
-    TODO("implement org_jetbrains_skia_Shader__1nMakeRadialGradient");
-}
-     
-#if 0 
-SKIKO_EXPORT KNativePointer org_jetbrains_skia_Shader__1nMakeRadialGradient
-  (KFloat x, KFloat y, KFloat r, KInt* colorsArray, KFloat* posArray, KInt tileModeInt, KInt flags, KFloat* matrixArray) {
-    KInt* colors = env->GetIntArrayElements(colorsArray, nullptr);
-    float* pos = posArray == nullptr ? nullptr : env->GetFloatArrayElements(posArray, nullptr);
+  (KFloat x, KFloat y, KFloat r, KInt* colorsArray, KFloat* posArray, KInt count, KInt tileModeInt, KInt flags, KFloat* matrixArray) {
+    SkColor* colors = reinterpret_cast<SkColor*>(colorsArray);
+    float* pos = reinterpret_cast<float*>(posArray);
     SkTileMode tileMode = static_cast<SkTileMode>(tileModeInt);
-    std::unique_ptr<SkMatrix> localMatrix = skMatrix(env, matrixArray);
-    SkShader* ptr = SkGradientShader::MakeRadial(SkPoint::Make(x, y), r, reinterpret_cast<SkColor*>(colors), pos, env->GetArrayLength(colorsArray), tileMode, static_cast<uint32_t>(flags), localMatrix.get()).release();
-    env->ReleaseIntArrayElements(colorsArray, colors, 0);
-    if (posArray != nullptr)
-        env->ReleaseFloatArrayElements(posArray, pos, 0);
+    std::unique_ptr<SkMatrix> localMatrix = skMatrix(matrixArray);
+    SkShader* ptr = SkGradientShader::MakeRadial(SkPoint::Make(x, y), r, colors, pos, count, tileMode, static_cast<uint32_t>(flags), localMatrix.get()).release();
     return reinterpret_cast<KNativePointer>(ptr);
 }
-#endif
-
 
 
 SKIKO_EXPORT KNativePointer org_jetbrains_skia_Shader__1nMakeRadialGradientCS
-  (KFloat x, KFloat y, KFloat r, KFloat* colorsArray, KNativePointer colorSpacePtr, KFloat* posArray, KInt tileModeInt, KInt flags, KFloat* matrixArray) {
-    TODO("implement org_jetbrains_skia_Shader__1nMakeRadialGradientCS");
-}
-     
-#if 0 
-SKIKO_EXPORT KNativePointer org_jetbrains_skia_Shader__1nMakeRadialGradientCS
-  (KFloat x, KFloat y, KFloat r, KFloat* colorsArray, KNativePointer colorSpacePtr, KFloat* posArray, KInt tileModeInt, KInt flags, KFloat* matrixArray) {
-    float* colors = env->GetFloatArrayElements(colorsArray, nullptr);
+  (KFloat x, KFloat y, KFloat r, KFloat* colorsArray, KNativePointer colorSpacePtr, KFloat* posArray, KInt count, KInt tileModeInt, KInt flags, KFloat* matrixArray) {
+    SkColor4f* colors = reinterpret_cast<SkColor4f*>(colorsArray);
     sk_sp<SkColorSpace> colorSpace = sk_ref_sp<SkColorSpace>(reinterpret_cast<SkColorSpace*>((colorSpacePtr)));
-    float* pos = env->GetFloatArrayElements(posArray, nullptr);
+    float* pos = reinterpret_cast<float*>(posArray);
     SkTileMode tileMode = static_cast<SkTileMode>(tileModeInt);
-    std::unique_ptr<SkMatrix> localMatrix = skMatrix(env, matrixArray);
-    SkShader* ptr = SkGradientShader::MakeRadial(SkPoint::Make(x, y), r, reinterpret_cast<SkColor4f*>(colors), colorSpace, pos, env->GetArrayLength(posArray), tileMode, static_cast<uint32_t>(flags), localMatrix.get()).release();
-    env->ReleaseFloatArrayElements(colorsArray, colors, 0);
-    env->ReleaseFloatArrayElements(posArray, pos, 0);
+    std::unique_ptr<SkMatrix> localMatrix = skMatrix(matrixArray);
+    SkShader* ptr = SkGradientShader::MakeRadial(SkPoint::Make(x, y), r, colors, colorSpace, pos, count, tileMode, static_cast<uint32_t>(flags), localMatrix.get()).release();
     return reinterpret_cast<KNativePointer>(ptr);
 }
-#endif
-
 
 
 SKIKO_EXPORT KNativePointer org_jetbrains_skia_Shader__1nMakeTwoPointConicalGradient
-  (KFloat x0, KFloat y0, KFloat r0, KFloat x1, KFloat y1, KFloat r1, KInt* colorsArray, KFloat* posArray, KInt tileModeInt, KInt flags, KFloat* matrixArray) {
-    TODO("implement org_jetbrains_skia_Shader__1nMakeTwoPointConicalGradient");
-}
-     
-#if 0 
-SKIKO_EXPORT KNativePointer org_jetbrains_skia_Shader__1nMakeTwoPointConicalGradient
-  (KFloat x0, KFloat y0, KFloat r0, KFloat x1, KFloat y1, KFloat r1, KInt* colorsArray, KFloat* posArray, KInt tileModeInt, KInt flags, KFloat* matrixArray) {
-    KInt* colors = env->GetIntArrayElements(colorsArray, nullptr);
-    float* pos = posArray == nullptr ? nullptr : env->GetFloatArrayElements(posArray, nullptr);
+  (KFloat x0, KFloat y0, KFloat r0, KFloat x1, KFloat y1, KFloat r1, KInt* colorsArray, KFloat* posArray, KInt count, KInt tileModeInt, KInt flags, KFloat* matrixArray) {
+    SkColor* colors = reinterpret_cast<SkColor*>(colorsArray);
+    float* pos = reinterpret_cast<float*>(posArray);
     SkTileMode tileMode = static_cast<SkTileMode>(tileModeInt);
-    std::unique_ptr<SkMatrix> localMatrix = skMatrix(env, matrixArray);
-    SkShader* ptr = SkGradientShader::MakeTwoPointConical(SkPoint::Make(x0, y0), r0, SkPoint::Make(x1, y1), r1, reinterpret_cast<SkColor*>(colors), pos, env->GetArrayLength(colorsArray), tileMode, static_cast<uint32_t>(flags), localMatrix.get()).release();
-    env->ReleaseIntArrayElements(colorsArray, colors, 0);
-    if (posArray != nullptr)
-        env->ReleaseFloatArrayElements(posArray, pos, 0);
+    std::unique_ptr<SkMatrix> localMatrix = skMatrix(matrixArray);
+    SkShader* ptr = SkGradientShader::MakeTwoPointConical(SkPoint::Make(x0, y0), r0, SkPoint::Make(x1, y1), r1, colors, pos, count, tileMode, static_cast<uint32_t>(flags), localMatrix.get()).release();
     return reinterpret_cast<KNativePointer>(ptr);
 }
-#endif
-
 
 
 SKIKO_EXPORT KNativePointer org_jetbrains_skia_Shader__1nMakeTwoPointConicalGradientCS
-  (KFloat x0, KFloat y0, KFloat r0, KFloat x1, KFloat y1, KFloat r1, KFloat* colorsArray, KNativePointer colorSpacePtr, KFloat* posArray, KInt tileModeInt, KInt flags, KFloat* matrixArray) {
-    TODO("implement org_jetbrains_skia_Shader__1nMakeTwoPointConicalGradientCS");
-}
-     
-#if 0 
-SKIKO_EXPORT KNativePointer org_jetbrains_skia_Shader__1nMakeTwoPointConicalGradientCS
-  (KFloat x0, KFloat y0, KFloat r0, KFloat x1, KFloat y1, KFloat r1, KFloat* colorsArray, KNativePointer colorSpacePtr, KFloat* posArray, KInt tileModeInt, KInt flags, KFloat* matrixArray) {
-    float* colors = env->GetFloatArrayElements(colorsArray, nullptr);
+  (KFloat x0, KFloat y0, KFloat r0, KFloat x1, KFloat y1, KFloat r1, KFloat* colorsArray, KNativePointer colorSpacePtr, KFloat* posArray, KInt count, KInt tileModeInt, KInt flags, KFloat* matrixArray) {
+    SkColor4f* colors = reinterpret_cast<SkColor4f*>(colorsArray);
     sk_sp<SkColorSpace> colorSpace = sk_ref_sp<SkColorSpace>(reinterpret_cast<SkColorSpace*>((colorSpacePtr)));
-    float* pos = env->GetFloatArrayElements(posArray, nullptr);
+    float* pos = reinterpret_cast<float*>(posArray);
     SkTileMode tileMode = static_cast<SkTileMode>(tileModeInt);
-    std::unique_ptr<SkMatrix> localMatrix = skMatrix(env, matrixArray);
-    SkShader* ptr = SkGradientShader::MakeTwoPointConical(SkPoint::Make(x0, y0), r0, SkPoint::Make(x1, y1), r1, reinterpret_cast<SkColor4f*>(colors), colorSpace, pos, env->GetArrayLength(posArray), tileMode, static_cast<uint32_t>(flags), localMatrix.get()).release();
-    env->ReleaseFloatArrayElements(colorsArray, colors, 0);
-    env->ReleaseFloatArrayElements(posArray, pos, 0);
+    std::unique_ptr<SkMatrix> localMatrix = skMatrix(matrixArray);
+    SkShader* ptr = SkGradientShader::MakeTwoPointConical(SkPoint::Make(x0, y0), r0, SkPoint::Make(x1, y1), r1, colors, colorSpace, pos, count, tileMode, static_cast<uint32_t>(flags), localMatrix.get()).release();
     return reinterpret_cast<KNativePointer>(ptr);
 }
-#endif
-
 
 
 SKIKO_EXPORT KNativePointer org_jetbrains_skia_Shader__1nMakeSweepGradient
-  (KFloat x, KFloat y, KFloat start, KFloat end, KInt* colorsArray, KFloat* posArray, KInt tileModeInt, KInt flags, KFloat* matrixArray) {
-    TODO("implement org_jetbrains_skia_Shader__1nMakeSweepGradient");
-}
-     
-#if 0 
-SKIKO_EXPORT KNativePointer org_jetbrains_skia_Shader__1nMakeSweepGradient
-  (KFloat x, KFloat y, KFloat start, KFloat end, KInt* colorsArray, KFloat* posArray, KInt tileModeInt, KInt flags, KFloat* matrixArray) {
-    KInt* colors = env->GetIntArrayElements(colorsArray, nullptr);
-    float* pos = posArray == nullptr ? nullptr : env->GetFloatArrayElements(posArray, nullptr);
+  (KFloat x, KFloat y, KFloat start, KFloat end, KInt* colorsArray, KFloat* posArray, KInt count, KInt tileModeInt, KInt flags, KFloat* matrixArray) {
+    SkColor* colors = reinterpret_cast<SkColor*>(colorsArray);
+    float* pos = reinterpret_cast<float*>(posArray);
     SkTileMode tileMode = static_cast<SkTileMode>(tileModeInt);
-    std::unique_ptr<SkMatrix> localMatrix = skMatrix(env, matrixArray);
-    SkShader* ptr = SkGradientShader::MakeSweep(x, y, reinterpret_cast<SkColor*>(colors), pos, env->GetArrayLength(colorsArray), tileMode, start, end, static_cast<uint32_t>(flags), localMatrix.get()).release();
-    env->ReleaseIntArrayElements(colorsArray, colors, 0);
-    if (posArray != nullptr)
-        env->ReleaseFloatArrayElements(posArray, pos, 0);
+    std::unique_ptr<SkMatrix> localMatrix = skMatrix(matrixArray);
+    SkShader* ptr = SkGradientShader::MakeSweep(x, y, colors, pos, count, tileMode, start, end, static_cast<uint32_t>(flags), localMatrix.get()).release();
     return reinterpret_cast<KNativePointer>(ptr);
 }
-#endif
-
 
 
 SKIKO_EXPORT KNativePointer org_jetbrains_skia_Shader__1nMakeSweepGradientCS
-  (KFloat x, KFloat y, KFloat start, KFloat end, KFloat* colorsArray, KNativePointer colorSpacePtr, KFloat* posArray, KInt tileModeInt, KInt flags, KFloat* matrixArray) {
-    TODO("implement org_jetbrains_skia_Shader__1nMakeSweepGradientCS");
-}
+  (KFloat x, KFloat y, KFloat start, KFloat end, KFloat* colorsArray, KNativePointer colorSpacePtr, KFloat* posArray, KInt count, KInt tileModeInt, KInt flags, KFloat* matrixArray) {
+    SkColor4f* colors = reinterpret_cast<SkColor4f*>(colorsArray);
+    sk_sp<SkColorSpace> colorSpace = sk_ref_sp<SkColorSpace>(reinterpret_cast<SkColorSpace*>((colorSpacePtr)));
+    float* pos = reinterpret_cast<float*>(posArray);
+    SkTileMode tileMode = static_cast<SkTileMode>(tileModeInt);
+    std::unique_ptr<SkMatrix> localMatrix = skMatrix(matrixArray);
+    SkShader* ptr = SkGradientShader::MakeSweep(x, y, colors, colorSpace, pos, count, tileMode, start, end, static_cast<uint32_t>(flags), localMatrix.get()).release();
+    return reinterpret_cast<KNativePointer>(ptr);}
 
 
 SKIKO_EXPORT KNativePointer org_jetbrains_skia_Shader__1nMakeEmpty() {
@@ -194,7 +127,7 @@ SKIKO_EXPORT KNativePointer org_jetbrains_skia_Shader__1nMakeColorCS(KFloat r, K
     return reinterpret_cast<KNativePointer>(ptr);
 }
 
-extern "C" KNativePointer org_jetbrains_skia_Shader__1nMakeBlend(KInt blendModeInt, KNativePointer dstPtr,  KNativePointer srcPtr) {
+SKIKO_EXPORT KNativePointer org_jetbrains_skia_Shader__1nMakeBlend(KInt blendModeInt, KNativePointer dstPtr,  KNativePointer srcPtr) {
     SkShader* dst = reinterpret_cast<SkShader*>((dstPtr));
     SkShader* src = reinterpret_cast<SkShader*>((srcPtr));
     SkBlendMode blendMode = static_cast<SkBlendMode>(blendModeInt);
@@ -203,24 +136,15 @@ extern "C" KNativePointer org_jetbrains_skia_Shader__1nMakeBlend(KInt blendModeI
 }
 
 SKIKO_EXPORT KNativePointer org_jetbrains_skia_Shader__1nMakeFractalNoise
-  (KFloat baseFrequencyX, KFloat baseFrequencyY, KInt numOctaves, KFloat seed, KInt* tilesArray) {
-    TODO("implement org_jetbrains_skia_Shader__1nMakeFractalNoise");
-}
-#if 0
-SKIKO_EXPORT KNativePointer org_jetbrains_skia_Shader__1nMakeFractalNoise
-  (KFloat baseFrequencyX, KFloat baseFrequencyY, KInt numOctaves, KFloat seed, KInt* tilesArray) {
-    int len = env->GetArrayLength(tilesArray);
-    std::vector<SkISize> tiles(len / 2);
-    KInt* arr = env->GetIntArrayElements(tilesArray, 0);
-    for (int i = 0; i < len; i += 2)
-        tiles[i / 2] = {arr[i], arr[i+1]};
-    env->ReleaseIntArrayElements(tilesArray, arr, 0);
-    SkShader* ptr = SkPerlinNoiseShader::MakeFractalNoise(baseFrequencyX, baseFrequencyY, numOctaves, seed, tiles.data()).release();
+  (KFloat baseFrequencyX, KFloat baseFrequencyY, KInt numOctaves, KFloat seed, KInt tileW, KInt tileH) {
+    const SkISize tileSize = SkISize::Make(tileW, tileH);
+    SkShader* ptr = SkPerlinNoiseShader::MakeFractalNoise(baseFrequencyX, baseFrequencyY, numOctaves, seed, &tileSize).release();
     return reinterpret_cast<KNativePointer>(ptr);
 }
-#endif
 
 SKIKO_EXPORT KNativePointer org_jetbrains_skia_Shader__1nMakeTurbulence
-  (KFloat baseFrequencyX, KFloat baseFrequencyY, KInt numOctaves, KFloat seed, KInt* tilesArray) {
-    TODO("implement org_jetbrains_skia_Shader__1nMakeTurbulence");
+  (KFloat baseFrequencyX, KFloat baseFrequencyY, KInt numOctaves, KFloat seed, KInt tileW, KInt tileH) {
+    const SkISize tileSize = SkISize::Make(tileW, tileH);
+    SkShader* ptr = SkPerlinNoiseShader::MakeTurbulence(baseFrequencyX, baseFrequencyY, numOctaves, seed, &tileSize).release();
+    return reinterpret_cast<KNativePointer>(ptr);
 }
