38558835851979d797469ffd12ce5b1b71b15119
Aleksandr Veselov <aleksandr.veselov@jetbrains.com>
2021-11-02 18:30:18 +0300

    Separate managed and unmanaged string result converters

--- skiko/src/nativeJsMain/kotlin/org/jetbrains/skia/RuntimeEffect.nativejs.kt
+++ skiko/src/nativeJsMain/kotlin/org/jetbrains/skia/RuntimeEffect.nativejs.kt
@@ -2,7 +2,7 @@ package org.jetbrains.skia
 
 import org.jetbrains.skia.impl.Native
 import org.jetbrains.skia.impl.NativePointer
-import org.jetbrains.skia.impl.withStringResult
+import org.jetbrains.skia.impl.withStringReferenceResult
 
 internal actual fun RuntimeEffect.Companion.makeFromResultPtr(ptr: NativePointer): RuntimeEffect {
     val errorPtr = Result_nGetError(ptr)
@@ -12,7 +12,7 @@ internal actual fun RuntimeEffect.Companion.makeFromResultPtr(ptr: NativePointer
         return RuntimeEffect(effectPtr)
     } else {
         // Error string is owned by Result
-        val error = withStringResult(false) { errorPtr }
+        val error = withStringReferenceResult { errorPtr }
         Result_nDestroy(ptr)
         throw Error(error)
     }
