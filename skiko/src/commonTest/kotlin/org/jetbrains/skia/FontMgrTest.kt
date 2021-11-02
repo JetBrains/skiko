af45d8df87b5153fbe407acb25c8ba4ad9bf0fb5
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2021-11-02 14:40:00 +0100

    Disable FontMgrTEst::makeFromDataTest till further investigation on iOS failures

--- skiko/src/commonTest/kotlin/org/jetbrains/skia/FontMgrTest.kt
+++ skiko/src/commonTest/kotlin/org/jetbrains/skia/FontMgrTest.kt
@@ -4,6 +4,7 @@ import org.jetbrains.skia.impl.use
 import org.jetbrains.skia.paragraph.TypefaceFontProvider
 import org.jetbrains.skia.tests.makeFromResource
 import org.jetbrains.skiko.tests.SkipJsTarget
+import org.jetbrains.skiko.tests.SkipNativeTarget
 import org.jetbrains.skiko.tests.makeFromFileName
 import org.jetbrains.skiko.tests.runTest
 import kotlin.test.Test
@@ -94,6 +95,7 @@ class FontMgrTest {
 
     @Test
     @SkipJsTarget
+    @SkipNativeTarget
     fun makeFromDataTest() {
         makeFromFileName("src/commonTest/resources/fonts/JetBrainsMono-Italic.ttf").use { data ->
             FontMgr.default.makeFromData(data)!!.use { typeFace ->
