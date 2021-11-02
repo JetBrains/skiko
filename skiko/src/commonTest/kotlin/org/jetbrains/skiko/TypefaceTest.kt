325106850e4323df0a75bd2fe590d9151c13fc3a
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2021-11-02 15:56:23 +0100

    Split TypefaceTest to the part that passes on all platforms and on JVM only

--- skiko/src/commonTest/kotlin/org/jetbrains/skiko/TypefaceTest.kt
+++ skiko/src/commonTest/kotlin/org/jetbrains/skiko/TypefaceTest.kt
@@ -2,6 +2,8 @@ package org.jetbrains.skiko
 
 import org.jetbrains.skia.*
 import org.jetbrains.skia.tests.makeFromResource
+import org.jetbrains.skiko.tests.SkipJsTarget
+import org.jetbrains.skiko.tests.SkipNativeTarget
 import org.jetbrains.skiko.tests.runTest
 import kotlin.test.*
 
@@ -9,7 +11,6 @@ class TypefaceTest {
 
     @Test
     fun typefaceTest() = runTest {
-        // TODO Commonize array and string results and uncomment
         val inter = Typeface.makeFromResource("./fonts/Inter-Hinted-Regular.ttf")
         val interV = Typeface.makeFromResource("./fonts/Inter-V.ttf")
         val jbMono = Typeface.makeFromResource("./fonts/JetBrainsMono-Regular.ttf")
@@ -25,68 +26,70 @@ class TypefaceTest {
         assertFalse(inter.isFixedPitch)
         assertTrue(jbMono.isFixedPitch)
 
-        if (kotlinBackend == KotlinBackend.JVM) {
-            assertNull(inter.variationAxes)
-            assertNull(inter.variations)
-            val axes = arrayOf(
-                FontVariationAxis("wght", 100f, 400f, 900f),
-                FontVariationAxis("slnt", -10f, 0f, 0f)
-            )
-            assertContentEquals(axes, interV.variationAxes)
-
-            val inter500: Typeface = interV.makeClone(FontVariation("wght", 500f))
-            assertNotEquals(inter500, interV)
-            assertContentEquals(FontVariation.parse("wght=500 slnt=0"), inter500.variations)
-            val inter400: Typeface = interV.makeClone(FontVariation("wght", 400f))
-        }
-
         assertNotEquals(inter.uniqueId, interV.uniqueId)
         assertNotEquals(inter, interV)
         assertNotNull(Typeface.makeDefault())
 
-
-        if (kotlinBackend == KotlinBackend.JVM) {
-            val Skia = intArrayOf(83, 107, 105, 97)
-            assertContentEquals(shortArrayOf(394, 713, 677, 503), inter.getUTF32Glyphs(Skia))
-            assertContentEquals(shortArrayOf(394, 713, 677, 503), inter.getStringGlyphs("Skia"))
-        }
         assertEquals(394, inter.getUTF32Glyph(83))
         assertEquals(2548, interV.glyphsCount)
         assertEquals(17, inter.tablesCount)
 
-        if (kotlinBackend == KotlinBackend.JVM) {
-            assertContentEquals(
-                arrayOf(
-                    "GDEF",
-                    "GPOS",
-                    "GSUB",
-                    "OS/2",
-                    "cmap",
-                    "cvt ",
-                    "fpgm",
-                    "gasp",
-                    "glyf",
-                    "head",
-                    "hhea",
-                    "hmtx",
-                    "loca",
-                    "maxp",
-                    "name",
-                    "post",
-                    "prep"
-                ), inter.tableTags
-            )
-        }
-
         assertTrue(inter.getTableData("loca")!!.size > 0)
         assertEquals(2816, inter.unitsPerEm)
-
-        if (kotlinBackend == KotlinBackend.JVM) {
-            assertNull(jbMono.getKerningPairAdjustments(null))
-            assertNull(jbMono.getKerningPairAdjustments(jbMono.getStringGlyphs("TAV")))
-
-            assertContentEquals(arrayOf(FontFamilyName("Inter", "en-US")), interV.familyNames)
-            assertEquals("Inter", interV.familyName)
-        }
     }
+
+    @Test
+    @SkipJsTarget
+    @SkipNativeTarget
+    fun fontVariationTest() = runTest {
+        val inter = Typeface.makeFromResource("./fonts/Inter-Hinted-Regular.ttf")
+        val interV = Typeface.makeFromResource("./fonts/Inter-V.ttf")
+        val jbMono = Typeface.makeFromResource("./fonts/JetBrainsMono-Regular.ttf")
+
+        assertNull(inter.variationAxes)
+        assertNull(inter.variations)
+        val axes = arrayOf(
+            FontVariationAxis("wght", 100f, 400f, 900f),
+            FontVariationAxis("slnt", -10f, 0f, 0f)
+        )
+        assertContentEquals(axes, interV.variationAxes)
+
+        val inter500: Typeface = interV.makeClone(FontVariation("wght", 500f))
+        assertNotEquals(inter500, interV)
+        assertContentEquals(FontVariation.parse("wght=500 slnt=0"), inter500.variations)
+
+        val Skia = intArrayOf(83, 107, 105, 97)
+        assertContentEquals(shortArrayOf(394, 713, 677, 503), inter.getUTF32Glyphs(Skia))
+        assertContentEquals(shortArrayOf(394, 713, 677, 503), inter.getStringGlyphs("Skia"))
+
+        assertContentEquals(
+            arrayOf(
+                "GDEF",
+                "GPOS",
+                "GSUB",
+                "OS/2",
+                "cmap",
+                "cvt ",
+                "fpgm",
+                "gasp",
+                "glyf",
+                "head",
+                "hhea",
+                "hmtx",
+                "loca",
+                "maxp",
+                "name",
+                "post",
+                "prep"
+            ), inter.tableTags
+        )
+
+
+        assertNull(jbMono.getKerningPairAdjustments(null))
+        assertNull(jbMono.getKerningPairAdjustments(jbMono.getStringGlyphs("TAV")))
+
+        assertContentEquals(arrayOf(FontFamilyName("Inter", "en-US")), interV.familyNames)
+        assertEquals("Inter", interV.familyName)
+    }
+
 }
