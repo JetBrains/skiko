f20f688cd9afb2e6a05ee5b7b573da7b1ee0f100
Oleksandr Karpovich <oleksandr.karpovich@jetbrains.com>
2021-11-05 17:21:29 +0100

    TextBlob: _nMakeFromRSXform

--- skiko/src/commonTest/kotlin/org/jetbrains/skia/TextBlobTest.kt
+++ skiko/src/commonTest/kotlin/org/jetbrains/skia/TextBlobTest.kt
@@ -5,6 +5,8 @@ import org.jetbrains.skia.tests.assertContentCloseEnough
 import org.jetbrains.skia.tests.makeFromResource
 import org.jetbrains.skiko.kotlinBackend
 import org.jetbrains.skiko.tests.runTest
+import kotlin.math.cos
+import kotlin.math.sin
 import kotlin.test.*
 
 class TextBlobTest {
@@ -89,10 +91,6 @@ class TextBlobTest {
         val blobFromData = TextBlob.makeFromData(data)!!
         assertContentEquals(expected = glyphs, actual = blobFromData.glyphs)
         assertContentEquals(expected = Point.flattenArray(positions), actual = blobFromData.positions)
-
-//        assertFailsWith<IllegalArgumentException> {
-//            textBlob.clusters
-//        }
     }
 
     @Test
@@ -162,5 +160,74 @@ class TextBlobTest {
         val blobFromData = TextBlob.makeFromData(data)!!
         assertContentEquals(expected = glyphs, actual = blobFromData.glyphs)
         assertContentEquals(expected = positions, actual = blobFromData.positions)
+    }
+
+    @Test // https://fiddle.skia.org/c/@Canvas_drawTextRSXform
+    fun canMakeFromRSXform() = runTest {
+
+        val glyphs = shortArrayOf(1983, 830, 1213, 1205, 638, 1231, 1326, 161, 611, 721, 721, 773, 1326)
+
+        var angle = 0f
+        var scale = 1f
+
+        val rsxForms = glyphs.map {
+            val s = sin(angle.toDouble()).toFloat() * scale
+            val c = cos(angle.toDouble()).toFloat() * scale
+            angle += .45f
+            scale += .2f
+            RSXform(scos = c, ssin = s, tx = -s * 16, ty = c * 16)
+        }.toTypedArray()
+
+        val textBlob = TextBlob.makeFromRSXform(
+            glyphs = glyphs,
+            xform = rsxForms,
+            font = inter36()
+        )!!
+
+        assertNotEquals(
+            illegal = 0,
+            actual = textBlob.uniqueId,
+            message = "uniqueId should return a non-zero value unique among all text blobs."
+        )
+
+        assertNotEquals(
+            illegal = 0,
+            actual = textBlob.uniqueId,
+            message = "uniqueId should return a non-zero value unique among all text blobs."
+        )
+
+        assertContentEquals(
+            expected = glyphs,
+            actual = textBlob.glyphs
+        )
+
+        assertContentCloseEnough(
+            expected = floatArrayOf(
+                1.0f, 0.0f, -0.0f, 16.0f, 1.0805366f, 0.52195865f, -8.351338f, 17.288586f, 0.87025404f, 1.0966578f,
+                -17.546524f, 13.924065f, 0.35041088f, 1.5611575f, -24.97852f, 5.606574f, -0.4089637f, 1.7529259f,
+                -28.046814f, -6.5434194f, -1.2563474f, 1.5561466f, -24.898346f, -20.101559f, -1.9889591f, 0.9402358f,
+                -15.043773f, -31.823345f, -2.3999155f, -0.020177627f, 0.32284203f, -38.398647f, -2.331572f, -1.1505536f,
+                18.408857f, -37.305153f, -1.7220064f, -2.2078712f, 35.32594f, -27.552103f, -0.6323875f, -2.9325907f,
+                46.92145f, -10.1182f, 0.75322014f, -3.1100905f, 49.761448f, 12.051522f, 2.1579552f, -2.6274006f,
+                42.03841f, 34.527283f
+            ),
+            actual = textBlob.positions.also { println(it.joinToString()) },
+            epsilon = eps
+        )
+
+        assertCloseEnough(
+            expected = Rect(-243.59294f, -306.74146f, 272.9392f, 173.94711f),
+            actual = textBlob.bounds,
+            epsilon = eps
+        )
+
+        assertFailsWith<IllegalArgumentException> { textBlob.tightBounds }
+        assertFailsWith<IllegalArgumentException> { textBlob.blockBounds }
+        assertFailsWith<IllegalArgumentException> { textBlob.firstBaseline }
+        assertFailsWith<IllegalArgumentException> { textBlob.lastBaseline }
+
+        val data = textBlob.serializeToData()
+        val blobFromData = TextBlob.makeFromData(data)!!
+        assertContentEquals(expected = glyphs, actual = blobFromData.glyphs)
     }
 }
