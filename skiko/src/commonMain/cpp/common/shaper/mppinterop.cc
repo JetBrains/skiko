c67a7cbcd5ce4a3c36f6c36d45e482de6740f0ae
Oleksandr Karpovich <oleksandr.karpovich@jetbrains.com>
2021-11-05 13:29:01 +0100

    TextBlob: make _nGetBlockBounds code reusable by moving it to commonMain/cpp

--- skiko/src/commonMain/cpp/common/shaper/mppinterop.cc
+++ skiko/src/commonMain/cpp/common/shaper/mppinterop.cc
@@ -1,4 +1,5 @@
 #include "mppinterop.h"
+#include "RunRecordClone.hh"
 
 namespace skikoMpp {
     namespace skrect {
@@ -7,6 +8,39 @@ namespace skikoMpp {
             result[1] = rect.fTop;
             result[2] = rect.fRight;
             result[3] = rect.fBottom;
+        }
+    }
+
+    namespace textblob {
+        std::unique_ptr<SkRect> getBlockBounds(SkTextBlob* instance) {
+            SkTextBlob::Iter iter(*instance);
+            SkTextBlob::Iter::Run run;
+
+            auto bounds = new SkRect();
+            SkFontMetrics metrics;
+
+            while (iter.next(&run)) {
+                // run.fGlyphIndices points directly to runRecord.glyphBuffer(), which comes directly after RunRecord itself
+                auto runRecord = reinterpret_cast<const RunRecordClone*>(run.fGlyphIndices) - 1;
+                if (runRecord->positioning() != 2) // kFull_Positioning
+                    return std::unique_ptr<SkRect>(nullptr);
+
+                SkScalar* posBuffer = runRecord->posBuffer();
+                const SkFont& font = runRecord->fFont;
+                font.getMetrics(&metrics);
+
+                SkScalar lastLeft = posBuffer[(run.fGlyphCount - 1) * 2];
+                SkScalar lastWidth;
+                if (run.fGlyphCount > 1 && SkScalarNearlyEqual(posBuffer[(run.fGlyphCount - 2) * 2], lastLeft))
+                    lastWidth = 0;
+                else
+                    font.getWidths(&run.fGlyphIndices[run.fGlyphCount - 1], 1, &lastWidth);
+
+                auto runBounds = SkRect::MakeLTRB(posBuffer[0], posBuffer[1] + metrics.fAscent, lastLeft + lastWidth, posBuffer[1] + metrics.fDescent);
+                bounds->join(runBounds);
+            }
+
+            return std::unique_ptr<SkRect>(bounds);
         }
     }
 }
