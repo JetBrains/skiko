fa2f2af4bf23dbba3a04c857bfc51e0c8b61b15f
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2021-11-08 13:05:41 +0100

    Commonize setting fontStyle in StrutStyle (#359)
    * Remove rednundant braces
    
    * Introduce StruStyleTests
    
    * StrutStyle.fontStyle commonized
    
    * Setting fontStyle in StrutStyle

---
+++ skiko/src/commonTest/kotlin/org/jetbrains/skia/StrutStyleTests.kt
@@ -0,0 +1,21 @@
+package org.jetbrains.skia
+
+import org.jetbrains.skia.impl.use
+import org.jetbrains.skia.paragraph.StrutStyle
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+
+class StrutStyleTests {
+
+    @Test
+    fun strutStyleTest() {
+        StrutStyle().use { strutStyle ->
+            assertEquals(FontStyle(400, 5, FontSlant.UPRIGHT), strutStyle.fontStyle)
+
+            strutStyle.fontStyle = FontStyle(300, 4, FontSlant.ITALIC)
+
+            assertEquals(FontStyle(300, 4, FontSlant.ITALIC), strutStyle.fontStyle)
+        }
+    }
+}
--- skiko/src/nativeJsMain/cpp/paragraph/StrutStyle.cc
+++ skiko/src/nativeJsMain/cpp/paragraph/StrutStyle.cc
@@ -14,7 +14,7 @@ static void deleteStrutStyle(StrutStyle* instance) {
 
 SKIKO_EXPORT KNativePointer org_jetbrains_skia_paragraph_StrutStyle__1nGetFinalizer
   () {
-    return reinterpret_cast<KNativePointer>((&deleteStrutStyle));
+    return reinterpret_cast<KNativePointer>(&deleteStrutStyle);
 }
 
 SKIKO_EXPORT KNativePointer org_jetbrains_skia_paragraph_StrutStyle__1nMake
@@ -25,8 +25,8 @@ SKIKO_EXPORT KNativePointer org_jetbrains_skia_paragraph_StrutStyle__1nMake
 
 SKIKO_EXPORT KBoolean org_jetbrains_skia_paragraph_StrutStyle__1nEquals
   (KNativePointer ptr, KNativePointer otherPtr) {
-    StrutStyle* instance = reinterpret_cast<StrutStyle*>((ptr));
-    StrutStyle* other = reinterpret_cast<StrutStyle*>((otherPtr));
+    StrutStyle* instance = reinterpret_cast<StrutStyle*>(ptr);
+    StrutStyle* other = reinterpret_cast<StrutStyle*>(otherPtr);
     return *instance == *other;
 }
 
@@ -54,110 +54,96 @@ SKIKO_EXPORT void org_jetbrains_skia_paragraph_StrutStyle__1nSetFontFamilies
 #if 0 
 SKIKO_EXPORT void org_jetbrains_skia_paragraph_StrutStyle__1nSetFontFamilies
   (KNativePointer ptr, KInteropPointerArray familiesArr) {
-    StrutStyle* instance = reinterpret_cast<StrutStyle*>((ptr));
+    StrutStyle* instance = reinterpret_cast<StrutStyle*>(ptr);
     instance->setFontFamilies(skStringVector(env, familiesArr));
 }
 #endif
 
 
 
-SKIKO_EXPORT KInt org_jetbrains_skia_paragraph_StrutStyle__1nGetFontStyle
-  (KNativePointer ptr) {
-    TODO("implement org_jetbrains_skia_paragraph_StrutStyle__1nGetFontStyle");
+SKIKO_EXPORT void org_jetbrains_skia_paragraph_StrutStyle__1nGetFontStyle
+  (KNativePointer ptr, KInt* fontStyleData) {
+  StrutStyle* instance = reinterpret_cast<StrutStyle*>(ptr);
+  SkFontStyle fontStyle = instance->getFontStyle();
+  fontStyleData[0] = fontStyle.weight();
+  fontStyleData[1] = fontStyle.width();
+  fontStyleData[2] = fontStyle.slant();
 }
-     
-#if 0 
-SKIKO_EXPORT KInt org_jetbrains_skia_paragraph_StrutStyle__1nGetFontStyle
-  (KNativePointer ptr) {
-    StrutStyle* instance = reinterpret_cast<StrutStyle*>((ptr));
-    return skija::FontStyle::toJava(instance->getFontStyle());
-}
-#endif
-
-
 
 SKIKO_EXPORT void org_jetbrains_skia_paragraph_StrutStyle__1nSetFontStyle
   (KNativePointer ptr, KInt style) {
-    TODO("implement org_jetbrains_skia_paragraph_StrutStyle__1nSetFontStyle");
+    StrutStyle* instance = reinterpret_cast<StrutStyle*>(ptr);
+    instance->setFontStyle(skija::FontStyle::fromKotlin(style));
 }
      
-#if 0 
-SKIKO_EXPORT void org_jetbrains_skia_paragraph_StrutStyle__1nSetFontStyle
-  (KNativePointer ptr, KInt style) {
-    StrutStyle* instance = reinterpret_cast<StrutStyle*>((ptr));
-    instance->setFontStyle(skija::FontStyle::fromJava(style));
-}
-#endif
-
-
 SKIKO_EXPORT KFloat org_jetbrains_skia_paragraph_StrutStyle__1nGetFontSize
   (KNativePointer ptr) {
-    StrutStyle* instance = reinterpret_cast<StrutStyle*>((ptr));
+    StrutStyle* instance = reinterpret_cast<StrutStyle*>(ptr);
     return instance->getFontSize();
 }
 
 SKIKO_EXPORT void org_jetbrains_skia_paragraph_StrutStyle__1nSetFontSize
   (KNativePointer ptr, KFloat size) {
-    StrutStyle* instance = reinterpret_cast<StrutStyle*>((ptr));
+    StrutStyle* instance = reinterpret_cast<StrutStyle*>(ptr);
     instance->setFontSize(size);
 }
 
 SKIKO_EXPORT KFloat org_jetbrains_skia_paragraph_StrutStyle__1nGetHeight
   (KNativePointer ptr) {
-    StrutStyle* instance = reinterpret_cast<StrutStyle*>((ptr));
+    StrutStyle* instance = reinterpret_cast<StrutStyle*>(ptr);
     return instance->getHeight();
 }
 
 SKIKO_EXPORT void org_jetbrains_skia_paragraph_StrutStyle__1nSetHeight
   (KNativePointer ptr, KFloat height) {
-    StrutStyle* instance = reinterpret_cast<StrutStyle*>((ptr));
+    StrutStyle* instance = reinterpret_cast<StrutStyle*>(ptr);
     instance->setHeight(height);
 }
 
 SKIKO_EXPORT KFloat org_jetbrains_skia_paragraph_StrutStyle__1nGetLeading
   (KNativePointer ptr) {
-    StrutStyle* instance = reinterpret_cast<StrutStyle*>((ptr));
+    StrutStyle* instance = reinterpret_cast<StrutStyle*>(ptr);
     return instance->getLeading();
 }
 
 SKIKO_EXPORT void org_jetbrains_skia_paragraph_StrutStyle__1nSetLeading
   (KNativePointer ptr, KFloat leading) {
-    StrutStyle* instance = reinterpret_cast<StrutStyle*>((ptr));
+    StrutStyle* instance = reinterpret_cast<StrutStyle*>(ptr);
     instance->setLeading(leading);
 }
 
 SKIKO_EXPORT KBoolean org_jetbrains_skia_paragraph_StrutStyle__1nIsEnabled
   (KNativePointer ptr) {
-    StrutStyle* instance = reinterpret_cast<StrutStyle*>((ptr));
+    StrutStyle* instance = reinterpret_cast<StrutStyle*>(ptr);
     return instance->getStrutEnabled();
 }
 
 SKIKO_EXPORT void org_jetbrains_skia_paragraph_StrutStyle__1nSetEnabled
   (KNativePointer ptr, KBoolean value) {
-    StrutStyle* instance = reinterpret_cast<StrutStyle*>((ptr));
+    StrutStyle* instance = reinterpret_cast<StrutStyle*>(ptr);
     instance->setStrutEnabled(value);
 }
 
 SKIKO_EXPORT KBoolean org_jetbrains_skia_paragraph_StrutStyle__1nIsHeightForced
   (KNativePointer ptr) {
-    StrutStyle* instance = reinterpret_cast<StrutStyle*>((ptr));
+    StrutStyle* instance = reinterpret_cast<StrutStyle*>(ptr);
     return instance->getForceStrutHeight();
 }
 
 SKIKO_EXPORT void org_jetbrains_skia_paragraph_StrutStyle__1nSetHeightForced
   (KNativePointer ptr, KBoolean value) {
-    StrutStyle* instance = reinterpret_cast<StrutStyle*>((ptr));
+    StrutStyle* instance = reinterpret_cast<StrutStyle*>(ptr);
     instance->setForceStrutHeight(value);
 }
 
 SKIKO_EXPORT KBoolean org_jetbrains_skia_paragraph_StrutStyle__1nIsHeightOverridden
   (KNativePointer ptr) {
-    StrutStyle* instance = reinterpret_cast<StrutStyle*>((ptr));
+    StrutStyle* instance = reinterpret_cast<StrutStyle*>(ptr);
     return instance->getHeightOverride();
 }
 
 SKIKO_EXPORT void org_jetbrains_skia_paragraph_StrutStyle__1nSetHeightOverridden
   (KNativePointer ptr, KBoolean value) {
-    StrutStyle* instance = reinterpret_cast<StrutStyle*>((ptr));
+    StrutStyle* instance = reinterpret_cast<StrutStyle*>(ptr);
     instance->setHeightOverride(value);
 }
