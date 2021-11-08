970290d8e85bab7e42250dd997430993cb19f597
Oleksandr Karpovich <oleksandr.karpovich@jetbrains.com>
2021-11-08 16:33:42 +0100

    TextBlobBuilder: common appendRunPosH

--- skiko/src/commonTest/kotlin/org/jetbrains/skia/TextBlobBuilderTest.kt
+++ skiko/src/commonTest/kotlin/org/jetbrains/skia/TextBlobBuilderTest.kt
@@ -34,5 +34,57 @@ class TextBlobBuilderTest {
             expected = shortArrayOf(1983, 830, 1213, 1205, 638, 1231, 1326, 161, 611, 721, 721, 773, 1326),
             actual = textBlob.glyphs
         )
+
+        assertCloseEnough(
+            expected = Rect(2f, -28f, 234.97159f, 9f),
+            textBlob.bounds
+        )
+    }
+
+    @Test
+    fun canAppendRunWithTextAndBounds() = runTest {
+        val textBlob = TextBlobBuilder().appendRun(
+            font = inter36(),
+            text = "Привет!Hello!",
+            x = 0f,
+            y = 0f,
+            bounds = Rect(0f, 0f, 50f, 40f)
+        ).build()!!
+
+        assertContentEquals(
+            expected = shortArrayOf(1983, 830, 1213, 1205, 638, 1231, 1326, 161, 611, 721, 721, 773, 1326),
+            actual = textBlob.glyphs
+        )
+
+        assertCloseEnough(
+            expected = Rect(0f, 0f, 50f, 40f),
+            textBlob.bounds
+        )
+    }
+
+    @Test
+    fun canAppendRunWithPositionH() = runTest {
+        val font = inter36()
+        val str = "Привет!Hello!"
+        val glyphs = font.getStringGlyphs(str)
+        assertEquals(str.length, glyphs.size)
+
+        val textBlob = TextBlobBuilder().appendRunPosH(
+            font = font,
+            glyphs = glyphs,
+            bounds = Rect(0f, 0f, 50f, 40f),
+            xs = FloatArray(glyphs.size) { (it + 1) * 10f},
+            y = 5f
+        ).build()!!
+
+        assertContentEquals(
+            expected = shortArrayOf(1983, 830, 1213, 1205, 638, 1231, 1326, 161, 611, 721, 721, 773, 1326),
+            actual = textBlob.glyphs
+        )
+
+        assertCloseEnough(
+            expected = Rect(0f, 0f, 50f, 40f),
+            textBlob.bounds
+        )
     }
 }
