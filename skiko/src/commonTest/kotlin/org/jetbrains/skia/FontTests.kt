bf1e63a1acf26f310a4dca07e223b95a84fe8c61
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2021-11-09 12:53:48 +0100

    Prepartations for commonizing Font::getStringGlyphs

--- skiko/src/commonTest/kotlin/org/jetbrains/skia/FontTests.kt
+++ skiko/src/commonTest/kotlin/org/jetbrains/skia/FontTests.kt
@@ -3,17 +3,24 @@ package org.jetbrains.skia
 import org.jetbrains.skia.impl.use
 import org.jetbrains.skia.tests.assertCloseEnough
 import org.jetbrains.skia.tests.makeFromResource
+import org.jetbrains.skiko.tests.SkipJsTarget
+import org.jetbrains.skiko.tests.SkipNativeTarget
 import org.jetbrains.skiko.tests.runTest
 import kotlin.test.Test
+import kotlin.test.assertContentEquals
 import kotlin.test.assertEquals
 
-class FonTests {
+class FontTests {
     @Test
+    @SkipJsTarget
+    @SkipNativeTarget
     fun fontTest() = runTest {
         val jbMono = Typeface.makeFromResource("./fonts/JetBrainsMono-Regular.ttf")
         Font(jbMono).use { font ->
             assertEquals(12f, font.size)
             assertCloseEnough(14.880001f, font.spacing)
+
+            assertContentEquals(shortArrayOf(17, 18, 19, 20, 21), font.getStringGlyphs("ABCDE"))
         }
     }
 }
