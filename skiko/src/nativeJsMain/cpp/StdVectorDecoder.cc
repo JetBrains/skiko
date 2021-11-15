f003c231390e8c5ad599425a958dd3ca07a39102
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2021-11-15 15:23:48 +0100

    Port StdVectorDecoder from the array_of_pointers branch

---
+++ skiko/src/nativeJsMain/cpp/StdVectorDecoder.cc
@@ -0,0 +1,34 @@
+#include "common.h"
+
+SKIKO_EXPORT KInt org_jetbrains_skia_StdVectorDecoder__1nGetArraySize
+    (KNativePointer ptr) {
+        std::vector<KNativePointer>* vec = reinterpret_cast<std::vector<KNativePointer> *>(ptr);
+        return vec->size();
+    }
+
+SKIKO_EXPORT void org_jetbrains_skia_StdVectorDecoder__1nDisposeArray
+    (KNativePointer ptr) {
+        std::vector<KNativePointer>* vec = reinterpret_cast<std::vector<KNativePointer> *>(ptr);
+
+        void (*dctr)(SkString*) = reinterpret_cast<void (*)(SkString*)>(vec->back());
+        vec->pop_back();
+
+        while (!vec->empty()){
+            SkString* res = reinterpret_cast<SkString*>(vec->back());
+            if (res != nullptr) {
+                dctr(res);
+            }
+
+            vec->pop_back();
+        }
+
+        delete vec;
+    }
+
+SKIKO_EXPORT KNativePointer org_jetbrains_skia_StdVectorDecoder__1nReleaseElement
+    (KNativePointer ptr, KInt index) {
+        std::vector<KNativePointer>* vec = reinterpret_cast<std::vector<KNativePointer> *>(ptr);
+        auto res = (*vec)[index];
+        (*vec)[index] = 0;
+        return res;
+    }
