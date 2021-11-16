1e7963f5fb69e0ef162c86e29bc3efb9de609d56
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2021-11-16 13:19:32 +0100

    Introduce arrayDecoderScope

--- skiko/src/commonMain/kotlin/org/jetbrains/skia/StdVectorDecoder.kt
+++ skiko/src/commonMain/kotlin/org/jetbrains/skia/StdVectorDecoder.kt
@@ -4,8 +4,10 @@ import org.jetbrains.skia.impl.NativePointer
 
 @ExternalSymbolName("org_jetbrains_skia_StdVectorDecoder__1nGetArraySize")
 private external fun StdVectorDecoder_nGetArraySize(array: NativePointer): Int
+
 @ExternalSymbolName("org_jetbrains_skia_StdVectorDecoder__1nDisposeArray")
 private external fun StdVectorDecoder_nDisposeArray(array: NativePointer, disposePtr: NativePointer)
+
 @ExternalSymbolName("org_jetbrains_skia_StdVectorDecoder__1nReleaseElement")
 private external fun StdVectorDecoder_nReleaseElement(array: NativePointer, index: Int): NativePointer
 
@@ -21,4 +23,14 @@ class ArrayDecoder(private val ptr: NativePointer, private val disposePtr: Nativ
 
     val size: Int
         get() = StdVectorDecoder_nGetArraySize(ptr)
+}
+
+inline fun <T> arrayDecoderScope(arrayDecoderBlock: () -> ArrayDecoder, block: (arrayCode: ArrayDecoder) -> T): T {
+    var arrayDecoder: ArrayDecoder? = null
+    return try {
+        arrayDecoder = arrayDecoderBlock()
+        block.invoke(arrayDecoder)
+    } finally {
+        arrayDecoder?.dispose()
+    }
 }
