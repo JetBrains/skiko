50fe753405398a02b50c4170dfb467c1f7afd217
Roman Sedaikin <roman.sedaikin@jetbrains.com>
2021-11-18 16:57:08 +0300

    Refactor 1.

--- skiko/src/macosMain/kotlin/org/jetbrains/skiko/SystemTheme.macos.kt
+++ skiko/src/macosMain/kotlin/org/jetbrains/skiko/SystemTheme.macos.kt
@@ -1,7 +1,6 @@
 package org.jetbrains.skiko
 
-import platform.AppKit.*
-import platform.Foundation.*
+import platform.Foundation.NSUserDefaults
 
 val currentSystemTheme: SystemTheme
     get() = when (NSUserDefaults.standardUserDefaults.stringForKey("AppleInterfaceStyle")) {
