37f8b936301692b44bff2de9f4a469e5e32072f9
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2021-11-22 11:42:32 +0100

    assertCloseEnough for js test

--- skiko/src/commonTest/kotlin/org/jetbrains/skiko/TypefaceTest.kt
+++ skiko/src/commonTest/kotlin/org/jetbrains/skiko/TypefaceTest.kt
@@ -1,6 +1,7 @@
 package org.jetbrains.skiko
 
 import org.jetbrains.skia.*
+import org.jetbrains.skia.tests.assertCloseEnough
 import org.jetbrains.skia.tests.makeFromResource
 import org.jetbrains.skiko.tests.SkipJsTarget
 import org.jetbrains.skiko.tests.SkipNativeTarget
@@ -37,7 +38,7 @@ class TypefaceTest {
         assertTrue(inter.getTableData("loca")!!.size > 0)
         assertEquals(2816, inter.unitsPerEm)
 
-        assertEquals(Rect(left=-0.7386364f, top=-1.0909091f, right=2.5830965f, bottom=0.31960228f), inter.bounds)
+        assertCloseEnough(Rect(left=-0.7386364f, top=-1.0909091f, right=2.5830965f, bottom=0.31960228f), inter.bounds)
     }
 
     @Test
