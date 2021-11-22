5cfc53aca95dddac1702c6fd344817ffb14c7ee5
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2021-11-22 17:35:43 +0100

    Yet another custom epsilon for test

--- skiko/src/commonTest/kotlin/org/jetbrains/skiko/TypefaceTest.kt
+++ skiko/src/commonTest/kotlin/org/jetbrains/skiko/TypefaceTest.kt
@@ -38,7 +38,7 @@ class TypefaceTest {
         assertTrue(inter.getTableData("loca")!!.size > 0)
         assertEquals(2816, inter.unitsPerEm)
 
-        assertCloseEnough(Rect(left=-0.7386364f, top=-1.0909091f, right=2.5830965f, bottom=0.31960228f), inter.bounds)
+        assertCloseEnough(Rect(left=-0.7386364f, top=-1.0909091f, right=2.5830965f, bottom=0.31960228f), inter.bounds, 10e-4f)
     }
 
     @Test
