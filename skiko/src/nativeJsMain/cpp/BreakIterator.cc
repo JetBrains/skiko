16bc9c98b4e0ae20e85183769b185271e899b6af
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2021-11-23 15:00:02 +0100

    Remove redundant double brackets

--- skiko/src/nativeJsMain/cpp/BreakIterator.cc
+++ skiko/src/nativeJsMain/cpp/BreakIterator.cc
@@ -38,55 +38,55 @@ SKIKO_EXPORT KInt org_jetbrains_skia_BreakIterator__1nClone
 
 SKIKO_EXPORT KInt org_jetbrains_skia_BreakIterator__1nCurrent
   (KNativePointer ptr) {
-    UBreakIterator* instance = reinterpret_cast<UBreakIterator*>((ptr));
+    UBreakIterator* instance = reinterpret_cast<UBreakIterator*>(ptr);
     return ubrk_current(instance);
 }
 
 SKIKO_EXPORT KInt org_jetbrains_skia_BreakIterator__1nNext
   (KNativePointer ptr) {
-    UBreakIterator* instance = reinterpret_cast<UBreakIterator*>((ptr));
+    UBreakIterator* instance = reinterpret_cast<UBreakIterator*>(ptr);
     return ubrk_next(instance);
 }
 
 SKIKO_EXPORT KInt org_jetbrains_skia_BreakIterator__1nPrevious
   (KNativePointer ptr) {
-    UBreakIterator* instance = reinterpret_cast<UBreakIterator*>((ptr));
+    UBreakIterator* instance = reinterpret_cast<UBreakIterator*>(ptr);
     return ubrk_previous(instance);
 }
 
 SKIKO_EXPORT KInt org_jetbrains_skia_BreakIterator__1nFirst
   (KNativePointer ptr) {
-    UBreakIterator* instance = reinterpret_cast<UBreakIterator*>((ptr));
+    UBreakIterator* instance = reinterpret_cast<UBreakIterator*>(ptr);
     return ubrk_first(instance);
 }
 
 SKIKO_EXPORT KInt org_jetbrains_skia_BreakIterator__1nLast
   (KNativePointer ptr) {
-    UBreakIterator* instance = reinterpret_cast<UBreakIterator*>((ptr));
+    UBreakIterator* instance = reinterpret_cast<UBreakIterator*>(ptr);
     return ubrk_last(instance);
 }
 
 SKIKO_EXPORT KInt org_jetbrains_skia_BreakIterator__1nPreceding
   (KNativePointer ptr, KInt offset) {
-    UBreakIterator* instance = reinterpret_cast<UBreakIterator*>((ptr));
+    UBreakIterator* instance = reinterpret_cast<UBreakIterator*>(ptr);
     return ubrk_preceding(instance, offset);
 }
 
 SKIKO_EXPORT KInt org_jetbrains_skia_BreakIterator__1nFollowing
   (KNativePointer ptr, KInt offset) {
-    UBreakIterator* instance = reinterpret_cast<UBreakIterator*>((ptr));
+    UBreakIterator* instance = reinterpret_cast<UBreakIterator*>(ptr);
     return ubrk_following(instance, offset);
 }
 
 SKIKO_EXPORT KBoolean org_jetbrains_skia_BreakIterator__1nIsBoundary
   (KNativePointer ptr, KInt offset) {
-    UBreakIterator* instance = reinterpret_cast<UBreakIterator*>((ptr));
+    UBreakIterator* instance = reinterpret_cast<UBreakIterator*>(ptr);
     return ubrk_isBoundary(instance, offset);
 }
 
 SKIKO_EXPORT KInt org_jetbrains_skia_BreakIterator__1nGetRuleStatus
   (KNativePointer ptr) {
-    UBreakIterator* instance = reinterpret_cast<UBreakIterator*>((ptr));
+    UBreakIterator* instance = reinterpret_cast<UBreakIterator*>(ptr);
     return ubrk_getRuleStatus(instance);
 }
 
@@ -95,7 +95,7 @@ SKIKO_EXPORT KInt org_jetbrains_skia_BreakIterator__1nGetRuleStatus
 #if 0 
 SKIKO_EXPORT KInt* org_jetbrains_skia_BreakIterator__1nGetRuleStatuses
   (KNativePointer ptr) {
-    UBreakIterator* instance = reinterpret_cast<UBreakIterator*>((ptr));
+    UBreakIterator* instance = reinterpret_cast<UBreakIterator*>(ptr);
     UErrorCode status = U_ZERO_ERROR;
     int32_t len = ubrk_getRuleStatusVec(instance, nullptr, 0, &status);
     if (U_FAILURE(status))
@@ -118,7 +118,7 @@ SKIKO_EXPORT void org_jetbrains_skia_BreakIterator__1nSetText
 #if 0 
 SKIKO_EXPORT void org_jetbrains_skia_BreakIterator__1nSetText
   (KNativePointer ptr, KNativePointer textPtr) {
-    UBreakIterator* instance = reinterpret_cast<UBreakIterator*>((ptr));
+    UBreakIterator* instance = reinterpret_cast<UBreakIterator*>(ptr);
     std::vector<KChar>* text = reinterpret_cast<std::vector<KChar>*>((textPtr));
     UErrorCode status = U_ZERO_ERROR;
     ubrk_setText(instance, reinterpret_cast<UChar *>(text->data()), text->size(), &status);
