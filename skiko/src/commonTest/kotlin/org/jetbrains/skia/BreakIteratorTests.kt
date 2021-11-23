18b4d13ee769f4bfb432ce077d4868331b5f9ce7
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2021-11-23 16:44:00 +0100

    Better test for break iterator

--- skiko/src/commonTest/kotlin/org/jetbrains/skia/BreakIteratorTests.kt
+++ skiko/src/commonTest/kotlin/org/jetbrains/skia/BreakIteratorTests.kt
@@ -1,17 +1,22 @@
 package org.jetbrains.skia
 
 import kotlin.test.Test
+import kotlin.test.assertContentEquals
 import kotlin.test.assertEquals
+
+private fun BreakIterator.asSequence() = generateSequence { next().let { n -> if (n == -1) null else n } }
 
 class BreakIteratorTests {
 
     @Test
     fun breakIteratorTest() {
         val boundary = BreakIterator.makeWordInstance()
-        boundary.setText("家捷克的软件开发公司")
-        assertEquals(U16String("家捷克的软件开发公司").toString(), boundary._text.toString())
+        boundary.setText("家捷克的软件开发公司 ,software development company")
+
+        assertContentEquals(listOf(1, 3, 4, 6, 8, 10, 11, 12, 20, 21, 32, 33, 40), boundary.asSequence().toList())
 
         assertEquals(0, boundary.first())
-        assertEquals(10, boundary.last())
+        assertEquals(40, boundary.last())
+
     }
 }
