4c3bcf876c5e25a33ecb18ec796315e6ad629ae0
dima.avdeev <dima.avdeev@jetbrains.com>
2022-04-18 15:17:29 +0400

    skiko debug with AppCode

--- iosApp/src/iosAppMain/apple/AppDelegate.swift
+++ iosApp/src/iosAppMain/apple/AppDelegate.swift
@@ -8,9 +8,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = SwiftHelper().getViewController()
-        window?.makeKeyAndVisible()
         return true
     }
 
--- iosApp/src/iosAppMain/apple/SceneDelegate.swift
+++ iosApp/src/iosAppMain/apple/SceneDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import SwiftUI
+import shared
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -12,15 +13,22 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
         // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
-            let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView: contentView)
+
+            let window = UIWindow(windowScene: windowScene) //UIWindow(frame: UIScreen.main.bounds)
+
+            if (false) {
+                window.rootViewController = UIHostingController(rootView: ContentView())
+            } else {
+                window.rootViewController = SwiftHelper().getViewController()
+            }
+
             self.window = window
             window.makeKeyAndVisible()
         }
+
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
