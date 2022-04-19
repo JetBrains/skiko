d2c21d21393753d1bf5a1eb18299abb9c90cbb8e
dima.avdeev <dima.avdeev@jetbrains.com>
2022-04-19 15:04:55 +0400

    remove redundant code

--- iosApp/src/iosAppMain/apple/AppDelegate.swift
+++ iosApp/src/iosAppMain/apple/AppDelegate.swift
@@ -4,14 +4,9 @@ import shared
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
@@ -25,6 +20,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
 }
-
--- iosApp/src/iosAppMain/apple/SceneDelegate.swift
+++ iosApp/src/iosAppMain/apple/SceneDelegate.swift
@@ -6,7 +6,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
@@ -16,19 +15,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
-
-            let window = UIWindow(windowScene: windowScene) //UIWindow(frame: UIScreen.main.bounds)
-
-            if (false) {
-                window.rootViewController = UIHostingController(rootView: ContentView())
-            } else {
-                window.rootViewController = SwiftHelper().getViewController()
-            }
-
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = SwiftHelper().getViewController()
             self.window = window
             window.makeKeyAndVisible()
         }
-
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
