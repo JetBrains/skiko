7b88029356b14d6a470b927573c6da243edc9111
dima.avdeev <dima.avdeev@jetbrains.com>
2022-04-24 22:52:36 +0400

    revert some changes

--- skiko/build.gradle.kts
+++ skiko/build.gradle.kts
@@ -196,13 +196,13 @@ internal val Project.isInIdea: Boolean
     }
 
 val Project.supportNative: Boolean
-   get() = true ///findProperty("skiko.native.enabled") == "true" || isInIdea
+   get() = findProperty("skiko.native.enabled") == "true" || isInIdea
 
 val Project.supportWasm: Boolean
-    get() = true //findProperty("skiko.wasm.enabled") == "true" || isInIdea
+    get() = findProperty("skiko.wasm.enabled") == "true" || isInIdea
 
 val Project.supportAndroid: Boolean
-    get() = true //findProperty("skiko.android.enabled") == "true" // || isInIdea
+    get() = findProperty("skiko.android.enabled") == "true" // || isInIdea
 
 kotlin {
     jvm("awt") {
