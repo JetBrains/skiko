efed5477e4c6684049d375101405d59036720974
dima.avdeev <dima.avdeev@jetbrains.com>
2022-08-03 11:10:28 +0400

    not working uiContextMenuInteraction

--- skiko/src/iosMain/kotlin/org/jetbrains/skiko/SkikoViewController.kt
+++ skiko/src/iosMain/kotlin/org/jetbrains/skiko/SkikoViewController.kt
@@ -1,8 +1,11 @@
 package org.jetbrains.skiko
 
 import kotlinx.cinterop.*
+import platform.CoreGraphics.CGPoint
+import platform.CoreGraphics.CGRectMake
 import platform.Foundation.*
 import platform.UIKit.*
+import platform.darwin.NSObject
 
 @ExportObjCClass
 class SkikoViewController : UIViewController {
@@ -17,11 +20,43 @@ class SkikoViewController : UIViewController {
 
     private var skikoUIView: SkikoUIView? = null
 
+    private val uiContextMenuInteraction = UIContextMenuInteraction(object : NSObject(), UIContextMenuInteractionDelegateProtocol {
+        override fun contextMenuInteraction(
+            interaction: UIContextMenuInteraction,
+            configurationForMenuAtLocation: CValue<CGPoint>
+        ): UIContextMenuConfiguration? {
+            val view = interaction.view
+            println("TODO GOOD contextMenuInteraction, interaction: $interaction, view: $view")
+            return UIContextMenuConfiguration.configurationWithIdentifier(
+                identifier = null,
+                previewProvider = null,
+                actionProvider = null/*object : UIContextMenuActionProvider {
+                        override fun invoke(suggestedActions: List<*>?): UIMenu? {
+                            println("TODO suggestedActions: $suggestedActions")
+                            return null
+                        }
+                    }*/
+            )
+        }
+    })
+
     override fun loadView() {
         if (skikoUIView == null) {
             super.loadView()
         } else {
-            this.view = skikoUIView!!.load()
+            val uiView = UIView(CGRectMake(0.0, 0.0, 100.0, 100.0))
+            uiView.backgroundColor = UIColor.blueColor
+            uiView.addInteraction(uiContextMenuInteraction)
+//            this.view = uiView
+            val loaded = skikoUIView!!.load()
+            loaded.backgroundColor = UIColor.greenColor
+            loaded.addInteraction(uiContextMenuInteraction)
+//            this.view = loaded
+
+            val uiCollectionView = UICollectionView(CGRectMake(0.0, 0.0, 100.0, 200.0), UICollectionViewCompositionalLayout())
+            uiCollectionView.addInteraction(uiContextMenuInteraction)
+            this.view = uiCollectionView
+            uiCollectionView.setBackgroundView(loaded)
         }
     }
 
