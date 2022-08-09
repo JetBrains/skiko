4561adb56b5b575e9b0e607f334f8ccff50ff596
Igor Demin <igordmn@users.noreply.github.com>
2022-08-09 13:09:41 +0300

    Refactor Skiko properties (#563)
    - Make SkikoProperties public, and don't cache the values, so users can change properties in runtime
    
    - Deprecate skiko.directx.gpu.priority and skiko.directx.gpu.priority, introduce skiko.gpu.priority. There is no need in separate priority for macOs/DirectX
    
    - Fix skiko.fps.enabled property

--- skiko/src/jvmMain/kotlin/org/jetbrains/skiko/SkiaLayerProperties.kt
+++ skiko/src/jvmMain/kotlin/org/jetbrains/skiko/SkiaLayerProperties.kt
@@ -4,4 +4,5 @@ internal data class SkiaLayerProperties(
     val isVsyncEnabled: Boolean = SkikoProperties.vsyncEnabled,
     val isVsyncFramelimitFallbackEnabled: Boolean = SkikoProperties.vsyncFramelimitFallbackEnabled,
     val renderApi: GraphicsApi = SkikoProperties.renderApi,
+    val adapterPriority: GpuPriority = SkikoProperties.gpuPriority,
 )
