276a166badda1420975e6a086aacd0d5546c412f
Sebastiano Poggi <sebp@google.com>
2023-01-23 15:38:39 +0100

    Set Java toolchain level to 11
    Was set to 17 by mistake.

--- skiko/build.gradle.kts
+++ skiko/build.gradle.kts
@@ -5,7 +5,6 @@ import org.jetbrains.compose.internal.publishing.MavenCentralProperties
 import org.jetbrains.kotlin.gradle.plugin.KotlinTarget
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompileTool
-import org.jetbrains.kotlin.konan.target.KonanTarget
 
 plugins {
     kotlin("multiplatform") version "1.8.20"
@@ -223,7 +222,7 @@ val Project.supportAndroid: Boolean
     get() = findProperty("skiko.android.enabled") == "true" // || isInIdea
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(11)
 
     jvm("awt") {
         generateVersion(targetOs, targetArch)
@@ -816,8 +815,8 @@ fun createCompileJvmBindingsTask(
 ) = project.registerSkikoTask<CompileSkikoCppTask>("compileJvmBindings", targetOs, targetArch) {
     // Prefer 'java.home' system property to simplify overriding from Intellij.
     // When used from command-line, it is effectively equal to JAVA_HOME.
-    if (JavaVersion.current() < JavaVersion.VERSION_17) {
-        error("JDK 17+ is required, but Gradle JVM is ${JavaVersion.current()}. " +
+    if (JavaVersion.current() < JavaVersion.VERSION_11) {
+        error("JDK 11+ is required, but Gradle JVM is ${JavaVersion.current()}. " +
                 "Check JAVA_HOME (CLI) or Gradle settings (Intellij).")
     }
     val jdkHome = File(System.getProperty("java.home") ?: error("'java.home' is null"))
