8b746d052d415bc13da7b20b4c606422c649fb15
Oleksandr Karpovich <a.n.karpovich@gmail.com>
2023-01-30 16:25:03 +0100

    Update kotlin and coroutines version

--- skiko/settings.gradle.kts
+++ skiko/settings.gradle.kts
@@ -4,6 +4,7 @@ pluginManagement {
         mavenLocal()
         gradlePluginPortal()
         maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/wasm/experimental")
     }
     buildscript {
         repositories {
