aa5e5f7acfac9aa08f216807321a670e8f69894b
Pavel <pavel.sergeev@jetbrains.com>
2023-02-13 15:52:16 +0100

    Fix crash on pack after dispose (#662)
    * added performance test with clocks
    
    ignore tests with clocks
    
    * use simple logger instead of printlns
    
    * checked that we call `finishRecording` at the same picture that we called a `beginRecording`
    
    * make `Logger` internal
    
    * disable assertions in rendering performance test with content; otherwise it might be too flaky
    
    * unignore `FPS is near display refresh rate (multiple windows with clocks)`

--- skiko/src/awtMain/kotlin/org/jetbrains/skiko/SkiaLayer.awt.kt
+++ skiko/src/awtMain/kotlin/org/jetbrains/skiko/SkiaLayer.awt.kt
@@ -291,7 +291,7 @@ actual open class SkiaLayer internal constructor(
                 redrawer = renderFactory.createRedrawer(this, renderApi, analytics, properties)
                 redrawer?.syncSize()
             } catch (e: RenderException) {
-                println(e.message)
+                Logger.warn("Fallback to next API: ${e.message}")
                 thrown = true
             }
         } while (thrown && fallbackRenderApiQueue.isNotEmpty())
@@ -513,7 +513,8 @@ actual open class SkiaLayer internal constructor(
         val pictureHeight = (height * contentScale).toInt().coerceAtLeast(0)
 
         val bounds = Rect.makeWH(pictureWidth.toFloat(), pictureHeight.toFloat())
-        val canvas = pictureRecorder!!.beginRecording(bounds)
+        val pictureRecorder = pictureRecorder!!
+        val canvas = pictureRecorder.beginRecording(bounds)
 
         // clipping
         for (component in clipComponents) {
@@ -528,10 +529,11 @@ actual open class SkiaLayer internal constructor(
         }
 
         // we can dispose layer during onRender
-        if (!isDisposed) {
+        // or even dispose it and pack it again
+        if (!isDisposed && !pictureRecorder.isClosed) {
             synchronized(pictureLock) {
                 picture?.instance?.close()
-                val picture = pictureRecorder!!.finishRecordingAsPicture()
+                val picture = pictureRecorder.finishRecordingAsPicture()
                 this.picture = PictureHolder(picture, pictureWidth, pictureHeight)
             }
         }
@@ -546,7 +548,7 @@ actual open class SkiaLayer internal constructor(
             // ignore
         } catch (e: RenderException) {
             if (!isDisposed) {
-                println(e.message)
+                Logger.warn("Exception in draw scope: ${e.message}")
                 findNextWorkingRenderApi()
                 redrawer?.redrawImmediately()
             }
