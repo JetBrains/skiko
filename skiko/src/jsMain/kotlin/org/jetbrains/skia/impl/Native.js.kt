c939cc31eaa696a1808b038ef11e2494c6e78011
igoriakovlev <54274820+igoriakovlev@users.noreply.github.com>
2023-03-14 20:22:58 +0100

    Fast skiko.wasm to skia arrays and string interop (#675)

--- skiko/src/jsMain/kotlin/org/jetbrains/skia/impl/Native.js.kt
+++ skiko/src/jsMain/kotlin/org/jetbrains/skia/impl/Native.js.kt
@@ -1,5 +1,7 @@
 package org.jetbrains.skia.impl
 
+import org.khronos.webgl.ArrayBufferView
+
 internal actual fun toWasm(dest: NativePointer, src: ByteArray): Unit = HEAPU8.set(src, dest)
 internal actual fun toWasm(dest: NativePointer, src: ShortArray): Unit = HEAPU16.set(src, dest / 2)
 internal actual fun toWasm(dest: NativePointer, src: CharArray): Unit = HEAPU16.set(src, dest / 2)
@@ -7,6 +9,35 @@ internal actual fun toWasm(dest: NativePointer, src: FloatArray): Unit = HEAPF32
 internal actual fun toWasm(dest: NativePointer, src: DoubleArray): Unit = HEAPF64.set(src, dest / 8)
 internal actual fun toWasm(dest: NativePointer, src: IntArray): Unit = HEAPU32.set(src, dest / 4)
 
+internal external interface HEAP<T> {
+    fun set(src: T, dest: NativePointer)
+    fun subarray(startIndex: Int, endIndex: Int): ArrayBufferView
+}
+internal external object HEAPU8: HEAP<ByteArray> {
+    override fun set(src: ByteArray, dest: NativePointer)
+    override fun subarray(startIndex: Int, endIndex: Int): ArrayBufferView
+}
+
+internal external object HEAPU16: HEAP<ShortArray> {
+    override fun set(src: ShortArray, dest: NativePointer)
+    fun set(src: CharArray, dest: NativePointer)
+    override fun subarray(startIndex: Int, endIndex: Int): ArrayBufferView
+}
+
+internal external object HEAPU32: HEAP<IntArray> {
+    override fun set(src: IntArray, dest: NativePointer)
+    override fun subarray(startIndex: Int, endIndex: Int): ArrayBufferView
+}
+
+internal external object HEAPF32: HEAP<FloatArray> {
+    override fun set(src: FloatArray, dest: NativePointer)
+    override fun subarray(startIndex: Int, endIndex: Int): ArrayBufferView
+}
+
+internal external object HEAPF64: HEAP<DoubleArray> {
+    override fun set(src: DoubleArray, dest: NativePointer)
+    override fun subarray(startIndex: Int, endIndex: Int): ArrayBufferView
+}
 
 internal actual fun fromWasm(src: NativePointer, result: ByteArray) {
     result.asDynamic().set(HEAPU8.subarray(src, src + result.size))
@@ -31,3 +62,5 @@ internal actual fun fromWasm(src: NativePointer, result: DoubleArray) {
     val startIndex = src / 8
     result.asDynamic().set(HEAPF64.subarray(startIndex, startIndex + result.size))
 }
+
+internal actual external fun stringToUTF8(str: String, outPtr: NativePointer, maxBytesToWrite: Int)
