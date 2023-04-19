1e38df167617d8283a1a64aef52cf826d2562dfa
Ivan Matkov <ivan.matkov@jetbrains.com>
2023-04-19 16:52:08 +0200

    Move CharDirection to icu package

--- skiko/src/jvmMain/cpp/common/icu/Unicode.cc
+++ skiko/src/jvmMain/cpp/common/icu/Unicode.cc
@@ -1,7 +1,7 @@
 #include <jni.h>
 #include "third_party/externals/icu/source/common/unicode/uchar.h"
 
-extern "C" JNIEXPORT jint JNICALL Java_org_jetbrains_skia_paragraph_DirectionKt_unicodeCharDirection
+extern "C" JNIEXPORT jint JNICALL Java_org_jetbrains_skia_icu_UnicodeKt_charDirection
   (JNIEnv* env, jclass jclass, jint codePoint) {
     return u_charDirection(codePoint);
 }
