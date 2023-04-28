1307b6b2f2b76fce41032ee3ffdd2833225da906
Oleksandr Karpovich <a.n.karpovich@gmail.com>
2023-04-28 12:38:43 +0200

    wasm-main use kotlin 1.9 dev (#696)
    * Use kotlin 1.9.0-dev build - wip - tests do not pass
    
    * Fix tests
    
    * tmp: disable k/js tests on ci - can't find resources when running tests
    
    * Prepare publishing with new dev kotlin
    
    * Update wasm.yml

--- skiko/src/jvmMain/kotlin/org/jetbrains/skia/OutputWStream.kt
+++ skiko/src/jvmMain/kotlin/org/jetbrains/skia/OutputWStream.kt
@@ -1,13 +1,14 @@
 package org.jetbrains.skia
 
-import org.jetbrains.skia.impl.Library.Companion.staticLoad
-import org.jetbrains.skia.impl.Stats
+import org.jetbrains.skia.impl.Library
 import org.jetbrains.skia.impl.NativePointer
+import org.jetbrains.skia.impl.Stats
+import java.io.OutputStream
 
 class OutputWStream(out: OutputStream?) : WStream(_nMake(out), _FinalizerHolder.PTR) {
     companion object {
         init {
-            staticLoad()
+            Library.staticLoad()
         }
     }
 
@@ -21,7 +22,6 @@ class OutputWStream(out: OutputStream?) : WStream(_nMake(out), _FinalizerHolder.
         _out = out
     }
 }
-
 
 @ExternalSymbolName("org_jetbrains_skia_OutputWStream__1nGetFinalizer")
 @ModuleImport("skia", "org_jetbrains_skia_OutputWStream__1nGetFinalizer")
