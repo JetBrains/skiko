47341c43080a7bde375d2fb5858bbd9cfcb72a53
Elijah Semyonov <elijah.semenov@gmail.com>
2023-05-10 19:21:41 +0200

    Throw exception instead of force unwrapping Metal handles when initialising MetalRedrawer. (#704)
    * Throw exception instead of force unwrapping Metal handles
    
    * Update skiko/src/macosMain/kotlin/org/jetbrains/skiko/redrawer/MetalRedrawer.macos.kt
    
    Co-authored-by: Ivan Matkov <ivan.matkov@jetbrains.com>
    
    * Update based on review feedback
    
    * Remove redundant this
    
    * Remove unneeded imports
    
    ---------
    
    Co-authored-by: Elijah Semyonov <elijah.semyonov@jetbrains.com>
    Co-authored-by: Ivan Matkov <ivan.matkov@jetbrains.com>

--- skiko/src/macosMain/kotlin/org/jetbrains/skiko/redrawer/MetalRedrawer.macos.kt
+++ skiko/src/macosMain/kotlin/org/jetbrains/skiko/redrawer/MetalRedrawer.macos.kt
@@ -39,8 +39,8 @@ internal class MacOsMetalRedrawer(
     override val renderInfo: String get() = contextHandler.rendererInfo()
 
     private var isDisposed = false
-    internal val device = MTLCreateSystemDefaultDevice()!!
-    private val queue = device.newCommandQueue()!!
+    internal val device = MTLCreateSystemDefaultDevice() ?: throw IllegalStateException("Metal is not supported on this system")
+    private val queue = device.newCommandQueue() ?: throw IllegalStateException("Couldn't create Metal command queue")
     private var currentDrawable: CAMetalDrawableProtocol? = null
     private val metalLayer = MetalLayer()
 
