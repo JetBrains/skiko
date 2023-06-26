13ad9a814817f02871b088d365c3965c980b77ca
Ivan Matkov <ivan.matkov@jetbrains.com>
2023-06-26 11:57:50 +0200

    Check availability of UITraitCollection.currentTraitCollection

--- skiko/src/iosMain/kotlin/org/jetbrains/skiko/SystemTheme.ios.kt
+++ skiko/src/iosMain/kotlin/org/jetbrains/skiko/SystemTheme.ios.kt
@@ -1,11 +1,25 @@
 package org.jetbrains.skiko
 
+import kotlinx.cinterop.useContents
+import platform.Foundation.NSProcessInfo
 import platform.UIKit.*
 import platform.UIKit.UIUserInterfaceStyle.*
 
 actual val currentSystemTheme: SystemTheme
-    get() = when (UITraitCollection.currentTraitCollection.userInterfaceStyle) {
-        UIUserInterfaceStyleDark -> SystemTheme.DARK
-        UIUserInterfaceStyleLight -> SystemTheme.LIGHT
-        else -> SystemTheme.UNKNOWN
+    get() = if (supportsCurrentTraitCollectionApi) {
+        when (UITraitCollection.currentTraitCollection.userInterfaceStyle) {
+            UIUserInterfaceStyleDark -> SystemTheme.DARK
+            UIUserInterfaceStyleLight -> SystemTheme.LIGHT
+            else -> SystemTheme.UNKNOWN
+        }
+    } else {
+        SystemTheme.LIGHT
     }
+
+/*
+ * Getting trait collection for the current execution context supports only on iOS 13.0+
+ * https://developer.apple.com/documentation/uikit/uitraitcollection/3238080-currenttraitcollection?language=objc
+ */
+private val supportsCurrentTraitCollectionApi get() = NSProcessInfo.processInfo.operatingSystemVersion.useContents {
+    majorVersion >= 13
+}
