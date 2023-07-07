e8bb8637cd21e3ef6c50acce15c747b8106ff887
Elijah Semyonov <elijah.semenov@gmail.com>
2023-07-07 15:31:24 +0200

    Add overcommitment prevention mechanism (#754)
    * Add overcommitment prevention mechanism
    
    * Replace hardcoded value with CAMetalLayer property
    
    * Update skiko/src/awtMain/objectiveC/macos/MetalContextHandler.mm
    
    Co-authored-by: Igor Demin <igordmn@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Igor Demin <igordmn@users.noreply.github.com>

--- skiko/src/awtMain/objectiveC/macos/MetalDevice.h
+++ skiko/src/awtMain/objectiveC/macos/MetalDevice.h
@@ -13,6 +13,7 @@
 @property (strong) id<MTLDevice> adapter;
 @property (strong) id<MTLCommandQueue> queue;
 @property (strong) id<CAMetalDrawable> drawableHandle;
+@property (strong) dispatch_semaphore_t inflightSemaphore;
 
 @end
 
