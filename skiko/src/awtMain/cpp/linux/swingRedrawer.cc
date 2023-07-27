b8ecb16f28a1ddb93d781fc1807ba3f9ce20ad56
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2023-07-27 17:30:47 +0200

    Non-optimal working version of openGL

--- skiko/src/awtMain/cpp/linux/swingRedrawer.cc
+++ skiko/src/awtMain/cpp/linux/swingRedrawer.cc
@@ -14,10 +14,16 @@ class TextureHolder
 public:
     GLuint tex;
     GLuint fbo;
+    Display* display;
+    GLXPbuffer pbuffer;
+    GLXContext context;
 
     ~TextureHolder() {
         glDeleteFramebuffers(1, &fbo);
         glDeleteTextures(1, &tex);
+        glXDestroyContext(display, context);
+        glXDestroyPbuffer(display, pbuffer);
+        XCloseDisplay(display);
     }
 };
 
@@ -26,6 +32,37 @@ extern "C"
     JNIEXPORT jlong JNICALL Java_org_jetbrains_skiko_swing_LinuxOpenGLSwingRedrawer_createAndBindFrameBuffer(
         JNIEnv *env, jobject redrawer, jint width, jint height)
     {
+        const int glxContextAttribs[] {
+            GLX_DRAWABLE_TYPE, GLX_WINDOW_BIT,
+            GLX_RENDER_TYPE, GLX_RGBA_BIT,
+            GLX_DOUBLEBUFFER, False,
+            GLX_RED_SIZE, 8,
+            GLX_GREEN_SIZE, 8,
+            GLX_BLUE_SIZE, 8,
+            None
+        };
+
+        Display* display = XOpenDisplay(nullptr);
+
+        int numConfigs = 0;
+        GLXFBConfig* fbConfigs = glXChooseFBConfig(display, DefaultScreen(display), glxContextAttribs, &numConfigs);
+
+        int pbufferAttribs[] = {
+            GLX_PBUFFER_WIDTH, width,
+            GLX_PBUFFER_HEIGHT, height,
+            None  
+        };
+
+        GLXPbuffer pbuffer = glXCreatePbuffer(display, fbConfigs[0], pbufferAttribs);
+
+        XVisualInfo* visual = glXGetVisualFromFBConfig(display, fbConfigs[0]);
+
+        GLXContext context = glXCreateContext(display, visual, nullptr, True);
+        glXMakeCurrent(display, pbuffer, context);
+
+        XFree(visual);
+        XFree(fbConfigs);
+
         GLuint tex;
         glGenTextures(1, &tex);
         glBindTexture(GL_TEXTURE_2D, tex);
@@ -38,10 +75,11 @@ extern "C"
         glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, tex, 0);
 
         TextureHolder *holder = new TextureHolder();
+        holder->display = display;
+        holder->context = context;
+        holder->pbuffer=pbuffer;
         holder->tex = tex;
         holder->fbo = fbo;
-
-        // DirectXDevice *d3dDevice = fromJavaPointer<DirectXDevice *>(devicePtr);
 
         return toJavaPointer(holder);
     }
@@ -60,5 +98,16 @@ extern "C"
         TextureHolder *d3dDevice = fromJavaPointer<TextureHolder *>(holderPtr);
 
         delete d3dDevice;
+    }
+
+    JNIEXPORT void JNICALL Java_org_jetbrains_skiko_swing_LinuxOpenGLSwingRedrawer_finishRendering(
+        JNIEnv *env, jobject redrawer, jlong holderPtr)
+    {
+        TextureHolder *d3dDevice = fromJavaPointer<TextureHolder *>(holderPtr);
+        glFinish();
+        glXMakeCurrent(d3dDevice->display, None, nullptr);
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        glBindTexture(GL_TEXTURE_2D, 0);
+        glFinish();
     }
 } // extern "C"
--- skiko/src/awtMain/kotlin/org/jetbrains/skiko/swing/LinuxOpenGLSwingRedrawer.kt
+++ skiko/src/awtMain/kotlin/org/jetbrains/skiko/swing/LinuxOpenGLSwingRedrawer.kt
@@ -5,10 +5,10 @@ import org.jetbrains.skiko.*
 import java.awt.Graphics2D
 
 internal class LinuxOpenGLSwingRedrawer(
-    private val swingLayerProperties: SwingLayerProperties,
-    skikoView: SkikoView,
+    swingLayerProperties: SwingLayerProperties,
+    private val skikoView: SkikoView,
     analytics: SkiaLayerAnalytics
-) : SwingRedrawerBase(swingLayerProperties, skikoView, analytics, GraphicsApi.OPENGL) {
+) : SwingRedrawerBase(swingLayerProperties, analytics, GraphicsApi.OPENGL) {
     init {
         onDeviceChosen("OpenGL") // TODO: properly choose device
     }
@@ -17,60 +17,77 @@ internal class LinuxOpenGLSwingRedrawer(
 
     private var holderPtr: Long? = null
 
-    override fun createDirectContext(): DirectContext {
-        return makeGLContext()
+    private val context get() = makeGLContext()
+
+    private val storage = Bitmap()
+
+    private var bytesToDraw = ByteArray(0)
+
+    init {
+        onContextInit()
     }
 
-    override fun initCanvas(context: DirectContext?): DrawingSurfaceData {
-        context ?: error("DirectContext should be initialized")
-        val scale = swingLayerProperties.scale
-        val w = (swingLayerProperties.width * scale).toInt().coerceAtLeast(0)
-        val h = (swingLayerProperties.height * scale).toInt().coerceAtLeast(0)
-
-        holderPtr = createAndBindFrameBuffer(w, h)
-        val fbId = getFboId(holderPtr!!)
-        val renderTarget = makeGLRenderTarget(
-            w,
-            h,
-            0,
-            8,
-            fbId,
-            FramebufferFormat.GR_GL_RGBA8
-        )
-        val surface = Surface.makeFromBackendRenderTarget(
-            context,
-            renderTarget,
-            SurfaceOrigin.BOTTOM_LEFT,
-            SurfaceColorFormat.RGBA_8888,
-            ColorSpace.sRGB,
-            SurfaceProps(pixelGeometry = PixelGeometry.UNKNOWN)
-        ) ?: throw RenderException("Cannot create surface")
-
-        return DrawingSurfaceData(renderTarget, surface, surface.canvas)
+    override fun dispose() {
+        bytesToDraw = ByteArray(0)
+        storage.close()
+        context.close()
+        super.dispose()
     }
 
-    override fun flush(drawingSurfaceData: DrawingSurfaceData, g: Graphics2D) {
-        val surface = drawingSurfaceData.surface ?: error("Surface should be initialized")
+    override fun onRender(g: Graphics2D, width: Int, height: Int, nanoTime: Long) {
+        autoCloseScope {
+            holderPtr = createAndBindFrameBuffer(width, height)
+            val fbId = getFboId(holderPtr!!)
+            val renderTarget = makeGLRenderTarget(
+                width,
+                height,
+                0,
+                8,
+                fbId,
+                FramebufferFormat.GR_GL_RGBA8
+            ).autoClose()
+            val surface = Surface.makeFromBackendRenderTarget(
+                context,
+                renderTarget,
+                SurfaceOrigin.TOP_LEFT,
+                SurfaceColorFormat.BGRA_8888,
+                ColorSpace.sRGB,
+                SurfaceProps(pixelGeometry = PixelGeometry.UNKNOWN)
+            )?.autoClose() ?: throw RenderException("Cannot create surface")
+
+            val canvas = surface.canvas
+            canvas.clear(Color.TRANSPARENT)
+            skikoView.onRender(canvas, width, height, nanoTime)
+            flush(surface, g)
+        }
+        finishRendering(holderPtr!!)
+        disposeTexture(holderPtr!!)
+        holderPtr = null
+    }
+
+    private fun flush(surface: Surface, g: Graphics2D) {
         surface.flushAndSubmit(syncCpu = true)
 
         val width = surface.width
         val height = surface.height
 
-        val storage = Bitmap()
-        storage.setImageInfo(ImageInfo.makeN32Premul(width, height))
-        storage.allocPixels()
+        val dstRowBytes = width * 4
+        if (storage.width != width || storage.height != height) {
+            storage.allocPixelsFlags(ImageInfo.makeS32(width, height, ColorAlphaType.PREMUL), false)
+            bytesToDraw = ByteArray(storage.getReadPixelsArraySize(dstRowBytes = dstRowBytes))
+        }
         // TODO: it copies pixels from GPU to CPU, so it is really slow
         surface.readPixels(storage, 0, 0)
 
-        val bytes = storage.readPixels(storage.imageInfo, (width * 4), 0, 0)
-        if (bytes != null) {
-            swingOffscreenDrawer.draw(g, bytes, width, height)
+        val successfulRead = storage.readPixels(bytesToDraw, dstRowBytes = dstRowBytes)
+        if (successfulRead) {
+            swingOffscreenDrawer.draw(g, bytesToDraw, width, height)
         }
-        disposeTexture(holderPtr!!)
     }
 
 
     private external fun createAndBindFrameBuffer(width: Int, height: Int): Long
     private external fun getFboId(holderPtr: Long): Int
     private external fun disposeTexture(holderPtr: Long)
+    private external fun finishRendering(holderPtr: Long)
 }
