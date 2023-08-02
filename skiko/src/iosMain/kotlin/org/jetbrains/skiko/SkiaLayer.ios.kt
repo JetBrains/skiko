09db86c0f61b63971fc6c3a208a1d33ec707e0ac
Elijah Semyonov <elijah.semyonov@jetbrains.com>
2023-08-02 14:56:38 +0200

    Refactor SkikoUIView and Metal interaction

--- skiko/src/iosMain/kotlin/org/jetbrains/skiko/SkiaLayer.ios.kt
+++ skiko/src/iosMain/kotlin/org/jetbrains/skiko/SkiaLayer.ios.kt
@@ -3,45 +3,30 @@ package org.jetbrains.skiko
 import kotlinx.cinterop.useContents
 import org.jetbrains.skia.Canvas
 import org.jetbrains.skia.PixelGeometry
-import org.jetbrains.skiko.context.MetalContextHandler
-import org.jetbrains.skiko.redrawer.MetalRedrawer
+import org.jetbrains.skia.Surface
 import platform.UIKit.*
 import kotlin.system.getTimeNanos
-import org.jetbrains.skia.*
 
 actual open class SkiaLayer {
-
-    fun isShowing(): Boolean {
-        return true
-    }
-
-    fun showScreenKeyboard() {
-        view?.becomeFirstResponder()
-    }
-
-    fun hideScreenKeyboard() { view?.resignFirstResponder() }
-
-    fun isScreenKeyboardOpen(): Boolean {
-        return if (view == null) false else view!!.isFirstResponder
-    }
+    internal var needRedrawCallback: () -> Unit = {}
 
     actual var renderApi: GraphicsApi
         get() = GraphicsApi.METAL
-        set(value) { throw UnsupportedOperationException() }
+        set(_) { throw UnsupportedOperationException() }
 
     actual val contentScale: Float
         get() = view!!.contentScaleFactor.toFloat()
 
     actual var fullscreen: Boolean
         get() = true
-        set(value) { throw UnsupportedOperationException() }
+        set(_) { throw UnsupportedOperationException() }
 
     actual var transparency: Boolean
         get() = false
-        set(value) { throw UnsupportedOperationException() }
+        set(_) { throw UnsupportedOperationException() }
 
     actual fun needRedraw() {
-        redrawer?.needRedraw()
+        needRedrawCallback.invoke()
     }
 
     actual val component: Any?
@@ -57,7 +42,7 @@ actual open class SkiaLayer {
             return@useContents size.height.toFloat()
         }
 
-    internal var view: UIView? = null
+    internal var view: SkikoUIView? = null
     // We need to keep reference to gesturesDetector as Objective-C will only keep weak reference here.
     internal var gesturesDetector = SkikoGesturesDetector(this)
     var gesturesToListen: Array<SkikoGestureEventKind>? = null
@@ -71,42 +56,25 @@ actual open class SkiaLayer {
     }
 
     actual fun attachTo(container: Any) {
-        attachTo(container as UIView)
+        attachTo(container as SkikoUIView)
     }
 
-    fun attachTo(view: UIView) {
+    fun attachTo(view: SkikoUIView) {
         this.view = view
-        contextHandler = MetalContextHandler(this)
-        // TODO: maybe add observer for view.viewDidDisappear() to detach us?
-        redrawer = MetalRedrawer(this).apply {
-            needRedraw()
-        }
     }
 
-    private var isDisposed = false
     actual fun detach() {
-        if (!isDisposed) {
-            redrawer?.dispose()
-            redrawer = null
-            contextHandler?.dispose()
-            contextHandler = null
-            isDisposed = true
-        }
+        view?.detach()
     }
+
     actual var skikoView: SkikoView? = null
 
-    internal var redrawer: MetalRedrawer? = null
-    private var contextHandler: MetalContextHandler? = null
-
     internal actual fun draw(canvas: Canvas) {
-        check(!isDisposed) { "SkiaLayer is disposed" }
-        val (w, h) = view!!.frame.useContents {
-            size.width to size.height
-        }
-        val pictureWidth = (w.toFloat() * contentScale).coerceAtLeast(0.0F)
-        val pictureHeight = (h.toFloat() * contentScale).coerceAtLeast(0.0F)
+        throw NotImplementedError("Don't call it")
+    }
 
-        skikoView?.onRender(canvas, pictureWidth.toInt(), pictureHeight.toInt(), getTimeNanos())
+    internal fun draw(surface: Surface) {
+        skikoView?.onRender(surface.canvas, surface.width, surface.height, getTimeNanos())
     }
 
     actual val pixelGeometry: PixelGeometry
--- skiko/src/iosMain/kotlin/org/jetbrains/skiko/redrawer/MetalRedrawer.ios.kt
+++ skiko/src/iosMain/kotlin/org/jetbrains/skiko/redrawer/MetalRedrawer.ios.kt
@@ -1,37 +1,35 @@
 package org.jetbrains.skiko.redrawer
 
 import kotlinx.cinterop.*
-import org.jetbrains.skia.BackendRenderTarget
-import org.jetbrains.skia.DirectContext
-import org.jetbrains.skiko.SkiaLayer
-import org.jetbrains.skiko.context.ContextHandler
-import org.jetbrains.skiko.context.MetalContextHandler
-import platform.CoreGraphics.CGColorCreate
-import platform.CoreGraphics.CGColorSpaceCreateDeviceRGB
-import platform.CoreGraphics.CGContextRef
-import platform.CoreGraphics.CGSizeMake
+import org.jetbrains.skia.*
 import platform.Foundation.NSRunLoop
 import platform.Foundation.NSSelectorFromString
-import platform.Metal.MTLCreateSystemDefaultDevice
-import platform.Metal.MTLDeviceProtocol
-import platform.Metal.MTLPixelFormatBGRA8Unorm
 import platform.QuartzCore.*
 import platform.darwin.*
+import kotlin.math.roundToInt
 
 internal class MetalRedrawer(
-    private val layer: SkiaLayer
-) : Redrawer {
-    private val contextHandler = MetalContextHandler(layer)
-    override val renderInfo: String get() = contextHandler.rendererInfo()
-
+    private val metalLayer: CAMetalLayer,
+    private val drawIntoSurfaceCallback: (Surface) -> Unit
+) {
     private var isDisposed = false
-    internal val device = MTLCreateSystemDefaultDevice() ?: throw IllegalStateException("Metal is not supported on this system")
+
+    // Workaround for KN compiler bug
+    // Type mismatch: inferred type is objcnames.protocols.MTLDeviceProtocol but platform.Metal.MTLDeviceProtocol was expected
+    @Suppress("USELESS_CAST")
+    private val device = metalLayer.device as platform.Metal.MTLDeviceProtocol?
+        ?: throw IllegalStateException("CAMetalLayer.device can not be null")
     private val queue = device.newCommandQueue() ?: throw IllegalStateException("Couldn't create Metal command queue")
-    private var currentDrawable: CAMetalDrawableProtocol? = null
-    private val metalLayer = MetalLayer()
+    private val context = DirectContext.makeMetal(device.objcPtr(), queue.objcPtr())
 
     // Semaphore for preventing command buffers count more than swapchain size to be scheduled/executed at the same time
     private val inflightSemaphore = dispatch_semaphore_create(metalLayer.maximumDrawableCount.toLong())
+
+    var maximumFramesPerSecond: NSInteger
+        get() = caDisplayLink.preferredFramesPerSecond
+        set(value) {
+            caDisplayLink.preferredFramesPerSecond = value
+        }
 
     /*
      * Indicates that scene is invalidated and next display link callback will draw
@@ -55,7 +53,7 @@ internal class MetalRedrawer(
         if (hasScheduledDrawOnNextVSync) {
             hasScheduledDrawOnNextVSync = false
 
-            drawIfLayerIsShowing()
+            draw()
         }
 
         if (!needsProactiveDisplayLink) {
@@ -67,47 +65,20 @@ internal class MetalRedrawer(
         target = frameListener,
         selector = NSSelectorFromString(FrameTickListener::onDisplayLinkTick.name)
     )
+
     init {
-        metalLayer.init(this.layer, contextHandler, device)
         caDisplayLink.setPaused(true)
         caDisplayLink.addToRunLoop(NSRunLoop.mainRunLoop, NSRunLoop.mainRunLoop.currentMode)
     }
 
-    fun makeContext() = DirectContext.makeMetal(device.objcPtr(), queue.objcPtr())
-
-    fun makeRenderTarget(width: Int, height: Int): BackendRenderTarget {
-        // If more than swapchain size count of command buffers are inflight
-        // wait until one finishes work
-        dispatch_semaphore_wait(inflightSemaphore, DISPATCH_TIME_FOREVER)
-        currentDrawable = metalLayer.nextDrawable()!!
-        return BackendRenderTarget.makeMetal(width, height, currentDrawable!!.texture.objcPtr())
-    }
-
-    override fun dispose() {
+    internal fun dispose() {
         if (!isDisposed) {
             caDisplayLink.invalidate()
-            contextHandler.dispose()
-            metalLayer.dispose()
             isDisposed = true
         }
     }
 
-    override fun syncSize() {
-        metalLayer.contentsScale = layer.contentScale.toDouble()
-        val osView = layer.view!!
-        val (w, h) = osView.frame.useContents {
-            size.width to size.height
-        }
-        metalLayer.frame = osView.frame
-        metalLayer.init(layer, contextHandler, device)
-        metalLayer.drawableSize = CGSizeMake(w * metalLayer.contentsScale, h * metalLayer.contentsScale)
-
-        osView.window?.screen?.maximumFramesPerSecond?.let {
-            caDisplayLink.preferredFramesPerSecond = it
-        }
-    }
-
-    override fun needRedraw() {
+    internal fun needRedraw() {
         check(!isDisposed) { "MetalRedrawer is disposed" }
 
         hasScheduledDrawOnNextVSync = true
@@ -116,86 +87,64 @@ internal class MetalRedrawer(
         caDisplayLink.setPaused(false)
     }
 
-    override fun redrawImmediately() {
-        // TODO: separate iOS MetalRedrawer from Redrawer, it's a false abstraction, iOS only uses MetalRedrawer explicitly
-        throw UnsupportedOperationException("This should never be called on iOS")
-    }
-
-    private fun drawIfLayerIsShowing() {
-        if (layer.isShowing()) {
-            draw()
-        }
-    }
-
     private fun draw() {
-        // TODO: maybe make flush async as in JVM version.
-        autoreleasepool { //todo measure performance without autoreleasepool
-            if (!isDisposed) {
-                contextHandler.draw()
-            }
+        if (isDisposed) {
+            return
         }
-    }
 
-    fun finishFrame() {
         autoreleasepool {
-            currentDrawable?.let {
-                val commandBuffer = queue.commandBuffer()!!
-                commandBuffer.label = "Present"
-                commandBuffer.presentDrawable(it)
-                commandBuffer.addCompletedHandler {
-                    // Signal work finish, allow a new command buffer to be scheduled
-                    dispatch_semaphore_signal(inflightSemaphore)
-                }
-                commandBuffer.commit()
-                currentDrawable = null
+            val (width, height) = metalLayer.drawableSize.useContents {
+                width.roundToInt() to height.roundToInt()
             }
+
+            if (width <= 0 || height <= 0) {
+                return@autoreleasepool
+            }
+
+            dispatch_semaphore_wait(inflightSemaphore, DISPATCH_TIME_FOREVER)
+
+            val metalDrawable = metalLayer.nextDrawable()
+
+            if (metalDrawable == null) {
+                dispatch_semaphore_signal(inflightSemaphore)
+                return@autoreleasepool
+            }
+
+            val renderTarget = BackendRenderTarget.makeMetal(width, height, metalDrawable.texture.objcPtr())
+
+            val surface = Surface.makeFromBackendRenderTarget(
+                context,
+                renderTarget,
+                SurfaceOrigin.TOP_LEFT,
+                SurfaceColorFormat.BGRA_8888,
+                ColorSpace.sRGB,
+                SurfaceProps(pixelGeometry = PixelGeometry.UNKNOWN)
+            )
+
+            if (surface == null) {
+                renderTarget.close()
+                // TODO: manually release metalDrawable when K/N API arrives
+                dispatch_semaphore_signal(inflightSemaphore)
+                return@autoreleasepool
+            }
+
+            surface.canvas.clear(Color.WHITE)
+            drawIntoSurfaceCallback(surface)
+            surface.flushAndSubmit()
+
+            val commandBuffer = queue.commandBuffer()!!
+            commandBuffer.label = "Present"
+            commandBuffer.presentDrawable(metalDrawable)
+            commandBuffer.addCompletedHandler {
+                // Signal work finish, allow a new command buffer to be scheduled
+                dispatch_semaphore_signal(inflightSemaphore)
+            }
+            commandBuffer.commit()
+
+            surface.close()
+            renderTarget.close()
+            // TODO manually release metalDrawable when K/N API arrives
         }
-    }
-}
-
-internal class MetalLayer : CAMetalLayer {
-    private lateinit var skiaLayer: SkiaLayer
-    private lateinit var contextHandler: ContextHandler
-
-    @OverrideInit
-    constructor() : super()
-
-    @OverrideInit
-    constructor(layer: Any) : super(layer)
-
-    fun init(
-        skiaLayer: SkiaLayer,
-        contextHandler: ContextHandler,
-        theDevice: MTLDeviceProtocol
-    ) {
-        this.skiaLayer = skiaLayer
-        this.contextHandler = contextHandler
-        this.setNeedsDisplayOnBoundsChange(true)
-        this.removeAllAnimations()
-        // TODO: looks like a bug in K/N interop.
-        this.device = theDevice as objcnames.protocols.MTLDeviceProtocol?
-        this.pixelFormat = MTLPixelFormatBGRA8Unorm
-        this.contentsGravity = kCAGravityTopLeft
-        doubleArrayOf(0.0, 0.0, 0.0, 0.0).usePinned {
-            this.backgroundColor =
-                CGColorCreate(CGColorSpaceCreateDeviceRGB(), it.addressOf(0))
-        }
-        this.framebufferOnly = false
-        this.opaque = false // For UIKit interop through a "Hole"
-        skiaLayer.view?.let {
-            this.frame = it.frame
-            it.layer.addSublayer(this)
-        }
-    }
-
-    fun dispose() {
-        this.removeFromSuperlayer()
-        // TODO: anything else to dispose the layer?
-    }
-
-    override fun drawInContext(ctx: CGContextRef?) {
-        contextHandler.draw()
-        super.drawInContext(ctx)
     }
 }
 
