163aec11263fecbcf358183f81aebe4f71d82b45
Elijah Semyonov <elijah.semenov@gmail.com>
2023-08-03 09:24:02 +0200

    Refactoring pass for iOS counterpart of Skiko (#781)
    * Refactor SkikoUIView and Metal interaction
    
    * Move load logic to init
    
    * Remove dead code
    
    * Use contentScaleFactor for calculating drawable size
    
    * Move UITouch.isPressed to filescope
    
    * Add trace logs for anomaly situations

--- skiko/src/iosMain/kotlin/org/jetbrains/skiko/SkiaLayer.ios.kt
+++ skiko/src/iosMain/kotlin/org/jetbrains/skiko/SkiaLayer.ios.kt
@@ -3,45 +3,30 @@ package org.jetbrains.skiko
 import kotlinx.cinterop.useContents
 import org.jetbrains.skia.Canvas
 import org.jetbrains.skia.PixelGeometry
-import org.jetbrains.skiko.context.MetalContextHandler
-import org.jetbrains.skiko.redrawer.MetalRedrawer
+import org.jetbrains.skia.Surface
 import platform.UIKit.*
 import kotlin.system.getTimeNanos
-import org.jetbrains.skia.*
 
 actual open class SkiaLayer {
-
-    fun isShowing(): Boolean {
-        return true
-    }
-
-    fun showScreenKeyboard() {
-        view?.becomeFirstResponder()
-    }
-
-    fun hideScreenKeyboard() { view?.resignFirstResponder() }
-
-    fun isScreenKeyboardOpen(): Boolean {
-        return if (view == null) false else view!!.isFirstResponder
-    }
+    internal var needRedrawCallback: () -> Unit = {}
 
     actual var renderApi: GraphicsApi
         get() = GraphicsApi.METAL
-        set(value) { throw UnsupportedOperationException() }
+        set(_) { throw UnsupportedOperationException() }
 
     actual val contentScale: Float
         get() = view!!.contentScaleFactor.toFloat()
 
     actual var fullscreen: Boolean
         get() = true
-        set(value) { throw UnsupportedOperationException() }
+        set(_) { throw UnsupportedOperationException() }
 
     actual var transparency: Boolean
         get() = false
-        set(value) { throw UnsupportedOperationException() }
+        set(_) { throw UnsupportedOperationException() }
 
     actual fun needRedraw() {
-        redrawer?.needRedraw()
+        needRedrawCallback.invoke()
     }
 
     actual val component: Any?
@@ -57,56 +42,24 @@ actual open class SkiaLayer {
             return@useContents size.height.toFloat()
         }
 
-    internal var view: UIView? = null
-    // We need to keep reference to gesturesDetector as Objective-C will only keep weak reference here.
-    internal var gesturesDetector = SkikoGesturesDetector(this)
-    var gesturesToListen: Array<SkikoGestureEventKind>? = null
-        set(value) {
-            field = value
-            initGestures()
-        }
-
-    internal fun initGestures() {
-        gesturesDetector.setGesturesToListen(gesturesToListen)
-    }
+    internal var view: SkikoUIView? = null
 
     actual fun attachTo(container: Any) {
-        attachTo(container as UIView)
+        view = container as SkikoUIView
     }
 
-    fun attachTo(view: UIView) {
-        this.view = view
-        contextHandler = MetalContextHandler(this)
-        // TODO: maybe add observer for view.viewDidDisappear() to detach us?
-        redrawer = MetalRedrawer(this).apply {
-            needRedraw()
-        }
-    }
-
-    private var isDisposed = false
     actual fun detach() {
-        if (!isDisposed) {
-            redrawer?.dispose()
-            redrawer = null
-            contextHandler?.dispose()
-            contextHandler = null
-            isDisposed = true
-        }
+        view?.detach()
     }
+
     actual var skikoView: SkikoView? = null
 
-    internal var redrawer: MetalRedrawer? = null
-    private var contextHandler: MetalContextHandler? = null
-
     internal actual fun draw(canvas: Canvas) {
-        check(!isDisposed) { "SkiaLayer is disposed" }
-        val (w, h) = view!!.frame.useContents {
-            size.width to size.height
-        }
-        val pictureWidth = (w.toFloat() * contentScale).coerceAtLeast(0.0F)
-        val pictureHeight = (h.toFloat() * contentScale).coerceAtLeast(0.0F)
+        throw UnsupportedOperationException("Don't call it, artifact of wrong abstraction")
+    }
 
-        skikoView?.onRender(canvas, pictureWidth.toInt(), pictureHeight.toInt(), getTimeNanos())
+    internal fun draw(surface: Surface) {
+        skikoView?.onRender(surface.canvas, surface.width, surface.height, getTimeNanos())
     }
 
     actual val pixelGeometry: PixelGeometry
