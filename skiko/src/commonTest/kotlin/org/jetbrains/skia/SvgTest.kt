d9ab6e501f5eadce0bd926c71fed316344e7db8d
Oleksandr Karpovich <a.n.karpovich@gmail.com>
2023-09-27 14:27:25 +0200

    Fix SVGLength.withUnit: create a new SVGLength with a `_unit` from the input and not `this.unit` (#804)

--- skiko/src/commonTest/kotlin/org/jetbrains/skia/SvgTest.kt
+++ skiko/src/commonTest/kotlin/org/jetbrains/skia/SvgTest.kt
@@ -38,11 +38,28 @@ class SvgTest {
         require(e.tag == SVGTag.SVG)
         e.viewBox = Rect(0f, 1f, 100f, 200f)
         assertCloseEnough(Rect(0f, 1f, 100f, 200f), e.viewBox!!)
-        val aspectRatio = SVGPreserveAspectRatio(SVGPreserveAspectRatioAlign.XMIN_YMIN, SVGPreserveAspectRatioScale.MEET)
+        val aspectRatio =
+            SVGPreserveAspectRatio(SVGPreserveAspectRatioAlign.XMIN_YMIN, SVGPreserveAspectRatioScale.MEET)
         e.preserveAspectRatio = aspectRatio
         assertEquals(aspectRatio, e.preserveAspectRatio)
         require(e.getIntrinsicSize(SVGLengthContext(100f, 100f)).x == 300f)
         e.viewBox = Rect.makeXYWH(0f, 1f, 2f, 3f)
         require(e.viewBox == Rect.makeXYWH(0f, 1f, 2f, 3f))
+    }
+
+    @Test
+    fun SvgLength() {
+        val l = SVGLength(123f)
+
+        assertEquals(123f, l.value)
+        assertEquals(SVGLengthUnit.NUMBER, l.unit)
+
+        val lpx = l.withUnit(SVGLengthUnit.PX)
+        assertEquals(123f, lpx.value)
+        assertEquals(SVGLengthUnit.PX, lpx.unit)
+
+        val newLpx = lpx.withValue(321f)
+        assertEquals(321f, newLpx.value)
+        assertEquals(SVGLengthUnit.PX, newLpx.unit)
     }
 }
