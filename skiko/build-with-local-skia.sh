0db4e1ed008df52dc500718d1c70b2b8b22edab4
dima.avdeev <dima.avdeev@jetbrains.com>
2023-11-13 09:30:13 +0400

    Added ability to build macos jvm

--- skiko/build-with-local-skia.sh
+++ skiko/build-with-local-skia.sh
@@ -1,22 +1,67 @@
 #!/usr/bin/env bash
-# Variables
-export VERSION=m116-51072f3-1
-export DEBUG_MODE=false
-export TARGET="iosSim"
-export SKIKO_TARGET_FLAGS="-Pskiko.native.ios.simulatorArm64.enabled=true -Pskiko.awt.enabled=false"
+###### Variables you can edit to change build config ######
+VERSION="m116-51072f3-1" # Version of Skia m###-commit-sha-#. This commit sha will be cloned from repository https://github.com/JetBrains/skia
+DEBUG_MODE="false"
+TARGET="wasm" # possible values: "ios", "iosSim", "macos", "windows", "linux", "wasm", "android", "tvos", "tvosSim"
+SKIKO_TARGET_FLAGS="-Pskiko.native.ios.simulatorArm64.enabled=true -Pskiko.awt.enabled=false"
+###########################################################
 
-# Values
 if [[ $DEBUG_MODE == "true" ]]; then
   skikoBuildType=Debug
 else
   skikoBuildType=Release
 fi
 
-if [[ $(uname -m) == 'arm64' ]]; then
-  skikoMachine="arm64"
-else
-  skikoMachine="x86_64"
-fi
+case $TARGET in
+  "ios")
+    SKIKO_TARGET_FLAGS="-Pskiko.native.ios.arm64.enabled=true -Pskiko.awt.enabled=false"
+    skikoMachines=("arm64")
+    ;;
+  "iosSim")
+    if [[ $(uname -m) == 'arm64' ]]; then
+      SKIKO_TARGET_FLAGS="-Pskiko.native.ios.simulatorArm64.enabled=true -Pskiko.awt.enabled=false"
+      skikoMachines=("arm64")
+    else
+      SKIKO_TARGET_FLAGS="-Pskiko.native.ios.x64.enabled=true -Pskiko.awt.enabled=false"
+      skikoMachines=("x64")
+    fi
+    ;;
+  "macos")
+    SKIKO_TARGET_FLAGS="-Pskiko.awt.enabled=true"
+    if [[ $(uname -m) == 'arm64' ]]; then
+      skikoMachines=("arm64", "x64")
+    else
+      skikoMachines=("x64")
+    fi
+    ;;
+  "windows")
+    SKIKO_TARGET_FLAGS="-Pskiko.awt.enabled=true"
+    if [[ $(uname -m) == 'arm64' ]]; then
+      skikoMachines=("arm64")
+    else
+      skikoMachines=("x64")
+    fi
+    ;;
+  "linux")
+    SKIKO_TARGET_FLAGS="-Pskiko.awt.enabled=true"
+    if [[ $(uname -m) == 'arm64' ]]; then
+      skikoMachines=("arm64")
+    else
+      skikoMachines=("x64")
+    fi
+    ;;
+  "wasm")
+    SKIKO_TARGET_FLAGS="-Pskiko.wasm.enabled=true -Pskiko.awt.enabled=false"
+    if [[ $(uname -m) == 'arm64' ]]; then
+      skikoMachines=("arm64")
+    else
+      skikoMachines=("x64")
+    fi
+    ;;
+  *)
+    echo "can't determine skia target"; exit 1
+    ;;
+esac
 
 set -e # fail fast
 set -x # print all commands
@@ -25,8 +70,10 @@ cd "$(dirname "$0")"
 git clone https://github.com/JetBrains/skia-pack.git || echo "skia-pack exists. You can remove it or update by hands with git pull"
 cd skia-pack
 python3 script/checkout.py --version "$VERSION" --reset=False
-python3 script/build.py --target "$TARGET" --machine "$skikoMachine" --build-type $skikoBuildType
-python3 script/archive.py --version "$VERSION" --target "$TARGET" --machine "$skikoMachine" --build-type $skikoBuildType
+for skikoMachine in ${skikoMachines[@]}; do
+  python3 script/build.py --target "$TARGET" --machine "$skikoMachine" --build-type $skikoBuildType
+  python3 script/archive.py --version "$VERSION" --target "$TARGET" --machine "$skikoMachine" --build-type $skikoBuildType
+done
 cd "$(dirname "$0")"
 
 rm -rf build/classes/kotlin/* # We need to drop old cache. We can do it with ./gradlew clean as well, but it tooks longer time to redownload dependencies dir.
