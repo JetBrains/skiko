8763dbf1f8bc7e237c9e186a41ac94522f56c9a2
dmitriy tukhvatullin <tukhvatullin01@gmail.com>
2023-12-22 21:03:03 +0300

    add support for touchscreen events in js for mobile devices

--- skiko/src/jsMain/kotlin/org/jetbrains/skiko/SkiaLayer.js.kt
+++ skiko/src/jsMain/kotlin/org/jetbrains/skiko/SkiaLayer.js.kt
@@ -2,15 +2,45 @@ package org.jetbrains.skiko
 
 import kotlinx.browser.window
 import org.jetbrains.skiko.w3c.HTMLCanvasElement
+import org.w3c.dom.TouchEvent
 import org.w3c.dom.events.KeyboardEvent
 import org.w3c.dom.events.MouseEvent
 import org.w3c.dom.events.WheelEvent
+import org.w3c.dom.events.UIEvent
 
 internal actual fun SkiaLayer.bindCanvasEventsToSkikoView(
     canvas: HTMLCanvasElement
 ) {
     val htmlCanvas = canvas.unsafeCast<org.w3c.dom.HTMLCanvasElement>()
+    var offsetX = 0.0
+    var offsetY = 0.0
+    htmlCanvas.addEventListener("touchstart", { event ->
+        event.preventDefault()
+        event as TouchEvent
+        htmlCanvas.getBoundingClientRect().apply {
+            offsetX = left
+            offsetY = top
+        }
+        skikoView?.onPointerEvent(toSkikoEvent(event, SkikoPointerEventKind.DOWN, offsetX, offsetY))
+    })
 
+    htmlCanvas.addEventListener("touchmove", { event ->
+        event.preventDefault()
+        event as TouchEvent
+        skikoView?.onPointerEvent(toSkikoEvent(event, SkikoPointerEventKind.MOVE, offsetX, offsetY))
+    })
+
+    htmlCanvas.addEventListener("touchend", { event ->
+        event.preventDefault()
+        event as TouchEvent
+        skikoView?.onPointerEvent(toSkikoEvent(event, SkikoPointerEventKind.UP, offsetX, offsetY))
+    })
+
+    htmlCanvas.addEventListener("touchcancel", { event ->
+        event.preventDefault()
+        event as TouchEvent
+        skikoView?.onPointerEvent(toSkikoEvent(event, SkikoPointerEventKind.UP, offsetX, offsetY))
+    })
     htmlCanvas.addEventListener("mousedown", { event ->
         event as MouseEvent
         isPointerPressed = true
@@ -54,7 +84,7 @@ actual typealias SkikoGesturePlatformEvent = Any
 actual typealias SkikoPlatformInputEvent = KeyboardEvent
 actual typealias SkikoPlatformKeyboardEvent = KeyboardEvent
 //  MouseEvent is base class of PointerEvent
-actual typealias SkikoPlatformPointerEvent = MouseEvent
+actual typealias SkikoPlatformPointerEvent = UIEvent
 
 internal actual fun SkiaLayer.setOnChangeScaleNotifier() {
     state?.initCanvas(desiredWidth, desiredHeight, contentScale, this.pixelGeometry)
