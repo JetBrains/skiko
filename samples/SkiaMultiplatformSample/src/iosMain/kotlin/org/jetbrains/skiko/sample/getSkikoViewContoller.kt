fd9a62d559cb59d0fad5c3e0484b2b6db938a041
Elijah Semyonov <elijah.semenov@gmail.com>
2024-01-08 15:44:48 +0100

    Fix iOS sample (#854)
    * Add simple wrapper
    
    * Start redraw loop

--- samples/SkiaMultiplatformSample/src/iosMain/kotlin/org/jetbrains/skiko/sample/getSkikoViewContoller.kt
+++ samples/SkiaMultiplatformSample/src/iosMain/kotlin/org/jetbrains/skiko/sample/getSkikoViewContoller.kt
@@ -5,17 +5,33 @@ import org.jetbrains.skiko.*
 import org.jetbrains.skiko.sample.*
 import platform.Foundation.*
 import platform.UIKit.*
+import platform.darwin.dispatch_async
+import platform.darwin.dispatch_get_main_queue
 
 fun makeApp(skiaLayer: SkiaLayer) = IosClocks(skiaLayer)
 
 fun getSkikoViewContoller(): UIViewController {
     val view = SkikoUIView(
         SkiaLayer().apply {
-            gesturesToListen = SkikoGestureEventKind.values()
             skikoView = GenericSkikoView(this, makeApp(this))
+
+            dispatch_async(dispatch_get_main_queue()) {
+                needRedraw()
+            }
         }
     )
-    //view.currentKeyboardType = UIKeyboardTypePhonePad
-    //view.currentReturnKeyType = UIReturnKeyType.UIReturnKeyDone
-    return SkikoViewController(view)
+
+    view.translatesAutoresizingMaskIntoConstraints = false
+
+    val viewController = UIViewController()
+    viewController.view.addSubview(view)
+
+    NSLayoutConstraint.activateConstraints(listOf(
+        view.topAnchor.constraintEqualToAnchor(viewController.view.topAnchor),
+        view.bottomAnchor.constraintEqualToAnchor(viewController.view.bottomAnchor),
+        view.leadingAnchor.constraintEqualToAnchor(viewController.view.leadingAnchor),
+        view.trailingAnchor.constraintEqualToAnchor(viewController.view.trailingAnchor)
+    ))
+
+    return viewController
 }
