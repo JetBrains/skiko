2a8e25d85edfdf1e2d1338e3599d61544cb5f37d
Ivan Matkov <ivan.matkov@jetbrains.com>
2024-02-07 15:10:58 +0100

    Fix library load on SystemTheme (#868)

--- skiko/src/awtMain/kotlin/org/jetbrains/skiko/SystemTheme.awt.kt
+++ skiko/src/awtMain/kotlin/org/jetbrains/skiko/SystemTheme.awt.kt
@@ -1,14 +1,16 @@
 package org.jetbrains.skiko
 
 actual val currentSystemTheme: SystemTheme
-    get() {
-        Library.load()
-        return when (getCurrentSystemTheme()) {
-            0 -> SystemTheme.LIGHT
-            1 -> SystemTheme.DARK
-            else -> SystemTheme.UNKNOWN
-        }
+    get() = when (SystemThemeHelper.getCurrentSystemTheme()) {
+        0 -> SystemTheme.LIGHT
+        1 -> SystemTheme.DARK
+        else -> SystemTheme.UNKNOWN
     }
 
-// Common
-private external fun getCurrentSystemTheme(): Int
+private object SystemThemeHelper {
+    init {
+        Library.load()
+    }
+
+    external fun getCurrentSystemTheme(): Int
+}
