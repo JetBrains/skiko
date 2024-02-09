JetBrains/skiko

commit 2fac6861778aab7d51eab1cdf5bf4c33094adb29

Fix crash on Windows machines without OpenGL (#811)
* Fix crash on Windows machines without OpenGL

Some Windows CI machines don't contain OpenGL, so Skiko/Compose will crash on them. It shouldn't crash, it should fallback to Software rendering

To properly fallback, we need to load opengl32.dll dynamically in runtime.

Fixes https://github.com/JetBrains/compose-multiplatform/issues/3243

# Test 1
- `gradlew publishToMavenLocal` is successful (if we use functions directly, it fails with "error LNK2019: unresolved external symbol")
- `SkiaLayerTest` is successful (it tests OpenGL as well)

# Test 2
1. Remove `C:\Windows\System32\opengl32.dll`
2. `SkiaLayerTest` still should be successful, but logs should contain that it fallbacks to Software rendering.

# Test 3
1. Compile local skiko
2. Run Compose run1 and tests

* Refactor

* Discussions

* Discussions

* Split throwJavaRenderException

It isn't valid to use FormatMessage for code from GetLastError

skiko/src/awtMain/cpp/windows/OpenGLLibrary.cc
@@ -1,0 +1,64 @@
+#include <windows.h>
+#include <gl/GL.h>
+#include <jawt_md.h>
+#include "exceptions_handler.h"
+
+static HINSTANCE OpenGL32Library = nullptr;
+
+extern "C" {
+    void glFinish(void) {
+        typedef void (*PROC_glFinish) (void);
+        static auto glFinish = (PROC_glFinish) GetProcAddress(OpenGL32Library, "glFinish");
+        glFinish();
+    }
+
+    void glGetIntegerv(GLenum pname, GLint *data) {
+        typedef void (*PROC_glGetIntegerv) (GLenum pname, GLint *data);
+        static auto glGetIntegerv = (PROC_glGetIntegerv) GetProcAddress(OpenGL32Library, "glGetIntegerv");
+        glGetIntegerv(pname, data);
+    }
+
+    const GLubyte * glGetString(GLenum name) {
+        typedef const GLubyte *(*PROC_glGetString) (GLenum name);
+        static auto glGetString = (PROC_glGetString) GetProcAddress(OpenGL32Library, "glGetString");
+        return glGetString(name);
+    }
+
+    HGLRC WINAPI wglCreateContext(HDC hDc) {
+        typedef HGLRC (WINAPI * PROC_wglCreateContext) (HDC hDc);
+        static auto wglCreateContext = (PROC_wglCreateContext) GetProcAddress(OpenGL32Library, "wglCreateContext");
+        return wglCreateContext(hDc);
+    }
+
+    BOOL WINAPI wglDeleteContext(HGLRC oldContext) {
+        typedef BOOL (WINAPI * PROC_wglDeleteContext) (HGLRC oldContext);
+        static auto wglDeleteContext = (PROC_wglDeleteContext) GetProcAddress(OpenGL32Library, "wglDeleteContext");
+        return wglDeleteContext(oldContext);
+    }
+
+    PROC WINAPI wglGetProcAddress(LPCSTR lpszProc) {
+        typedef PROC (WINAPI * PROC_wglGetProcAddress) (LPCSTR lpszProc);
+        static auto wglGetProcAddress = (PROC_wglGetProcAddress) GetProcAddress(OpenGL32Library, "wglGetProcAddress");
+        return wglGetProcAddress(lpszProc);
+    }
+
+    BOOL WINAPI wglMakeCurrent(HDC hDc, HGLRC newContext) {
+        typedef BOOL (WINAPI * PROC_wglMakeCurrent) (HDC hDc, HGLRC newContext);
+        static auto wglMakeCurrent = (PROC_wglMakeCurrent) GetProcAddress(OpenGL32Library, "wglMakeCurrent");
+        return wglMakeCurrent(hDc, newContext);
+    }
+
+    HGLRC WINAPI wglGetCurrentContext() {
+        typedef HGLRC (WINAPI * PROC_wglGetCurrentContext) (void);
+        static auto wglGetCurrentContext = (PROC_wglGetCurrentContext) GetProcAddress(OpenGL32Library, "wglGetCurrentContext");
+        return wglGetCurrentContext();
+    }
+
+    JNIEXPORT void JNICALL Java_org_jetbrains_skiko_OpenGLLibraryKt_loadOpenGLLibraryWindows(JNIEnv *env, jobject obj) {
+        OpenGL32Library = LoadLibrary("opengl32.dll");
+        if (OpenGL32Library == nullptr) {
+            auto code = GetLastError();
+            throwJavaRenderExceptionByErrorCode(env, __FUNCTION__, code);
+        }
+    }
+}

skiko/src/jvmMain/kotlin/org/jetbrains/skiko/OpenGLLibrary.kt
@@ -1,0 +1,23 @@
+package org.jetbrains.skiko
+
+/**
+ * Load OpenGL library into memory.
+ *
+ * Should be called before any OpenGL operation.
+ *
+ * The current implementation loads OpenGl lazily on Windows, and at app startup on Linux/macOs.
+ *
+ * Some Windows machines don't have OpenGL (usually CI machines), so we'll fallback to other renderers on them.
+ *
+ * Throws [RenderException] if OpenGL library can't be loaded.
+ */
+internal fun loadOpenGLLibrary() {
+    if (hostOs.isWindows) {
+        loadOpenGLLibraryWindows()
+    } else {
+        // do nothing, the library should be already available
+    }
+}
+
+@Synchronized
+private external fun loadOpenGLLibraryWindows()

skiko/build.gradle.kts
@@ -1036,12 +1036,18 @@ fun createLinkJvmBindings(
                 addAll(buildType.msvcLinkerFlags)
                 addAll(
                     arrayOf(
+                        // ignore https://learn.microsoft.com/en-us/cpp/error-messages/tool-errors/linker-tools-warning-lnk4217
+                        // because we link OpenGl dynamically, defining functions in our own file in OpenGLLibrary.cc
+                        "/ignore:4217"
+                    )
+                )
+                addAll(
+                    arrayOf(
                         "/NOLOGO",
                         "/DLL",
                         "Advapi32.lib",
                         "gdi32.lib",
                         "Dwmapi.lib",
-                        "opengl32.lib",
                         "shcore.lib",
                         "user32.lib",
                     )

skiko/src/awtMain/cpp/include/exceptions_handler.h
@@ -4,6 +4,7 @@
 #include <windows.h>
 #include <stdio.h>
 
-void throwJavaException(JNIEnv *env, const char * function, DWORD sehCode);
+void throwJavaRenderExceptionByExceptionCode(JNIEnv *env, const char * function, DWORD code);
+void throwJavaRenderExceptionByErrorCode(JNIEnv *env, const char * function, DWORD code);
 
 #endif

skiko/src/awtMain/cpp/windows/SoftwareRedrawer.cc
@@ -64,7 +64,7 @@ extern "C"
         }
         __except(EXCEPTION_EXECUTE_HANDLER) {
             auto code = GetExceptionCode();
-            throwJavaException(env, __FUNCTION__, code);
+            throwJavaRenderExceptionByExceptionCode(env, __FUNCTION__, code);
         }
     }
 
@@ -91,7 +91,7 @@ extern "C"
         }
         __except(EXCEPTION_EXECUTE_HANDLER) {
             auto code = GetExceptionCode();
-            throwJavaException(env, __FUNCTION__, code);
+            throwJavaRenderExceptionByExceptionCode(env, __FUNCTION__, code);
         }
     }
 

skiko/src/awtMain/cpp/windows/direct3DContext.cc
@@ -25,7 +25,7 @@ extern "C"
         }
         __except(EXCEPTION_EXECUTE_HANDLER) {
             auto code = GetExceptionCode();
-            throwJavaException(env, __FUNCTION__, code);
+            throwJavaRenderExceptionByExceptionCode(env, __FUNCTION__, code);
         }
     }
 }

skiko/src/awtMain/cpp/windows/directXRedrawer.cc
@@ -321,7 +321,7 @@ extern "C"
         }
         __except(EXCEPTION_EXECUTE_HANDLER) {
             auto code = GetExceptionCode();
-            throwJavaException(env, __FUNCTION__, code);
+            throwJavaRenderExceptionByExceptionCode(env, __FUNCTION__, code);
         }
     }
 
@@ -341,7 +341,7 @@ extern "C"
         }
         __except(EXCEPTION_EXECUTE_HANDLER) {
             auto code = GetExceptionCode();
-            throwJavaException(env, __FUNCTION__, code);
+            throwJavaRenderExceptionByExceptionCode(env, __FUNCTION__, code);
         }
     }
 
@@ -396,7 +396,7 @@ extern "C"
         }
         __except(EXCEPTION_EXECUTE_HANDLER) {
             auto code = GetExceptionCode();
-            throwJavaException(env, __FUNCTION__, code);
+            throwJavaRenderExceptionByExceptionCode(env, __FUNCTION__, code);
         }
     }
 
@@ -413,7 +413,7 @@ extern "C"
         }
         __except(EXCEPTION_EXECUTE_HANDLER) {
             auto code = GetExceptionCode();
-            throwJavaException(env, __FUNCTION__, code);
+            throwJavaRenderExceptionByExceptionCode(env, __FUNCTION__, code);
         }
     }
 
@@ -441,7 +441,7 @@ extern "C"
         }
         __except(EXCEPTION_EXECUTE_HANDLER) {
             auto code = GetExceptionCode();
-            throwJavaException(env, __FUNCTION__, code);
+            throwJavaRenderExceptionByExceptionCode(env, __FUNCTION__, code);
         }
     }
 

skiko/src/awtMain/cpp/windows/exceptions_handler.cc
@@ -1,12 +1,8 @@
-#if SK_BUILD_FOR_WIN
-
 #include "exceptions_handler.h"
 #include "../common/interop.hh"
 
-const char *getDescription(DWORD code)
-{
-    switch (code)
-    {
+const char *getDescription(DWORD code) {
+    switch (code) {
     case EXCEPTION_ACCESS_VIOLATION:
         return "EXCEPTION_ACCESS_VIOLATION";
     case EXCEPTION_ARRAY_BOUNDS_EXCEEDED:
@@ -47,19 +43,35 @@ const char *getDescription(DWORD code)
         return "EXCEPTION_SINGLE_STEP";
     case EXCEPTION_STACK_OVERFLOW:
         return "EXCEPTION_STACK_OVERFLOW";
+    case ERROR_MOD_NOT_FOUND:
+        return "ERROR_MOD_NOT_FOUND";
     default:
         return "UNKNOWN EXCEPTION";
     }
 }
 
-void throwJavaException(JNIEnv *env, const char *function, DWORD sehCode)
-{
-    char buffer[200];
-    int result = snprintf(
-        buffer, sizeof(buffer) - 1, "Native exception in [%s]:\nSEH description: %s\n", function, getDescription(sehCode));
-    static jclass logClass = (jclass) env->NewGlobalRef(env->FindClass("org/jetbrains/skiko/RenderExceptionsHandler"));
-    static jmethodID logMethod = env->GetStaticMethodID(logClass, "throwException", "(Ljava/lang/String;)V");
-    env->CallStaticVoidMethod(logClass, logMethod, env->NewStringUTF(buffer));
+void throwJavaRenderExceptionByExceptionCode(JNIEnv *env, const char *function, DWORD code) {
+    char fullMsg[200];
+    int result = snprintf(fullMsg, sizeof(fullMsg) - 1,
+        "Native exception in [%s], code %lu: %s", function, code, getDescription(code));
+
+    static jclass cls = (jclass) env->NewGlobalRef(env->FindClass("org/jetbrains/skiko/RenderExceptionsHandler"));
+    static jmethodID method = env->GetStaticMethodID(cls, "throwException", "(Ljava/lang/String;)V");
+    env->CallStaticVoidMethod(cls, method, env->NewStringUTF(fullMsg));
 }
 
-#endif
+
+void throwJavaRenderExceptionByErrorCode(JNIEnv *env, const char *function, DWORD code) {
+    char fullMsg[1024];
+    char *msg = 0;
+    FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+        NULL, code, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR) &msg, 0, NULL);
+
+    int result = snprintf(fullMsg, sizeof(fullMsg) - 1,
+        "Native exception in [%s], code %lu: %s", function, code, msg);
+    LocalFree(msg);
+
+    static jclass cls = (jclass) env->NewGlobalRef(env->FindClass("org/jetbrains/skiko/RenderExceptionsHandler"));
+    static jmethodID method = env->GetStaticMethodID(cls, "throwException", "(Ljava/lang/String;)V");
+    env->CallStaticVoidMethod(cls, method, env->NewStringUTF(fullMsg));
+}

commit 29d73285f2d97cec8cc4dcb2fbbcaf30e3c3834d

Fix a native crash on `makeGL`
It is a regression after https://github.com/JetBrains/skiko/pull/811

After that PR, OpenGL functions are linked in runtime via function `loadOpenGLLibrary`. We didn't call it in `makeGL` functions. Now we call.

Adding also `isLoaded` to avoid loading it multiple times

Fixes https://youtrack.jetbrains.com/issue/COMPOSE-642/Fix-Skiko-API-regression-for-creating-OpenGL-context

skiko/src/awtMain/cpp/windows/OpenGLLibrary.cc
@@ -54,7 +54,7 @@ extern "C" {
         return wglGetCurrentContext();
     }
 
-    JNIEXPORT void JNICALL Java_org_jetbrains_skiko_OpenGLLibraryKt_loadOpenGLLibraryWindows(JNIEnv *env, jobject obj) {
+    JNIEXPORT void JNICALL Java_org_jetbrains_skiko_OpenGLLibrary_1jvmKt_loadOpenGLLibraryWindows(JNIEnv *env, jobject obj) {
         OpenGL32Library = LoadLibrary("opengl32.dll");
         if (OpenGL32Library == nullptr) {
             auto code = GetLastError();

skiko/src/awtTest/kotlin/org/jetbrains/skiko/util/Threads.kt
@@ -1,0 +1,19 @@
+package org.jetbrains.skiko.util
+
+/**
+ * Runs body in another thread, join it and throws a last exception in this thread if any
+ */
+fun joinThreadCatching(body: () -> Unit) {
+    var exception: Throwable? = null
+    val thread = object : Thread() {
+        override fun run() = body()
+    }
+    thread.setUncaughtExceptionHandler { _, e ->
+        exception = e
+    }
+    thread.start()
+    thread.join()
+    if (exception != null) {
+        throw exception!!
+    }
+}

skiko/src/commonMain/kotlin/org/jetbrains/skia/BackendRenderTarget.kt
@@ -4,6 +4,8 @@ import org.jetbrains.skia.impl.Library.Companion.staticLoad
 import org.jetbrains.skia.impl.Managed
 import org.jetbrains.skia.impl.NativePointer
 import org.jetbrains.skia.impl.Stats
+import org.jetbrains.skiko.RenderException
+import org.jetbrains.skiko.loadOpenGLLibrary
 
 class BackendRenderTarget internal constructor(ptr: NativePointer) : Managed(ptr, _FinalizerHolder.PTR) {
     companion object {
@@ -16,16 +18,10 @@ class BackendRenderTarget internal constructor(ptr: NativePointer) : Managed(ptr
             fbFormat: Int
         ): BackendRenderTarget {
             Stats.onNativeCall()
-            return BackendRenderTarget(
-                _nMakeGL(
-                    width,
-                    height,
-                    sampleCnt,
-                    stencilBits,
-                    fbId,
-                    fbFormat
-                )
-            )
+            loadOpenGLLibrary()
+            val ptr =_nMakeGL(width, height, sampleCnt, stencilBits, fbId, fbFormat)
+            if (ptr == NullPointer) throw RenderException("Can't create OpenGL BackendRenderTarget")
+            return BackendRenderTarget(ptr)
         }
 
         fun makeMetal(width: Int, height: Int, texturePtr: NativePointer): BackendRenderTarget {

skiko/src/commonMain/kotlin/org/jetbrains/skia/DirectContext.kt
@@ -2,12 +2,17 @@ package org.jetbrains.skia
 
 import org.jetbrains.skia.impl.*
 import org.jetbrains.skia.impl.Library.Companion.staticLoad
+import org.jetbrains.skiko.RenderException
+import org.jetbrains.skiko.loadOpenGLLibrary
 
 class DirectContext internal constructor(ptr: NativePointer) : RefCnt(ptr) {
     companion object {
         fun makeGL(): DirectContext {
             Stats.onNativeCall()
-            return DirectContext(_nMakeGL())
+            loadOpenGLLibrary()
+            val ptr = _nMakeGL()
+            if (ptr == NullPointer) throw RenderException("Can't create OpenGL DirectContext")
+            return DirectContext(ptr)
         }
 
         fun makeMetal(devicePtr: NativePointer, queuePtr: NativePointer): DirectContext {

skiko/src/commonMain/kotlin/org/jetbrains/skiko/OpenGLLibrary.kt
@@ -1,0 +1,14 @@
+package org.jetbrains.skiko
+
+/**
+ * Load OpenGL library into memory.
+ *
+ * Should be called before any OpenGL operation.
+ *
+ * The current implementation loads OpenGl lazily on Windows, and at app startup on Linux/macOs, native and web targets
+ *
+ * Some Windows machines don't have OpenGL (usually CI machines), so we'll fallback to other renderers on them.
+ *
+ * @throws RenderException if OpenGL library can't be loaded.
+ */
+internal expect fun loadOpenGLLibrary()

skiko/src/jvmMain/kotlin/org/jetbrains/skiko/OpenGLLibrary.jvm.kt
@@ -1,0 +1,29 @@
+package org.jetbrains.skiko
+
+import org.jetbrains.skia.impl.Library
+
+private external fun loadOpenGLLibraryWindows()
+
+private var isLoaded = false
+
+@Synchronized
+internal actual fun loadOpenGLLibrary() {
+    if (!isLoaded) {
+        when {
+            // On Windows it is linked dynamically
+            hostOs.isWindows -> {
+                Library.staticLoad()
+                loadOpenGLLibraryWindows()
+            }
+            // it was deprecated in macOS 10.14 and isn't supported in Skiko
+            // see https://developer.apple.com/library/archive/documentation/GraphicsImaging/Conceptual/OpenGL-MacProgGuide/opengl_intro/opengl_intro.html
+            hostOs.isMacOS -> throw RenderException("OpenGL on macOS isn't supported")
+            // Do nothing as we don't know for sure which OS supports OpenGL.
+            // If there is support, we assume that it is already linked.
+            // If there is no support, there will be a native crash
+            // (to throw a RenderException we need to investigate case by case)
+            else -> Unit
+        }
+        isLoaded = true
+    }
+}

skiko/src/jvmMain/kotlin/org/jetbrains/skiko/OpenGLLibrary.kt
@@ -1,23 +1,0 @@
-package org.jetbrains.skiko
-
-/**
- * Load OpenGL library into memory.
- *
- * Should be called before any OpenGL operation.
- *
- * The current implementation loads OpenGl lazily on Windows, and at app startup on Linux/macOs.
- *
- * Some Windows machines don't have OpenGL (usually CI machines), so we'll fallback to other renderers on them.
- *
- * Throws [RenderException] if OpenGL library can't be loaded.
- */
-internal fun loadOpenGLLibrary() {
-    if (hostOs.isWindows) {
-        loadOpenGLLibraryWindows()
-    } else {
-        // do nothing, the library should be already available
-    }
-}
-
-@Synchronized
-private external fun loadOpenGLLibraryWindows()

skiko/src/nativeJsMain/kotlin/org/jetbrains/skiko/OpenGLLibrary.nativeJs.kt
@@ -1,0 +1,13 @@
+package org.jetbrains.skiko
+
+internal actual fun loadOpenGLLibrary() {
+    when (hostOs) {
+        // It is deprecated. See https://developer.apple.com/documentation/opengles
+        OS.Ios -> throw RenderException("OpenGL on iOS isn't supported")
+        // Do nothing as we don't know for sure which OS supports OpenGL.
+        // If there is support, we assume that it is already linked.
+        // If there is no support, there will be a native crash
+        // (to throw a RenderException we need to investigate case by case)
+        else -> Unit
+    }
+}

skiko/src/awtTest/kotlin/org/jetbrains/skiko/BackendRenderTargetTest.kt
@@ -1,0 +1,20 @@
+package org.jetbrains.skiko
+
+import org.jetbrains.skia.BackendRenderTarget
+import org.jetbrains.skiko.util.joinThreadCatching
+import kotlin.test.*
+
+class BackendRenderTargetTest {
+    @Test
+    fun makeGL() {
+        // use a new thread to be sure that there is no OpenGL context in it
+        joinThreadCatching {
+            try {
+                // if OpenGL is supported, this method doesn't fail, even if there is no OpenGL Context in the thread
+                BackendRenderTarget.makeGL(100, 100, 1, 8, 0, 0x8058)
+            } catch (e: RenderException) {
+                // it fails if OpenGL isn't supported at all
+            }
+        }
+    }
+}

skiko/src/awtTest/kotlin/org/jetbrains/skiko/DirectContextTest.kt
@@ -1,0 +1,26 @@
+package org.jetbrains.skiko
+
+import org.jetbrains.skia.DirectContext
+import org.jetbrains.skiko.util.joinThreadCatching
+import kotlin.test.*
+
+class DirectContextTest {
+    @Test
+    fun makeGL() {
+        // use a new thread to be sure that there is no OpenGL context in it
+        joinThreadCatching {
+            // Test only that the method doesn't crash VM, and only throw an exception.
+            // To properly test it, we have to use third-party created OpenGL context.
+            // Testing with our own created OpenGL context won't be fair, as these functions can do additional
+            // initialization (i.e. if we call our own glMakeCurrent, we have to call `loadOpenGLLibrary`)
+            var actualException: RenderException? = null
+            try {
+                DirectContext.makeGL()
+            } catch (e: RenderException) {
+                actualException = e
+            }
+            assertTrue(actualException != null, "makeGL didn't fail")
+        }
+    }
+}
+