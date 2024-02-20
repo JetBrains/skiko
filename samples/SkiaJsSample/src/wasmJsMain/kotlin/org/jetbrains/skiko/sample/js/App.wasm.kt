09b6a8098a71d1cc965f2fdf5219dbe069f805db
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2024-02-20 17:20:58 +0100

    Remove SkiaJsSample
    SkiaMultiplatformSample covers any possible scenario one can be interested in, including actually the
    js-only solution as well

--- samples/SkiaJsSample/src/wasmJsMain/kotlin/org/jetbrains/skiko/sample/js/App.wasm.kt
+++
@@ -1,9 +0,0 @@
-package org.jetbrains.skiko.sample.js
-
-import kotlinx.browser.document
-
-fun main() {
-    runApp()
-}
-
-actual fun findElementById(id: String): Any? = document.getElementById(id)
