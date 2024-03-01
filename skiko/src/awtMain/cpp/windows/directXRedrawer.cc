4c79cb3c0b6f4fc6df6ffd16d177d631054ab1a9
Ivan Matkov <matkov.ivan@gmail.com>
2024-03-01 09:17:28 +0100

    Use scaling = DXGI_SCALING_STRETCH for fallback too

--- skiko/src/awtMain/cpp/windows/directXRedrawer.cc
+++ skiko/src/awtMain/cpp/windows/directXRedrawer.cc
@@ -117,7 +117,7 @@ private:
         swapChainDesc.SampleDesc.Quality = 0;
         swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
         swapChainDesc.BufferCount = BuffersCount;
-        swapChainDesc.Scaling = DXGI_SCALING_NONE;
+        swapChainDesc.Scaling = DXGI_SCALING_STRETCH;
         swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
         return swapChainFactory4->CreateSwapChainForHwnd(queue.get(), hWnd, &swapChainDesc, nullptr, nullptr, swapChain1);
     }
