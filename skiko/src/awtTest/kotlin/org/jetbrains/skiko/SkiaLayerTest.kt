JetBrains/skiko

commit 21d7a12bfe4bde55b93865dcd0ec81d3ee072553

Don't try to init D3D with zero size (#858)
* Don't try to init D3D with zero size

* Pass AWT size for swap chain initialization

* Avoid reading size twice

skiko/src/awtMain/kotlin/org/jetbrains/skiko/context/Direct3DContextHandler.kt
@@ -41,34 +41,38 @@ internal class Direct3DContextHandler(layer: SkiaLayer) : JvmContextHandler(laye
         }
         return false
     }
-    private var isD3DInited = false
 
     override fun initCanvas() {
+        val context = context ?: return
         val scale = layer.contentScale
-        val w = (layer.width * scale).toInt().coerceAtLeast(0)
-        val h = (layer.height * scale).toInt().coerceAtLeast(0)
-        val surfaceProps = SurfaceProps(pixelGeometry = layer.pixelGeometry)
+        val width = (layer.width * scale).toInt()
+        val height = (layer.height * scale).toInt()
 
-        if (isSizeChanged(w, h) || isSurfacesNull()) {
+        if (width <= 0 || height <= 0) {
+            return
+        }
+
+        if (isSizeChanged(width, height) || isSurfacesNull()) {
             disposeCanvas()
-            context?.flush()
-            
-            if (!isD3DInited) {
-                directXRedrawer.initSwapChain()
-            } else {
-                directXRedrawer.resizeBuffers(w, h)
-            }
-            
+            context.flush()
+
+            val justInitialized = directXRedrawer.changeSize(width, height)
             try {
-                for (bufferIndex in 0..bufferCount - 1) {
-                    surfaces[bufferIndex] = directXRedrawer.makeSurface(getPtr(context!!), w, h, surfaceProps, bufferIndex)
+                val surfaceProps = SurfaceProps(pixelGeometry = layer.pixelGeometry)
+                for (bufferIndex in 0 until bufferCount) {
+                    surfaces[bufferIndex] = directXRedrawer.makeSurface(
+                        context = getPtr(context),
+                        width = width,
+                        height = height,
+                        surfaceProps = surfaceProps,
+                        index = bufferIndex
+                    )
                 }
             } finally {
-                Reference.reachabilityFence(context!!)
+                Reference.reachabilityFence(context)
             }
 
-            if (!isD3DInited) {
-                isD3DInited = true
+            if (justInitialized) {
                 directXRedrawer.initFence()
             }
         }
@@ -77,14 +81,13 @@ internal class Direct3DContextHandler(layer: SkiaLayer) : JvmContextHandler(laye
     }
 
     override fun flush() {
+        val context = context ?: return
+        val surface = surface ?: return
         try {
-            flush(
-                getPtr(context!!),
-                getPtr(surface!!)
-            )
+            flush(getPtr(context), getPtr(surface))
         } finally {
-            Reference.reachabilityFence(context!!)
-            Reference.reachabilityFence(surface!!)
+            Reference.reachabilityFence(context)
+            Reference.reachabilityFence(surface)
         }
     }
 

skiko/src/awtMain/cpp/windows/directXRedrawer.cc
@@ -58,12 +58,7 @@ public:
         device.reset(nullptr);
     }
 
-    void initSwapChain() {
-        RECT windowRect;
-        GetClientRect(window, &windowRect);
-        UINT width = windowRect.right - windowRect.left;
-        UINT height = windowRect.bottom - windowRect.top;
-
+    void initSwapChain(UINT width, UINT height) {
         gr_cp<IDXGIFactory4> swapChainFactory4;
         gr_cp<IDXGISwapChain1> swapChain1;
         CreateDXGIFactory2(0, IID_PPV_ARGS(&swapChainFactory4));
@@ -330,12 +325,12 @@ extern "C"
     }
 
     JNIEXPORT void JNICALL Java_org_jetbrains_skiko_redrawer_Direct3DRedrawer_initSwapChain(
-        JNIEnv *env, jobject redrawer, jlong devicePtr)
+        JNIEnv *env, jobject redrawer, jlong devicePtr, jint width, jint height)
     {
         __try
         {
             DirectXDevice *d3dDevice = fromJavaPointer<DirectXDevice *>(devicePtr);
-            d3dDevice->initSwapChain();
+            d3dDevice->initSwapChain((UINT) width, (UINT) height);
         }
         __except(EXCEPTION_EXECUTE_HANDLER) {
             auto code = GetExceptionCode();

skiko/src/awtMain/kotlin/org/jetbrains/skiko/redrawer/Direct3DRedrawer.kt
@@ -19,6 +19,7 @@ internal class Direct3DRedrawer(
     override val renderInfo: String get() = contextHandler.rendererInfo()
 
     private var drawLock = Any()
+    private var isSwapChainInitialized = false
 
     private var device: Long = 0L
         get() {
@@ -84,10 +85,11 @@ internal class Direct3DRedrawer(
     }
 
     private fun drawAndSwap(withVsync: Boolean) = synchronized(drawLock) {
-        if (!isDisposed) {
-            contextHandler.draw()
-            swap(device, withVsync)
+        if (isDisposed) {
+            return
         }
+        contextHandler.draw()
+        swap(withVsync)
     }
 
     fun makeContext() = DirectContext(
@@ -100,10 +102,25 @@ internal class Direct3DRedrawer(
         }
     }
 
-    fun resizeBuffers(width: Int, height: Int) = resizeBuffers(device, width, height)
+    fun changeSize(width: Int, height: Int): Boolean {
+        return if (!isSwapChainInitialized) {
+            initSwapChain(device, width, height)
+            isSwapChainInitialized = true
+            true
+        } else {
+            resizeBuffers(device, width, height)
+            false
+        }
+    }
+
+    private fun swap(withVsync: Boolean) {
+        if (!isSwapChainInitialized) {
+            return
+        }
+        swap(device, withVsync)
+    }
 
     fun getBufferIndex() = getBufferIndex(device)
-    fun initSwapChain() = initSwapChain(device)
     fun initFence() = initFence(device)
 
     // Called from native code
@@ -117,7 +134,7 @@ internal class Direct3DRedrawer(
     private external fun swap(device: Long, isVsyncEnabled: Boolean)
     private external fun disposeDevice(device: Long)
     private external fun getBufferIndex(device: Long): Int
-    private external fun initSwapChain(device: Long)
+    private external fun initSwapChain(device: Long, width: Int, height: Int)
     private external fun initFence(device: Long)
     private external fun getAdapterName(adapter: Long): String
     private external fun getAdapterMemorySize(adapter: Long): Long

commit 25b5f6890c7d165d8e50a90adc159f87af9e2d3f

Windows. Fix a crash when we resize to zero and then to non-zero
Fixes https://github.com/JetBrains/compose-multiplatform/issues/4425

It is a regression after https://github.com/JetBrains/skiko/pull/858

The crash was because `d3dDevice->swapChain->GetBuffer` didn't return the buffer to draw on if we reused the buffer from the previous frame (we didn't change `surface` in case of zero size).

I am not completely sure why, but I exhausted my the investigation limit
and this fix is needed by other reasons (we need vsync on swap buffers).

## Testing
An additional check in the existed test (fails before the fix)

skiko/src/awtMain/kotlin/org/jetbrains/skiko/context/Direct3DContextHandler.kt
@@ -45,12 +45,12 @@ internal class Direct3DContextHandler(layer: SkiaLayer) : JvmContextHandler(laye
     override fun initCanvas() {
         val context = context ?: return
         val scale = layer.contentScale
-        val width = (layer.width * scale).toInt()
-        val height = (layer.height * scale).toInt()
 
-        if (width <= 0 || height <= 0) {
-            return
-        }
+        // Direct3D can't work with zero size.
+        // Don't rewrite code to skipping, as we need the whole pipeline in zero case too
+        // (drawing -> flushing -> swapping -> waiting for vsync)
+        val width = (layer.width * scale).toInt().coerceAtLeast(1)
+        val height = (layer.height * scale).toInt().coerceAtLeast(1)
 
         if (isSizeChanged(width, height) || isSurfacesNull()) {
             disposeCanvas()

skiko/src/awtTest/kotlin/org/jetbrains/skiko/SkiaLayerTest.kt
@@ -249,6 +249,12 @@ class SkiaLayerTest {
             layer.needRedraw()
             delay(1000)
             assertEquals(0, renderedWidth)
+
+            renderedWidth = -1
+            layer.size = Dimension(40, 40)
+            layer.needRedraw()
+            delay(1000)
+            assertEquals((40 * density).toInt(), renderedWidth)
         } finally {
             layer.dispose()
             window.close()