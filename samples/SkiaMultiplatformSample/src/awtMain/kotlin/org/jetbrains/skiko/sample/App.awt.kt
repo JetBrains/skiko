bff1e6118055d28e9c311403e6e79c4e2c2b7352
Alovchin91 <8490695+Alovchin91@users.noreply.github.com>
2024-07-02 15:56:08 +0200

    Add a Jump List sample to SkiaMultiplatformSample

--- samples/SkiaMultiplatformSample/src/awtMain/kotlin/org/jetbrains/skiko/sample/App.awt.kt
+++ samples/SkiaMultiplatformSample/src/awtMain/kotlin/org/jetbrains/skiko/sample/App.awt.kt
@@ -1,11 +1,13 @@
 package org.jetbrains.skiko.sample
 
 import org.jetbrains.skiko.*
+import org.jetbrains.skiko.windows.*
 import java.awt.Dimension
 import javax.swing.*
 
-fun main() {
+fun main(args: Array<String>) {
     val skiaLayer = SkiaLayer()
+    parseArgs(args)
     val app = run {
         //EmojiStory()
         AwtClocks(skiaLayer)
@@ -20,5 +22,68 @@ fun main() {
         skiaLayer.needRedraw()
         window.pack()
         window.isVisible = true
+        createWindowsJumpList()
+    }
+}
+
+/**
+ * The Jump List sample requires a separate executable for two reasons:
+ * 1. The java.exe process somehow blocks the Jump Lists so to enable them we would have to provide
+ *    an AppUserModelID for the sample's window, and
+ * 2. In the current implementation, the Jump List items launch the calling process when clicked with
+ *    command line arguments provided to the JumpListItem. Without a separate executable,
+ *    java.exe will be launched.
+ */
+private fun createWindowsJumpList() {
+    // Check if Jump Lists are supported
+    if (JumpList.isSupported()) {
+        // Start a Jump List building transaction
+        JumpList.build {
+            // If the process has an explicit AppUserModelID, it should be set for the Jump List
+            // before calling [beginList]
+            // setAppID("org.jetbrains.skiko.SkiaMultiplatformSample.awt")
+
+            // Create a new Jump List
+            beginList()
+
+            // Collect elements to add to the Jump List, e.g. some recent documents
+            val recentItems = listOf(
+                JumpListItem("Recent item 1", "path\\to\\recent\\item\\1").apply {
+                    attributes = JumpListItemAttributes().apply {
+                        description = "The first recent item"
+                        icon = JumpListItemIcon("explorer.exe", 0)
+                    }
+                }
+            )
+
+            // TODO: Exclude [removedItems] from [recentItems]
+            val removedItems = getRemovedItems()
+
+            // Add recent items to the Jump List
+            addCategory("Recent items", recentItems)
+
+            // Add a user task to the Jump List
+            addUserTask(JumpListItem("User Task 1", "--user-task-1"))
+
+            // Commit the Jump List and finish the transaction
+            try {
+                commit()
+            }
+            catch (e: java.lang.RuntimeException) {
+                println("Couldn't build the Jump List, most likely because the sample is run via java.exe. ${e.message}")
+            }
+        }
+    }
+}
+
+// When a Jump List item is clicked, the app is re-launched with an argument
+// passed to the JumpListItem's constructor
+private fun parseArgs(args: Array<String>) {
+    for(arg in args) {
+        when (arg) {
+            "--user-task-1" -> println("User Task 1 in the Jump List was clicked!")
+            "path\\to\\recent\\item\\1" -> println("Recent Item 1 in the Jump List was clicked!")
+            else -> println("Unknown argument: ($arg)")
+        }
     }
 }
