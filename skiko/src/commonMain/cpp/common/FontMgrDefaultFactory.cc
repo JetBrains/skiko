d9d30be378027c6ef2d67247acea197d6b2455c4
Oleksandr.Karpovich <a.n.karpovich@gmail.com>
2024-07-26 16:24:44 +0200

    Use SkFontMgr_New_Custom_Data for Wasm

--- skiko/src/commonMain/cpp/common/FontMgrDefaultFactory.cc
+++ skiko/src/commonMain/cpp/common/FontMgrDefaultFactory.cc
@@ -17,8 +17,16 @@
 #include "ports/SkFontMgr_android.h"
 #endif
 
-sk_sp<SkFontMgr> SkFontMgrSkikoDefault() {
+#ifdef SKIKO_WASM
+#include <vector>
+#include "SkData.h"
+#include "ports/SkFontMgr_data.h"
+struct SkEmbeddedResource { const uint8_t* data; size_t size; };
+struct SkEmbeddedResourceHeader { const SkEmbeddedResource* entries; int count; };
+extern "C" const SkEmbeddedResourceHeader SK_EMBEDDED_FONTS;
+#endif
 
+sk_sp<SkFontMgr> SkFontMgrSkikoDefault() {
 #if defined(SK_BUILD_FOR_MAC) || defined(SK_BUILD_FOR_IOS)
     return SkFontMgr_New_CoreText(nullptr);
 #endif
@@ -34,8 +42,14 @@ sk_sp<SkFontMgr> SkFontMgrSkikoDefault() {
 #ifdef SK_BUILD_FOR_ANDROID
     return SkFontMgr_New_Android(nullptr);
 #endif
-}
 
-struct SkEmbeddedResource { const uint8_t* data; size_t size; };
-struct SkEmbeddedResourceHeader { const SkEmbeddedResource* entries; int count; };
-extern "C" const SkEmbeddedResourceHeader SK_EMBEDDED_FONTS;
+#ifdef SKIKO_WASM
+    sk_sp<SkData> embeddedFontData =
+        SkData::MakeWithoutCopy(SK_EMBEDDED_FONTS.entries[0].data, SK_EMBEDDED_FONTS.entries[0].size);
+    std::vector<sk_sp<SkData>> fontDataVector = { embeddedFontData };
+    SkSpan<sk_sp<SkData>> dataSpan(fontDataVector);
+    return SkFontMgr_New_Custom_Data(dataSpan);
+#endif
+
+    return nullptr;
+}
