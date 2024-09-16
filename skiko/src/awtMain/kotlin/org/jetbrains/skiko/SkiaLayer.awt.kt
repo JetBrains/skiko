JetBrains/skiko

commit b53f5d4ab6c2ace86bc1d9c9ae6bfa1b4a9caf14

Make device scaling reactivity more reliable and fix layout glitch (#661)
…on Windows

First off, I don't think there was any need for the repeated checks for device pixel ratio changes via checkContentScale because we should be able to rely on the graphicsContextScaleTransform property from AWT.

I removed the corresponding code and changed contentScale to directly return the platform-specific implementations from the toolkit/device. There's also no need to cache the value because the device and the graphics configuration already does it for us.

Secondly, there was a layout glitch on Windows when moving windows between monitors with different device pixel ratios. A fix has to happen directly in the JBR, but for now I created a semi-cheap workaround. See this issue and the linked ones for details: https://youtrack.jetbrains.com/issue/JBR-5259/Canvas-is-painted-at-the-wrong-position-after-dragging-JFrame-to-a-monitor-with-different-scale

skiko/src/awtMain/kotlin/org/jetbrains/skiko/SkiaLayer.awt.kt
@@ -1,12 +1,12 @@
 package org.jetbrains.skiko
 
 import org.jetbrains.skia.*
-import org.jetbrains.skia.Canvas
 import org.jetbrains.skiko.redrawer.Redrawer
 import java.awt.Color
 import java.awt.Component
 import java.awt.Window
 import java.awt.event.*
+import java.awt.geom.AffineTransform
 import java.awt.im.InputMethodRequests
 import java.util.concurrent.CancellationException
 import javax.accessibility.Accessible
@@ -15,6 +15,9 @@ import javax.swing.JPanel
 import javax.swing.SwingUtilities
 import javax.swing.SwingUtilities.isEventDispatchThread
 import javax.swing.UIManager
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 actual open class SkiaLayer internal constructor(
     externalAccessibleFactory: ((Component) -> Accessible)? = null,
@@ -71,11 +74,15 @@ actual open class SkiaLayer internal constructor(
     val canvas: java.awt.Canvas
         get() = backedLayer
 
+    private var peerBufferSizeFixJob: Job? = null
+
     init {
         isOpaque = false
         layout = null
         backedLayer = object : HardwareLayer(externalAccessibleFactory) {
             override fun paint(g: java.awt.Graphics) {
+                checkContentScale()
+
                 // 1. JPanel.paint is not always called (in rare cases).
                 //    For example if we call 'jframe.isResizable = false` on Ubuntu
                 //
@@ -83,7 +90,6 @@ actual open class SkiaLayer internal constructor(
                 //    For example, on macOs when we resize window or change DPI
                 //
                 // 3. to avoid double paint in one single frame, use needRedraw instead of redrawImmediately
-                this@SkiaLayer.checkContentScale()
                 redrawer?.needRedraw()
             }
 
@@ -109,9 +115,24 @@ actual open class SkiaLayer internal constructor(
         }
         @Suppress("LeakingThis")
         add(backedLayer)
+
         backedLayer.addHierarchyListener {
             if (it.changeFlags and HierarchyEvent.SHOWING_CHANGED.toLong() != 0L) {
                 checkShowing()
+            }
+        }
+
+        addPropertyChangeListener("graphicsContextScaleTransform") {
+            redrawer?.syncSize()
+            notifyChange(PropertyKind.ContentScale)
+
+            // Workaround for JBR-5259
+            if (hostOs == OS.Windows) {
+                peerBufferSizeFixJob?.cancel()
+                peerBufferSizeFixJob = GlobalScope.launch(MainUIDispatcher) {
+                    backedLayer.setLocation(1, 0)
+                    backedLayer.setLocation(0, 0)
+                }
             }
         }
     }
@@ -129,8 +150,6 @@ actual open class SkiaLayer internal constructor(
         super.addNotify()
         val window = SwingUtilities.getRoot(this) as Window
         window.addComponentListener(fullscreenAdapter)
-        backedLayer.defineContentScale()
-        checkContentScale()
         checkShowing()
         init(isInited)
     }
@@ -158,7 +177,7 @@ actual open class SkiaLayer internal constructor(
     }
 
     actual val contentScale: Float
-        get() = backedLayer.contentScale
+        get() = graphicsConfiguration.defaultTransform.scaleX.toFloat()
 
     /**
      * Returns the pointer to an OS specific handle (native resource) of the [SkiaLayer].
@@ -336,25 +355,21 @@ actual open class SkiaLayer internal constructor(
             pictureRecorder?.close()
             pictureRecorder = null
             backedLayer.dispose()
+            peerBufferSizeFixJob?.cancel()
             isDisposed = true
         }
     }
 
-    override fun setBounds(x: Int, y: Int, width: Int, height: Int) {
-        var roundedWidth = width
-        var roundedHeight = height
-        if (isInited) {
-            roundedWidth = roundSize(width)
-            roundedHeight = roundSize(height)
-        }
-        super.setBounds(x, y, roundedWidth, roundedHeight)
-        backedLayer.setSize(roundedWidth, roundedHeight)
+    override fun doLayout() {
+        backedLayer.setBounds(0, 0, roundSize(width), roundSize(height))
+        backedLayer.validate()
         redrawer?.syncSize()
     }
 
+
     override fun paint(g: java.awt.Graphics) {
-        super.paint(g)
         checkContentScale()
+
         // `paint` can be called when we already inside `draw` method.
         //
         // For example if we call some AWT function inside renderer.onRender,
@@ -368,14 +383,15 @@ actual open class SkiaLayer internal constructor(
         }
     }
 
-    /*
-    In AWT there is no a change DPI event; so we should call this function when we expect that DPI maybe changed
-    We hope that call it on AWT/SWING `paint` and our update is enough
-     */
-    private fun checkContentScale() {
-        if (backedLayer.checkContentScale()) {
-            notifyChange(PropertyKind.ContentScale)
-            redrawer?.syncSize()
+    private var latestCheckedDefaultTransform: AffineTransform? = null
+
+    // Workaround for JBR-5274 and JBR-5305
+    fun checkContentScale() {
+        graphicsConfiguration.defaultTransform.let {
+            if (it != latestCheckedDefaultTransform) {
+                firePropertyChange("graphicsContextScaleTransform", latestCheckedDefaultTransform, it)
+                latestCheckedDefaultTransform = it
+            }
         }
     }
 
@@ -503,8 +519,6 @@ actual open class SkiaLayer internal constructor(
     internal fun update(nanoTime: Long) {
         check(isEventDispatchThread()) { "Method should be called from AWT event dispatch thread" }
         check(!isDisposed) { "SkiaLayer is disposed" }
-
-        checkContentScale()
 
         FrameWatcher.nextFrame()
         fpsCounter?.tick()

samples/SkiaAwtSample/src/main/kotlin/SkiaAwtSample/App.kt
@@ -38,9 +38,8 @@ fun createWindow(title: String, exitOnClose: Boolean) = SwingUtilities.invokeLat
     val window = JFrame(title)
     window.defaultCloseOperation =
         if (exitOnClose) WindowConstants.EXIT_ON_CLOSE else WindowConstants.DISPOSE_ON_CLOSE
-    window.title = title
-
-    skiaLayer.attachTo(window.contentPane)
+    window.background = Color.GREEN
+    window.contentPane = skiaLayer
 
     // Create menu.
     val menuBar = JMenuBar()
@@ -126,7 +125,7 @@ fun createWindow(title: String, exitOnClose: Boolean) = SwingUtilities.invokeLat
         }
         skiaLayer.transparency = true
     } else {
-        skiaLayer.background = Color.WHITE
+        skiaLayer.background = Color.LIGHT_GRAY
     }
 
     // MANDATORY: set window preferred size before calling pack()

samples/SkiaAwtSample/src/main/kotlin/SkiaAwtSample/ClocksAwt.kt
@@ -84,7 +84,7 @@ class ClocksAwt(private val layer: SkiaLayer) : SkikoView {
         }
         val paragraph = ParagraphBuilder(style, fontCollection)
             .pushStyle(TextStyle().setColor(0xFF000000.toInt()))
-            .addText("Graphics API: ${layer.renderApi} ✿ﾟ $currentSystemTheme")
+            .addText("JRE: ${System.getProperty("java.vendor")}, ${System.getProperty("java.runtime.version")}, Graphics API: ${layer.renderApi} ✿ﾟ $currentSystemTheme")
             .popStyle()
             .build()
         paragraph.layout(Float.POSITIVE_INFINITY)

skiko/src/awtMain/cpp/linux/drawlayer.cc
@@ -82,12 +82,6 @@ extern "C"
         }
     }
 
-    JNIEXPORT jfloat JNICALL Java_org_jetbrains_skiko_PlatformOperationsKt_linuxGetDpiScaleNative(JNIEnv *env, jobject properties, jlong platformInfoPtr)
-    {
-        JAWT_X11DrawingSurfaceInfo *dsi_x11 = fromJavaPointer<JAWT_X11DrawingSurfaceInfo *>(platformInfoPtr);
-        return (float) getDpiScaleByDisplay(dsi_x11->display);
-    }
-
     JNIEXPORT jfloat JNICALL Java_org_jetbrains_skiko_SetupKt_linuxGetSystemDpiScale(JNIEnv *env, jobject layer)
     {
         return (float) getDpiScale();

skiko/src/awtMain/kotlin/org/jetbrains/skiko/HardwareLayer.kt
@@ -18,13 +18,6 @@ internal open class HardwareLayer(
         }
     }
 
-    // getDpiScale is expensive operation on some platforms, so we cache it
-    private var _contentScale: Float? = null
-
-    fun defineContentScale() {
-        _contentScale = getDpiScale()
-    }
-
     override fun paint(g: Graphics) {}
 
     open fun init() {
@@ -43,22 +36,6 @@ internal open class HardwareLayer(
     private external fun nativeInit(platformInfo: Long)
     private external fun nativeDispose()
 
-    // TODO checkContentScale is called before init. it is ok, but when we fix getDpiScale on Linux we should check [isInit]
-    fun checkContentScale(): Boolean {
-        val contentScale = getDpiScale()
-        if (contentScale != _contentScale) {
-            _contentScale = contentScale
-            return true
-        }
-        return false
-    }
-
-    private fun getDpiScale(): Float {
-        val scale = platformOperations.getDpiScale(this)
-        check(scale > 0) { "HardwareLayer.contentScale isn't positive: $contentScale"}
-        return scale
-    }
-
     val contentHandle: Long
         get() = useDrawingSurfacePlatformInfo(::getContentHandle)
 
@@ -67,9 +44,6 @@ internal open class HardwareLayer(
 
     val currentDPI: Int
         get() = useDrawingSurfacePlatformInfo(::getCurrentDPI)
-
-    val contentScale: Float
-        get() = _contentScale!!
 
     var fullscreen: Boolean
         get() = platformOperations.isFullscreen(this)

skiko/src/awtMain/kotlin/org/jetbrains/skiko/PlatformOperations.kt
@@ -41,7 +41,6 @@ internal interface PlatformOperations {
     fun setFullscreen(component: Component, value: Boolean)
     fun disableTitleBar(component: Component, headerHeight: Float)
     fun orderEmojiAndSymbolsPopup()
-    fun getDpiScale(component: Component): Float
 }
 
 internal val platformOperations: PlatformOperations by lazy {
@@ -54,10 +53,6 @@ internal val platformOperations: PlatformOperations by lazy {
 
                 override fun setFullscreen(component: Component, value: Boolean) {
                     osxSetFullscreenNative(component, value)
-                }
-
-                override fun getDpiScale(component: Component): Float {
-                    return component.graphicsConfiguration.defaultTransform.scaleX.toFloat()
                 }
 
                 override fun disableTitleBar(component: Component, headerHeight: Float) {
@@ -88,10 +83,6 @@ internal val platformOperations: PlatformOperations by lazy {
 
                 override fun orderEmojiAndSymbolsPopup() {
                 }
-
-                override fun getDpiScale(component: Component): Float {
-                    return component.graphicsConfiguration.defaultTransform.scaleX.toFloat()
-                }
             }
         }
         OS.Linux -> {
@@ -113,10 +104,6 @@ internal val platformOperations: PlatformOperations by lazy {
 
                 override fun orderEmojiAndSymbolsPopup() {
                 }
-
-                override fun getDpiScale(component: Component): Float {
-                    return component.graphicsConfiguration.defaultTransform.scaleX.toFloat()
-                }
             }
         }
         OS.Android -> TODO()
@@ -131,6 +118,3 @@ external private fun osxIsFullscreenNative(component: Component): Boolean
 external private fun osxSetFullscreenNative(component: Component, value: Boolean)
 external private fun osxDisableTitleBar(component: Component, headerHeight: Float)
 external private fun osxOrderEmojiAndSymbolsPopup()
-
-// Linux
-external private fun linuxGetDpiScaleNative(platformInfo: Long): Float

skiko/src/jvmMain/cpp/common/stubs.cc
@@ -15,12 +15,6 @@ void skikoUnimplemented(const char* message) {
 // we put here stubs for all OS specific native methods.
 
 #ifndef SK_BUILD_FOR_LINUX
-JNIEXPORT jfloat JNICALL Java_org_jetbrains_skiko_PlatformOperationsKt_linuxGetDpiScaleNative(
-    JNIEnv *env, jobject properties, jlong platformInfoPtr) {
-    skikoUnimplemented("Java_org_jetbrains_skiko_PlatformOperationsKt_linuxGetDpiScaleNative");
-    return 0;
-}
-
 JNIEXPORT jfloat JNICALL Java_org_jetbrains_skiko_SetupKt_linuxGetSystemDpiScale(JNIEnv *env, jobject layer) {
     skikoUnimplemented("Java_org_jetbrains_skiko_SetupKt_linuxGetSystemDpiScale");
     return 0;

commit cf1cc550eac36da4f0302bd2c4a68097b1d98411

Fix bounds of ComposePanel in IntelliJ on macOs
Fixes https://youtrack.jetbrains.com/issue/CMP-5856/Desktop-ComposePanel-size-breaks-with-.fillMax-modifiers#focus=Comments-27-10632441.0-0

Fixes https://youtrack.jetbrains.com/issue/CMP-5968/Compose-content-is-rendered-in-the-wrong-place-in-IJ-when-using-AWT-compositing

Regression after https://github.com/JetBrains/skiko/pull/661/files#diff-910a6e28fda20a00bc98c6a8a04f74ab701d79e841b9baddf146b810e610572fR363

When a panel changes its bounds, `doLayout` isn't called because the content itself wasn't changed. But we still need to update the bounds of the underlying layer.

Docs:
```
> doLayout
Causes this container to lay out its components

>
```

## Testing
https://youtrack.jetbrains.com/issue/CMP-5856/Desktop-ComposePanel-size-breaks-with-.fillMax-modifiers#focus=Comments-27-10632441.0-0 isn't reproducible after the fix

## Release Notes
### Fixes
- Fix bounds of ComposePanel in IntelliJ

skiko/src/awtMain/kotlin/org/jetbrains/skiko/SkiaLayer.awt.kt
@@ -143,7 +143,7 @@ actual open class SkiaLayer internal constructor(
         addPropertyChangeListener("graphicsContextScaleTransform") {
             Logger.debug { "graphicsContextScaleTransform changed for $this" }
             latestReceivedGraphicsContextScaleTransform = it.newValue as AffineTransform
-            redrawer?.syncSize()
+            redrawer?.syncBounds()
             notifyChange(PropertyKind.ContentScale)
 
             // Workaround for JBR-5259
@@ -191,7 +191,7 @@ actual open class SkiaLayer internal constructor(
             redrawer?.setVisible(isShowing)
         }
         if (isShowing) {
-            redrawer?.syncSize()
+            redrawer?.syncBounds()
             repaint()
         }
     }
@@ -252,7 +252,7 @@ actual open class SkiaLayer internal constructor(
     private val redrawerManager = RedrawerManager<Redrawer>(properties.renderApi) { renderApi, oldRedrawer ->
         oldRedrawer?.dispose()
         val newRedrawer = renderFactory.createRedrawer(this, renderApi, analytics, properties)
-        newRedrawer.syncSize()
+        newRedrawer.syncBounds()
         newRedrawer
     }
 
@@ -314,14 +314,6 @@ actual open class SkiaLayer internal constructor(
         }
     }
 
-    override fun doLayout() {
-        Logger.debug { "doLayout on $this" }
-        backedLayer.setBounds(0, 0, roundSize(width), roundSize(height))
-        backedLayer.validate()
-        redrawer?.syncSize()
-    }
-
-
     override fun paint(g: java.awt.Graphics) {
         Logger.debug { "Paint called on: $this" }
         checkContentScale()
@@ -331,6 +323,11 @@ actual open class SkiaLayer internal constructor(
     override fun setBounds(x: Int, y: Int, width: Int, height: Int) {
         super.setBounds(x, y, width, height)
 
+        Logger.debug { "setBounds on $this" }
+        backedLayer.setBounds(0, 0, roundSize(width), roundSize(height))
+        backedLayer.validate()
+        redrawer?.syncBounds()
+
         // To avoid visual artifacts on Windows/Direct3D,
         // redrawing should be performed immediately, without scheduling to "later".
         // Subscribing to events instead of overriding this method won't help too.
@@ -338,7 +335,6 @@ actual open class SkiaLayer internal constructor(
         // Please note that calling redraw during layout might break software renderers,
         // so applying this fix only for Direct3D case.
         if (renderApi == GraphicsApi.DIRECT3D && isShowing) {
-            redrawer?.syncSize()
             tryRedrawImmediately()
         }
     }

skiko/src/awtMain/kotlin/org/jetbrains/skiko/redrawer/MetalRedrawer.kt
@@ -169,7 +169,7 @@ internal class MetalRedrawer(
         }
     }
 
-    override fun syncSize() = synchronized(drawLock) {
+    override fun syncBounds() = synchronized(drawLock) {
         check(isEventDispatchThread()) { "Method should be called from AWT event dispatch thread" }
         val rootPane = getRootPane(layer)
         val globalPosition = convertPoint(layer.backedLayer, 0, 0, rootPane)

skiko/src/commonMain/kotlin/org/jetbrains/skiko/redrawer/Redrawer.kt
@@ -4,7 +4,7 @@ internal interface Redrawer {
     fun dispose()
     fun needRedraw()
     fun redrawImmediately()
-    fun syncSize() = Unit
+    fun syncBounds() = Unit
     fun setVisible(isVisible: Boolean) = Unit
     val renderInfo: String
 }

skiko/src/macosMain/kotlin/org/jetbrains/skiko/SkiaLayer.macos.kt
@@ -84,13 +84,13 @@ actual open class SkiaLayer {
     private val nsViewObserver = object : NSObject() {
         @ObjCAction
         fun frameDidChange(notification: NSNotification) {
-            redrawer?.syncSize()
+            redrawer?.syncBounds()
             redrawer?.redrawImmediately()
         }
 
         @ObjCAction
         fun windowDidChangeBackingProperties(notification: NSNotification) {
-            redrawer?.syncSize()
+            redrawer?.syncBounds()
             redrawer?.redrawImmediately()
         }
 
@@ -126,7 +126,7 @@ actual open class SkiaLayer {
         nsView.postsFrameChangedNotifications = true
         nsViewObserver.addObserver()
         redrawer = createNativeRedrawer(this, renderApi).apply {
-            syncSize()
+            syncBounds()
             needRedraw()
         }
     }

skiko/src/macosMain/kotlin/org/jetbrains/skiko/redrawer/MetalRedrawer.macos.kt
@@ -79,7 +79,7 @@ internal class MacOsMetalRedrawer(
     /**
      * Synchronizes the [metalLayer] size with the size of underlying nsView
      */
-    override fun syncSize() {
+    override fun syncBounds() {
         syncContentScale()
         val osFrame = skiaLayer.nsView.frame
         val (w, h) = osFrame.useContents {

skiko/src/macosMain/kotlin/org/jetbrains/skiko/redrawer/OpenGLRedrawer.macos.kt
@@ -43,7 +43,7 @@ internal class MacOsOpenGLRedrawer(
         glLayer.dispose()
     }
 
-    override fun syncSize() {
+    override fun syncBounds() {
         syncContentScale()
         skiaLayer.nsView.frame.useContents {
             glLayer.setFrame(