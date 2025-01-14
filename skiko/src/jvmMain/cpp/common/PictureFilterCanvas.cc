ec024bee93df1421dee0bf91aa4a68b6f2620bf3
Ivan Matkov <ivan.matkov@jetbrains.com>
2025-01-14 09:48:49 +0100

    Fix confused naming

--- skiko/src/jvmMain/cpp/common/PictureFilterCanvas.cc
+++ skiko/src/jvmMain/cpp/common/PictureFilterCanvas.cc
@@ -6,19 +6,19 @@ class SkikoPictureFilterCanvas : public SkNWayCanvas {
 public:
     SkikoPictureFilterCanvas(SkCanvas* canvas) :
         SkNWayCanvas(canvas->imageInfo().width(), canvas->imageInfo().height()),
-        _onDrawPicture(nullptr) {
+        _jobject(nullptr) {
         this->addCanvas(canvas);
     }
 
     virtual ~SkikoPictureFilterCanvas() {
-        skija::PictureFilterCanvas::detach(_onDrawPicture);
+        skija::PictureFilterCanvas::detach(_jobject);
     }
 
-    jobject _onDrawPicture;
+    jobject _jobject;
 
 protected:
     void onDrawPicture(const SkPicture* picture, const SkMatrix* matrix, const SkPaint* paint) override {
-        jboolean handled = skija::PictureFilterCanvas::onDrawPicture(_onDrawPicture, picture, matrix, paint);
+        jboolean handled = skija::PictureFilterCanvas::onDrawPicture(_jobject, picture, matrix, paint);
         if (!handled) {
             SkCanvas::onDrawPicture(picture, matrix, paint);
         }
@@ -26,9 +26,9 @@ protected:
 };
 
 extern "C" JNIEXPORT void JNICALL Java_org_jetbrains_skia_PictureFilterCanvas_1jvmKt_PictureFilterCanvas_1nInit
-  (JNIEnv* env, jclass jclass, jobject onDrawPicture, jlong canvasPtr) {
+  (JNIEnv* env, jclass jclass, jobject pictureFilterCanvas, jlong canvasPtr) {
     SkikoPictureFilterCanvas* canvas = reinterpret_cast<SkikoPictureFilterCanvas*>(static_cast<uintptr_t>(canvasPtr));
-    canvas->_onDrawPicture = skija::PictureFilterCanvas::attach(env, onDrawPicture);
+    canvas->_jobject = skija::PictureFilterCanvas::attach(env, pictureFilterCanvas);
 }
 
 extern "C" JNIEXPORT jlong JNICALL Java_org_jetbrains_skia_PictureFilterCanvasKt_PictureFilterCanvas_1nMake
