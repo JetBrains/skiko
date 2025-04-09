6156244a74a6fb50625092c2ef8bcbdcf8a697e3
Clément de La Bourdonnaye <clement.bourdonnaye@jetbrains.com>
2025-04-09 11:36:11 +0200

    Revert "Revert "SKIKO-1013: Migrate skiko android target to proper androidTarget using AGP" (#1044)"
    This reverts commit c1f28386aabd5250fe3468923e2b3b167a439e18.

--- skiko/settings.gradle.kts
+++ skiko/settings.gradle.kts
@@ -2,20 +2,28 @@ pluginManagement {
     repositories {
         mavenCentral()
         gradlePluginPortal()
+        google()
     }
     buildscript {
         repositories {
             mavenCentral()
             maven("https://maven.pkg.jetbrains.space/public/p/compose/internal")
             maven("https://maven.pkg.jetbrains.space/public/p/space/maven")
+            google()
         }
         dependencies {
+            // TODO Removing this makes publishing module below crash android internal plugin
+            classpath("org.ow2.asm:asm:9.6")
+
             // TODO https://youtrack.jetbrains.com/issue/SKIKO-1003/Unify-Maven-publication-of-Skiko-with-Compose
             classpath("org.jetbrains.compose.internal.build-helpers:publishing:0.1.3")
             // used by org.jetbrains.compose.internal.build-helpers:publishing because of https://youtrack.jetbrains.com/issue/CMP-7603/Fix-Maven-Central-publication
             classpath("org.jetbrains:space-sdk-jvm:2024.3-185883")
 
             classpath("org.kohsuke:github-api:1.116")
+
+            // Added dependency for Android Gradle plugin
+            classpath("com.android.tools.build:gradle:7.4.2")
         }
     }
 
@@ -23,6 +31,7 @@ pluginManagement {
         val kotlinVersion = extra["kotlin.version"] as String
         kotlin("jvm").version(kotlinVersion)
         kotlin("multiplatform").version(kotlinVersion)
+        id("com.android.library").version("7.4.2") apply false
     }
 }
 rootProject.name = "skiko"
