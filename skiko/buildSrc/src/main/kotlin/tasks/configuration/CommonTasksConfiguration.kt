1e849c187c0f8d70715323118b083d3a15ac0b72
Clément de La Bourdonnaye <clement.bourdonnaye@jetbrains.com>
2025-04-09 12:32:51 +0200

    Make configureSignAndPublishDependencies lazy by configuring instead of direct forEach

--- skiko/buildSrc/src/main/kotlin/tasks/configuration/CommonTasksConfiguration.kt
+++ skiko/buildSrc/src/main/kotlin/tasks/configuration/CommonTasksConfiguration.kt
@@ -111,8 +111,7 @@ fun skiaPreprocessorFlags(os: OS, buildType: SkiaBuildType): Array<String> {
 
 fun Project.configureSignAndPublishDependencies() {
     if (supportWasm) {
-        tasks.forEach { task ->
-            val name = task.name
+        tasks.configureEach {
             val publishJs = "publishJsPublicationTo"
             val publishWasm = "publishSkikoWasmRuntimePublicationTo"
             val publishWasmPub = "publishWasmJsPublicationTo"
@@ -121,16 +120,15 @@ fun Project.configureSignAndPublishDependencies() {
             val signWasmPub = "signWasmJsPublication"
 
             when {
-                name.startsWith(publishJs) -> task.dependsOn(signWasm, signWasmPub)
-                name.startsWith(publishWasm) -> task.dependsOn(signJs)
-                name.startsWith(publishWasmPub) -> task.dependsOn(signJs)
-                name.startsWith(signWasmPub) -> task.dependsOn(signWasm)
+                name.startsWith(publishJs) -> dependsOn(signWasm, signWasmPub)
+                name.startsWith(publishWasm) -> dependsOn(signJs)
+                name.startsWith(publishWasmPub) -> dependsOn(signJs)
+                name.startsWith(signWasmPub) -> dependsOn(signWasm)
             }
         }
     }
     if (supportAndroid) {
-        tasks.forEach { task ->
-            val name = task.name
+        tasks.configureEach {
             val signAndroid = "signAndroidPublication"
             val generateMetadata = "generateMetadataFileForAndroidPublication"
             val publishAndroid = "publishAndroidPublicationTo"
@@ -142,16 +140,16 @@ fun Project.configureSignAndPublishDependencies() {
 
             when {
                 name.startsWith(signAndroid) || name.startsWith(generateMetadata) -> {
-                    task.dependsOn(skikoAndroidJar)
+                    dependsOn(skikoAndroidJar)
                 }
                 name.startsWith(publishAndroid) -> {
-                    task.dependsOn(signX64, signArm64)
+                    dependsOn(signX64, signArm64)
                 }
                 name.startsWith(publishX64) -> {
-                    task.dependsOn(signAndroid, signArm64)
+                    dependsOn(signAndroid, signArm64)
                 }
                 name.startsWith(publishArm64) -> {
-                    task.dependsOn(signX64, signAndroid)
+                    dependsOn(signX64, signAndroid)
                 }
             }
         }
