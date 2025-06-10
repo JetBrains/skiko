5b1b6a7ed3efdc7e563ea20ac43a804b12cfa06d
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2025-06-10 15:01:04 +0200

    SkiaJsSample build val skikoVersion instead of var version

--- samples/SkiaJsSample/build.gradle.kts
+++ samples/SkiaJsSample/build.gradle.kts
@@ -8,9 +8,9 @@ repositories {
     mavenLocal()
 }
 
-var version = "0.0.0-SNAPSHOT"
-if (project.hasProperty("skiko.version")) {
-    version = project.properties["skiko.version"] as String
+val skikoVersion = when {
+    project.hasProperty("skiko.version") -> project.properties["skiko.version"] as String
+    else ->  "0.0.0-SNAPSHOT"
 }
 
 val resourcesDir = "$buildDir/resources/"
@@ -22,10 +22,10 @@ val isCompositeBuild = extra.properties.getOrDefault("skiko.composite.build", ""
 dependencies {
     if (isCompositeBuild) {
         val filePath = gradle.includedBuild("skiko").projectDir
-            .resolve("./build/libs/skiko-wasm-$version.jar")
+            .resolve("./build/libs/skiko-wasm-$skikoVersion.jar")
         skikoWasm(files(filePath))
     } else {
-        skikoWasm("org.jetbrains.skiko:skiko-js-wasm-runtime:$version")
+        skikoWasm("org.jetbrains.skiko:skiko-js-wasm-runtime:$skikoVersion")
     }
 }
 
@@ -68,7 +68,7 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation("org.jetbrains.skiko:skiko:$version")
+                implementation("org.jetbrains.skiko:skiko:$skikoVersion")
             }
         }
 
