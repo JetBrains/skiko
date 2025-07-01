7bc7f5f605d4d773292562b7093c327a7bef0f5d
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2025-07-01 17:56:35 +0200

    Introduce FontStyleSet.external.kt

---
+++ skiko/src/commonMain/kotlin/org/jetbrains/skia/externals/FontStyleSet.external.kt
@@ -0,0 +1,27 @@
+package org.jetbrains.skia
+
+import org.jetbrains.skia.impl.NativePointer
+
+@ExternalSymbolName("org_jetbrains_skia_FontStyleSet__1nMakeEmpty")
+@ModuleImport("./skiko.mjs", "org_jetbrains_skia_FontStyleSet__1nMakeEmpty")
+internal external fun FontStyleSet_nMakeEmpty(): NativePointer
+
+@ExternalSymbolName("org_jetbrains_skia_FontStyleSet__1nCount")
+@ModuleImport("./skiko.mjs", "org_jetbrains_skia_FontStyleSet__1nCount")
+internal external fun FontStyleSet_nCount(ptr: NativePointer): Int
+
+@ExternalSymbolName("org_jetbrains_skia_FontStyleSet__1nGetStyle")
+@ModuleImport("./skiko.mjs", "org_jetbrains_skia_FontStyleSet__1nGetStyle")
+internal external fun FontStyleSet_nGetStyle(ptr: NativePointer, index: Int): Int
+
+@ExternalSymbolName("org_jetbrains_skia_FontStyleSet__1nGetStyleName")
+@ModuleImport("./skiko.mjs", "org_jetbrains_skia_FontStyleSet__1nGetStyleName")
+internal external fun FontStyleSet_nGetStyleName(ptr: NativePointer, index: Int): NativePointer
+
+@ExternalSymbolName("org_jetbrains_skia_FontStyleSet__1nGetTypeface")
+@ModuleImport("./skiko.mjs", "org_jetbrains_skia_FontStyleSet__1nGetTypeface")
+internal external fun FontStyleSet_nGetTypeface(ptr: NativePointer, index: Int): NativePointer
+
+@ExternalSymbolName("org_jetbrains_skia_FontStyleSet__1nMatchStyle")
+@ModuleImport("./skiko.mjs", "org_jetbrains_skia_FontStyleSet__1nMatchStyle")
+internal external fun FontStyleSet_nMatchStyle(ptr: NativePointer, style: Int): NativePointer
