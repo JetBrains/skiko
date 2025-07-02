7a91b66b149fc9b777636f122233cbe9a21e6670
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2025-07-02 16:22:00 +0200

    Switch off module resolution for js target

--- skiko/src/jsMain/kotlin/org/jetbrains/skia/Actuals.js.kt
+++ skiko/src/jsMain/kotlin/org/jetbrains/skia/Actuals.js.kt
@@ -2,6 +2,6 @@ package org.jetbrains.skia
 
 actual typealias ExternalSymbolName = kotlin.js.JsName
 
-actual typealias QuasiJsModule = kotlin.js.JsModule
-actual typealias QuasiJsNonModule = kotlin.js.JsNonModule
-actual typealias QuasiJsQualifier = kotlin.js.JsQualifier
+//actual typealias QuasiJsModule = kotlin.js.JsModule
+//actual typealias QuasiJsNonModule = kotlin.js.JsNonModule
+//actual typealias QuasiJsQualifier = kotlin.js.JsQualifier
