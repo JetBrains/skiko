edd315c5e9a861dcab92a11b328a955ea25cb23a
Ivan Matkov <ivan.matkov@jetbrains.com>
2025-09-10 18:41:41 +0200

    Update JDK to 21 in windows docker

--- skiko/docker/windows/install_jdk.ps1
+++ skiko/docker/windows/install_jdk.ps1
@@ -1,11 +1,11 @@
 param ($url, $targetDir)
-$archiveFile="C:\TEMP\corretto-11-jdk.zip" 
+$archiveFile="C:\TEMP\jdk.zip" 
 Write-Host ('Downloading {0} ...' -f $url)		
 (New-Object System.Net.WebClient).DownloadFile($url, $archiveFile)
 Write-Host 'Installing ...'
 tar -xf $archiveFile
-# rename an unpacked directory like 'jdk11.0.10' to 'jdk11'
-$jdkDir=Get-ChildItem -Filter "jdk11.*"|Select-Object -First 1
+# rename an unpacked directory like 'jdk-21.0.8+9' to '$targetDir'
+$jdkDir=Get-ChildItem -Filter "jdk*"|Select-Object -First 1
 Rename-Item -Path $jdkDir.FullName -NewName $targetDir
 Remove-Item $archiveFile -Force 		
 Write-Host 'Installation is complete.'
