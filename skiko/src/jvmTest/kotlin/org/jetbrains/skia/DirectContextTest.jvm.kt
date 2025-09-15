702f9a9e74c24adc07ca692444f9fe26e65e1275
Nikita Lipsky <nikita.lipsky@jetbrains.com>
2025-09-15 11:48:11 +0300

    Test added

--- skiko/src/jvmTest/kotlin/org/jetbrains/skia/DirectContextTest.jvm.kt
+++ skiko/src/jvmTest/kotlin/org/jetbrains/skia/DirectContextTest.jvm.kt
@@ -3,7 +3,9 @@ package org.jetbrains.skia
 import org.jetbrains.skia.impl.Native.Companion.NullPointer
 import org.jetbrains.skia.impl.NativePointer
 import org.jetbrains.skia.impl.interopScope
+import org.jetbrains.skiko.Library
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 fun getMakeGLAssembledInterfaceFunc(): NativePointer {
     return interopScope {
@@ -12,11 +14,29 @@ fun getMakeGLAssembledInterfaceFunc(): NativePointer {
 }
 
 class DirectContextTest {
-    @Test
-    fun makeFromInterfaceTest() {
+
+    private companion object {
+        init {
+            Library.load()
+        }
+    }
+
+    private fun makeFromInterface(): DirectContext {
         val fPtr = getMakeGLAssembledInterfaceFunc()
         val i = GLAssembledInterface.createFromNativePointers(NullPointer, fPtr)
-        DirectContext.makeGLWithInterface(i)
+        return DirectContext.makeGLWithInterface(i)
+    }
+
+    @Test
+    fun makeFromInterfaceTest() {
+        makeFromInterface()
+    }
+
+    @Test
+    fun resourceCacheLimitTest() {
+        val context = makeFromInterface()
+        context.resourceCacheLimit = 1024
+        assertEquals(1024, context.resourceCacheLimit)
     }
 }
 
