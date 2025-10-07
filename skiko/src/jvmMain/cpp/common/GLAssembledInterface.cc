c28723c9e62990cd0beebd9327c1721704cd244a
tiesiogdvd <deivukelis@gmail.com>
2025-10-07 18:49:28 +0300

    Test mac gles adjustment

--- skiko/src/jvmMain/cpp/common/GLAssembledInterface.cc
+++ skiko/src/jvmMain/cpp/common/GLAssembledInterface.cc
@@ -1,12 +1,17 @@
 #include <jni.h>
 
 #include "ganesh/gl/GrGLAssembleInterface.h"
+#include "ganesh/gl/GrGLAssembleGLESInterface.h"
 #include "ganesh/gl/GrGLInterface.h"
 
 extern "C" JNIEXPORT jlong JNICALL Java_org_jetbrains_skia_GLAssembledInterfaceKt__1nCreateFromNativePointers
   (JNIEnv* env, jclass jclass, jlong ctxPtr, jlong fPtr) {
     void* ctx = reinterpret_cast<void*>(static_cast<uintptr_t>(ctxPtr));
     GrGLGetProc f = reinterpret_cast<GrGLGetProc>(static_cast<uintptr_t>(fPtr));
-    sk_sp<const GrGLInterface> interface = GrGLMakeAssembledInterface(ctx, f);
+    #if defined(__APPLE__)
+        sk_sp<const GrGLInterface> interface = GrGLMakeAssembledGLESInterface(ctx, f);
+    #else
+        sk_sp<const GrGLInterface> interface = GrGLMakeAssembledInterface(ctx, f);
+    #endif
     return reinterpret_cast<jlong>(interface.release());
 }
