78dc52ff443d07159d561ca0d7d919c02fe8c826
ApoloApps <info@composevisualdesigner.com>
2026-02-03 23:11:32 +0100

    Fix SKIKO-1059 by transforming ColorMatrix.kt to value class and changes the required argument message on construction

--- skiko/src/commonMain/kotlin/org/jetbrains/skia/ColorMatrix.kt
+++ skiko/src/commonMain/kotlin/org/jetbrains/skia/ColorMatrix.kt
@@ -1,26 +1,14 @@
 package org.jetbrains.skia
 
-class ColorMatrix(vararg mat: Float) {
-    val mat: FloatArray
-    override fun equals(other: Any?): Boolean {
-        if (other === this) return true
-        if (other !is ColorMatrix) return false
-        return mat.contentEquals(other.mat)
-    }
+import kotlin.jvm.JvmInline
 
-    override fun hashCode(): Int {
-        val PRIME = 59
-        var result = 1
-        result = result * PRIME + mat.contentHashCode()
-        return result
+@JvmInline
+value class ColorMatrix(val mat: FloatArray) {
+    init {
+        require(mat.size == 20) { "Expected 20 elements, got ${mat.size}" }
     }
 
     override fun toString(): String {
-        return "ColorMatrix(_mat=$mat)"
-    }
-
-    init {
-        require(mat.size == 20) { "Expected 20 elements, got $mat" }
-        this.mat = mat
+        return "ColorMatrix(mat=${mat.contentToString()})"
     }
 }
