a3ba3df27892836acd4d58c2f014f80b78a5658e
ApoloApps <info@composevisualdesigner.com>
2026-02-09 15:55:56 +0100

    Maintain vararg constructor for compatibility in ColorMatrix.kt

--- skiko/src/commonMain/kotlin/org/jetbrains/skia/ColorMatrix.kt
+++ skiko/src/commonMain/kotlin/org/jetbrains/skia/ColorMatrix.kt
@@ -4,6 +4,8 @@ import kotlin.jvm.JvmInline
 
 @JvmInline
 value class ColorMatrix(val mat: FloatArray) {
+    constructor(vararg mat: Float) : this(mat)
+
     init {
         require(mat.size == 20) { "Expected 20 elements, got ${mat.size}" }
     }
