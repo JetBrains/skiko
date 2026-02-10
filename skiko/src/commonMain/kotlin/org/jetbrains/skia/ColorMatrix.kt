5ffb421a7b3122d2692e94c35c9c2a750e9314c3
ApoloApps <info@composevisualdesigner.com>
2026-02-10 09:41:12 +0100

    Fix SKIKO-1059 ColorMatrix issues (#1157)
    Fixes
    [SKIKO-1059](https://youtrack.jetbrains.com/issue/SKIKO-1059/Output-the-argument-size-in-ColorMatrix-in-case-of-error)
    Converts to value class for inlining and no class allocation and fixes
    the requirement message

--- skiko/src/commonMain/kotlin/org/jetbrains/skia/ColorMatrix.kt
+++ skiko/src/commonMain/kotlin/org/jetbrains/skia/ColorMatrix.kt
@@ -1,26 +1,28 @@
 package org.jetbrains.skia
 
-class ColorMatrix(vararg mat: Float) {
-    val mat: FloatArray
-    override fun equals(other: Any?): Boolean {
-        if (other === this) return true
-        if (other !is ColorMatrix) return false
-        return mat.contentEquals(other.mat)
-    }
+import kotlin.jvm.JvmInline
 
-    override fun hashCode(): Int {
-        val PRIME = 59
-        var result = 1
-        result = result * PRIME + mat.contentHashCode()
-        return result
+@JvmInline
+value class ColorMatrix(val mat: FloatArray) {
+    constructor(
+        m00: Float, m01: Float, m02: Float, m03: Float, m04: Float,
+        m10: Float, m11: Float, m12: Float, m13: Float, m14: Float,
+        m20: Float, m21: Float, m22: Float, m23: Float, m24: Float,
+        m30: Float, m31: Float, m32: Float, m33: Float, m34: Float
+    ) : this(
+        floatArrayOf(
+            m00, m01, m02, m03, m04,
+            m10, m11, m12, m13, m14,
+            m20, m21, m22, m23, m24,
+            m30, m31, m32, m33, m34
+        )
+    )
+
+    init {
+        require(mat.size == 20) { "Expected 20 elements, got ${mat.size}" }
     }
 
     override fun toString(): String {
-        return "ColorMatrix(_mat=$mat)"
-    }
-
-    init {
-        require(mat.size == 20) { "Expected 20 elements, got $mat" }
-        this.mat = mat
+        return "ColorMatrix(mat=${mat.contentToString()})"
     }
 }
