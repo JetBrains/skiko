89609eebf043f602445a2b75cf1cef89b34ceb0e
Ivan Matkov <ivan.matkov@jetbrains.com>
2026-02-16 11:36:46 +0100

    Remove `URIManager`, `ClipboardManager` and `CursorManager`

--- skiko/src/androidMain/kotlin/org/jetbrains/skiko/Actuals.android.kt
+++ skiko/src/androidMain/kotlin/org/jetbrains/skiko/Actuals.android.kt
@@ -1,10 +1,7 @@
 package org.jetbrains.skiko
 
 import android.content.*
-import android.content.ClipboardManager
 import android.content.res.Configuration
-import android.net.Uri
-import android.view.PointerIcon.*
 import android.view.View
 import org.jetbrains.skiko.redrawer.Redrawer
 
@@ -50,47 +47,4 @@ actual val currentSystemTheme: SystemTheme
             Configuration.UI_MODE_NIGHT_NO -> SystemTheme.LIGHT
             else -> SystemTheme.UNKNOWN
         }
-    }
-
-internal actual fun URIHandler_openUri(uri: String) {
-    defaultContext!!.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(uri)))
-}
-
-internal actual fun ClipboardManager_setText(text: String) {
-    val clipboard = defaultContext!!.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-    val clip = ClipData.newPlainText("", text)
-    clipboard.setPrimaryClip(clip)
-}
-
-internal actual fun ClipboardManager_getText(): String? {
-    val context = defaultContext!!
-    val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-    val clip = clipboard.primaryClip ?: return null
-    return clip.getItemAt(0)?.text?.toString()
-}
-
-internal actual fun ClipboardManager_hasText(): Boolean = !ClipboardManager_getText().isNullOrEmpty()
-
-actual typealias Cursor = android.view.PointerIcon
-
-// TODO: not sure if correct.
-internal actual fun CursorManager_setCursor(component: Any, cursor: Cursor) {
-    if (component is View) {
-        component.pointerIcon = cursor
-    }
-}
-
-internal actual fun CursorManager_getCursor(component: Any): Cursor? =
-    if (component is View) {
-        component.pointerIcon
-    } else {
-        null
-    }
-
-internal actual fun getCursorById(id: PredefinedCursorsId): Cursor =
-    when (id) {
-        PredefinedCursorsId.DEFAULT -> getSystemIcon(defaultContext!!, TYPE_DEFAULT)
-        PredefinedCursorsId.CROSSHAIR -> getSystemIcon(defaultContext!!, TYPE_CROSSHAIR)
-        PredefinedCursorsId.HAND -> getSystemIcon(defaultContext!!, TYPE_HAND)
-        PredefinedCursorsId.TEXT -> getSystemIcon(defaultContext!!, TYPE_TEXT)
     }
--- skiko/src/webMain/kotlin/org/jetbrains/skiko/Actuals.js.kt
+++ skiko/src/webMain/kotlin/org/jetbrains/skiko/Actuals.js.kt
@@ -7,32 +7,6 @@ internal actual inline fun <R> maybeSynchronized(lock: Any, block: () -> R): R =
 
 actual fun currentNanoTime(): Long = (window.performance.now() * 1_000_000).toLong()
 
-internal actual fun URIHandler_openUri(uri: String) {
-    window.open(uri, target = "_blank")
-}
-
-internal actual fun ClipboardManager_setText(text: String) {
-    window.navigator.clipboard.writeText(text)
-}
-
-internal actual fun ClipboardManager_getText(): String? {
-    // TODO("implement ClipboardManager_getText")
-    // kotlinx.browser.window.navigator.clipboard.readText()
-    return null
-}
-
-internal actual fun ClipboardManager_hasText(): Boolean = !ClipboardManager_getText().isNullOrEmpty()
-
-actual typealias Cursor = String
-
-internal actual fun getCursorById(id: PredefinedCursorsId): Cursor =
-    when (id) {
-        PredefinedCursorsId.DEFAULT -> "default"
-        PredefinedCursorsId.CROSSHAIR -> "crosshair"
-        PredefinedCursorsId.HAND -> "pointer"
-        PredefinedCursorsId.TEXT -> "text"
-    }
-
 internal actual fun loadOpenGLLibrary() {
    // Nothing to do here
 }
