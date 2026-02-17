26b421edae26488c2a7faba4af2fb7220c4e9b22
Ivan Matkov <ivan.matkov@jetbrains.com>
2026-02-17 22:20:30 +0100

    Remove `URIManager`, `ClipboardManager` and `CursorManager` (#1166)
    They are out of the scope of the skiko library.
    
    [SKIKO-1020](https://youtrack.jetbrains.com/issue/SKIKO-1020) Deprecate
    and remove URIManager
    [SKIKO-1021](https://youtrack.jetbrains.com/issue/SKIKO-1021) Deprecate
    and remove ClipboardManager
    [SKIKO-1022](https://youtrack.jetbrains.com/issue/SKIKO-1022) Deprecate
    and remove CursorManager

--- skiko/src/wasmJsMain/kotlin/org/jetbrains/skiko/Actuals.wasm.kt
+++ skiko/src/wasmJsMain/kotlin/org/jetbrains/skiko/Actuals.wasm.kt
@@ -1,15 +1,6 @@
 package org.jetbrains.skiko
 
-import org.w3c.dom.HTMLElement
 import org.w3c.dom.events.UIEvent
-
-internal actual fun CursorManager_setCursor(component: Any, cursor: Cursor) {
-    (component as? HTMLElement)?.style?.cursor = cursor
-}
-
-internal actual fun CursorManager_getCursor(component: Any): Cursor? {
-    return (component as? HTMLElement)?.style?.cursor
-}
 
 internal actual fun getNavigatorInfo(): String =
     js("navigator.userAgentData ? navigator.userAgentData.platform : navigator.platform")
