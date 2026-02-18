28c730b0a5a6ad0f49be85fa8737a0116e91bfa5
Ivan Matkov <ivan.matkov@jetbrains.com>
2026-02-18 10:02:26 +0100

    Use TextStyle raster settings in ClocksAwt paragraph sample

--- samples/SkiaAwtSample/src/main/kotlin/SkiaAwtSample/ClocksAwt.kt
+++ samples/SkiaAwtSample/src/main/kotlin/SkiaAwtSample/ClocksAwt.kt
@@ -91,7 +91,12 @@ open class ClocksAwt(
 
         val style = ParagraphStyle()
         val paragraph = ParagraphBuilder(style, fontCollection)
-            .pushStyle(TextStyle().setColor(0xFF000000.toInt()))
+            .pushStyle(TextStyle().apply {
+                color = 0xFF000000.toInt()
+                fontEdging = FontEdging.SUBPIXEL_ANTI_ALIAS
+                fontHinting = FontHinting.SLIGHT
+                subpixel = true
+            })
             .addText("Graphic API: ${renderProvider()}, JRE: ${System.getProperty("java.vendor")}, ${System.getProperty("java.runtime.version")} $currentSystemTheme")
             .popStyle()
             .build()
