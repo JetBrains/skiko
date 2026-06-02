f5b0a10dcda4a544a6ecdeb6548e5e9d2c92b774
ApoloApps <apolostudioapps@gmail.com>
2026-06-02 13:26:02 +0200

    Transform Matrix* to value classes (#1205)
    It transforms Matrix* classes to be value classes so that we avoid
    double allocation in these simple classes (1 fo FloatArray and 1 for the
    Matrix class itself)
    https://github.com/JetBrains/skiko/pull/1137 followup (at least partly)
    
    Fixes
    [SKIKO-1140](https://youtrack.jetbrains.com/issue/SKIKO-1140/Change-Matrix-to-value-classes)
    
    **Next**
    Follow-up PR in Compose Core to make use of the internal float array
    constructor to reuse if coming from another FLoatArray
    
    ---------
    
    Co-authored-by: Ivan Matkov <matkov.ivan@gmail.com>

--- skiko/src/commonMain/kotlin/org/jetbrains/skia/Matrix22.kt
+++ skiko/src/commonMain/kotlin/org/jetbrains/skia/Matrix22.kt
@@ -3,31 +3,32 @@ package org.jetbrains.skia
 import org.jetbrains.skia.impl.InteropPointer
 import org.jetbrains.skia.impl.InteropScope
 import org.jetbrains.skia.impl.withResult
+import kotlin.jvm.JvmInline
 
 /**
  * 2x2 matrix.
+ * @param mat 4 elements in row-major order
  */
-class Matrix22(vararg mat: Float) {
+@JvmInline
+value class Matrix22 internal constructor(val mat: FloatArray) {
+    constructor(
+        m00: Float, m01: Float,
+        m10: Float, m11: Float
+    ) : this(
+        floatArrayOf(
+            m00, m01,
+            m10, m11
+        )
+    )
+
     /**
-     * Matrix elements are in row-major order.
+     * The constructor parameters are in row-major order.
      */
-    val mat: FloatArray
-
-    override fun equals(other: Any?): Boolean {
-        if (other === this) return true
-        if (other !is Matrix22) return false
-        return mat.contentEquals(other.mat)
+    init {
+        require(mat.size == 4) { "Expected 4 elements, got ${mat.size}" }
     }
-
-    override fun hashCode(): Int {
-        val PRIME = 59
-        var result = 1
-        result = result * PRIME + mat.contentHashCode()
-        return result
-    }
-
     override fun toString(): String {
-        return "Matrix22(_mat=$mat)"
+        return "Matrix22(mat=${mat.contentToString()})"
     }
 
     companion object {
@@ -35,15 +36,7 @@ class Matrix22(vararg mat: Float) {
 
         internal fun fromInteropPointer(block: InteropScope.(InteropPointer) -> Unit): Matrix22 {
             val result = withResult(FloatArray(4), block)
-            return Matrix22(*result)
+            return Matrix22(result)
         }
-    }
-
-    /**
-     * The constructor parameters are in row-major order.
-     */
-    init {
-        require(mat.size == 4) { "Expected 4 elements, got ${mat.size}" }
-        this.mat = mat
     }
 }
