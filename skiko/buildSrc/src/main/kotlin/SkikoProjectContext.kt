180c3dc3d42ca8f7102bccbee919b21638b2951e
Hubert Błaszczyk <hubertblaszczyk03@gmail.com>
2026-06-14 17:54:45 +0200

    Use test skia version

--- skiko/buildSrc/src/main/kotlin/SkikoProjectContext.kt
+++ skiko/buildSrc/src/main/kotlin/SkikoProjectContext.kt
@@ -102,7 +102,7 @@ fun SkikoProjectContext.declareSkiaTasks() {
 
             val skiaReleaseTag = project.skiaVersion(target)
 
-            val skiaBaseUrl = "https://github.com/JetBrains/skia/releases/download/$skiaReleaseTag"
+            val skiaBaseUrl = "https://github.com/hub-bla/skia/releases/download/$skiaReleaseTag"
 
             val artifactId = "Skia-${skiaReleaseTag}-${config}-$buildType-${arch}"
 
