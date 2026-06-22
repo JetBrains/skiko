dc41632cd9d9560cc3b10ce36547a57a7c80e233
Hubert Błaszczyk <hubertblaszczyk03@gmail.com>
2026-06-22 20:15:50 +0200

    Share SkottieLibrary actual in webMain

--- skiko/skiko-skottie/src/wasmJsMain/kotlin/org/jetbrains/skia/skottie/SkottieLibrary.wasmjs.kt
+++
@@ -1,9 +0,0 @@
-package org.jetbrains.skia.skottie
-
-internal actual object SkottieLibrary {
-    actual fun load() {
-        check(isSideModuleLoaded()) {
-            "Skottie side module was not loaded"
-        }
-    }
-}
