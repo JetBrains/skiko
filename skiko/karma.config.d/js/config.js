8f944c9642a1e2559bb45a0264e92f56d7892c4f
Oleksandr Karpovich <a.n.karpovich@gmail.com>
2026-08-14 16:53:00 +0200

    Support resizing of Web SkiaLayer and CanvasRenderer (#1256)
    - Introduce resize method to the Web CanvasRenderer and SkiaLayer.web.kt
    - Avoid CanvasRenderer re-creation for the same HTMLCanvasElement in
    attachTo

--- skiko/karma.config.d/js/config.js
+++ skiko/karma.config.d/js/config.js
@@ -4,6 +4,19 @@ const path = require("path");
 
 config.browserConsoleLogOptions.level = "debug";
 
+config.customLaunchers = Object.assign(config.customLaunchers || {}, {
+    ChromeHeadlessWithWebGL: {
+        base: 'ChromeHeadless',
+        flags: [
+            '--no-sandbox',
+            '--use-gl=angle',
+            '--use-angle=swiftshader',
+            '--ignore-gpu-blocklist',
+        ]
+    }
+});
+config.browsers = ['ChromeHeadlessWithWebGL'];
+
 const basePath = config.basePath;
 const projectPath = path.resolve(basePath, "..", "..", "..", "..");
 const wasmPath = path.resolve(basePath, "kotlin")
