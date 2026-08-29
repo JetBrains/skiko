c0cb8f222465bfbc2caac9d058e95361a9a0cac9
Oleksandr.Karpovich <a.n.karpovich@gmail.com>
2026-08-29 22:30:31 +0200

    Introduce SkiaLayer.makeWithContextAttributes to pass arbitrary EmscriptenWebGLContextAttributes

--- skiko/src/jsMain/kotlin/org/jetbrains/skiko/wasm/Wrapper.kt
+++ skiko/src/jsMain/kotlin/org/jetbrains/skiko/wasm/Wrapper.kt
@@ -1,8 +1,5 @@
 package org.jetbrains.skiko.wasm
 
-import org.jetbrains.skia.impl.NativePointer
-import org.jetbrains.skiko.GL
-import org.w3c.dom.HTMLCanvasElement
 import kotlin.js.*
 
 /**
@@ -12,40 +9,4 @@ import kotlin.js.*
  */
 actual fun onWasmReady(onReady: () -> Unit) {
     awaitSkiko.then { onReady() }
-}
-
-internal fun ContextAttributes.asJsObject(): dynamic {
-    val jsObject = js("{}")
-    alpha?.let { jsObject.alpha = alpha }
-    depth?.let { jsObject.depth = depth }
-    stencil?.let { jsObject.stencil = stencil }
-    antialias?.let { jsObject.antialias = antialias }
-    premultipliedAlpha?.let { jsObject.premultipliedAlpha = premultipliedAlpha }
-    preserveDrawingBuffer?.let { jsObject.preserveDrawingBuffer = preserveDrawingBuffer }
-    preferLowPowerToHighPerformance?.let { jsObject.preferLowPowerToHighPerformance = preferLowPowerToHighPerformance }
-    failIfMajorPerformanceCaveat?.let { jsObject.failIfMajorPerformanceCaveat = failIfMajorPerformanceCaveat }
-    enableExtensionsByDefault?.let { jsObject.enableExtensionsByDefault = enableExtensionsByDefault }
-    explicitSwapControl?.let { jsObject.explicitSwapControl = explicitSwapControl }
-    renderViaOffscreenBackBuffer?.let { jsObject.renderViaOffscreenBackBuffer = renderViaOffscreenBackBuffer }
-    majorVersion?.let { jsObject.majorVersion = majorVersion }
-    return jsObject
-}
-
-internal actual fun createWebGLContext(canvas: HTMLCanvasElement, attr: ContextAttributes?): NativePointer {
-    val contextAttributes = object : ContextAttributes {
-        override val alpha = attr?.alpha ?: 1
-        override val depth = attr?.depth ?: 1
-        override val stencil = attr?.stencil ?: 8
-        override val antialias = attr?.antialias ?: 0
-        override val premultipliedAlpha = attr?.premultipliedAlpha ?: 1
-        override val preserveDrawingBuffer = attr?.preserveDrawingBuffer ?: 0
-        override val preferLowPowerToHighPerformance = attr?.preferLowPowerToHighPerformance ?: 0
-        override val failIfMajorPerformanceCaveat = attr?.failIfMajorPerformanceCaveat ?: 0
-        override val enableExtensionsByDefault = attr?.enableExtensionsByDefault ?: 1
-        override val explicitSwapControl = attr?.explicitSwapControl ?: 0
-        override val renderViaOffscreenBackBuffer = attr?.renderViaOffscreenBackBuffer ?: 0
-        override val majorVersion = attr?.majorVersion ?: 2
-    }
-    patchWebGlContext(canvas)
-    return GL.createContext(canvas, contextAttributes.asJsObject())
 }
