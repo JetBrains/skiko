d442dae16ebf11b09c3d6fea07e0e31f47dd36a1
Dustin Feucht <dustin.feucht@jetbrains.com>
2026-09-04 10:49:48 +0200

    [remove before merge] Temporarily use custom built skia
    To ensure that the CI will pass when the PR in skia is merged (#28), we use a temporary release from my own fork with the changes.
    
    Once the passing pipeline is confirmed, this commit HAS TO BE removed.

--- skiko/buildSrc/src/main/kotlin/SkikoProjectContext.kt
+++ skiko/buildSrc/src/main/kotlin/SkikoProjectContext.kt
@@ -111,7 +111,7 @@ fun SkikoProjectContext.declareSkiaTasks() {
 
             val skiaReleaseTag = project.skiaVersion(target)
 
-            val skiaBaseUrl = "https://github.com/JetBrains/skia/releases/download/$skiaReleaseTag"
+            val skiaBaseUrl = "https://github.com/nopjar/skia/releases/download/$skiaReleaseTag"
 
             val artifactId = "Skia-${skiaReleaseTag}-${config}-$buildType-${arch}"
 
