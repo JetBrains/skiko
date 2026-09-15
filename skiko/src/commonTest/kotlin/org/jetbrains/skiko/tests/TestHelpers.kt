1e453daa9db5bf32521055fcce3c5fc2d78a804c
Hubert Blaszczyk <hubert.blaszczyk@jetbrains.com>
2026-09-16 00:19:31 +0200

    Extract Ganesh surface factories

--- skiko/src/commonTest/kotlin/org/jetbrains/skiko/tests/TestHelpers.kt
+++ skiko/src/commonTest/kotlin/org/jetbrains/skiko/tests/TestHelpers.kt
@@ -1,46 +1,7 @@
 package org.jetbrains.skiko.tests
 
-import org.jetbrains.skia.*
+import org.jetbrains.skia.ExternalSymbolName
 import org.jetbrains.skia.impl.*
-import org.jetbrains.skiko.KotlinBackend
-import org.jetbrains.skiko.OS
-import org.jetbrains.skiko.hostOs
-import org.jetbrains.skiko.kotlinBackend
-
-internal class TestGlContext : Managed(TestGlContext_nCreate(), FinalizerHolder.PTR) {
-    private object FinalizerHolder {
-        val PTR = TestGlContext_nGetFinalizer()
-    }
-
-    fun makeCurrent() {
-        TestGlContext_nMakeCurrent(_ptr)
-    }
-
-    fun swapBuffers() {
-        TestGlContext_nSwapBuffers(_ptr)
-    }
-
-    companion object {
-
-        fun isAvailable(): Boolean {
-            if (hostOs != OS.Linux || kotlinBackend != KotlinBackend.Native) {
-                // TODO implement for other platforms and render targets
-                return false
-            }
-            return true
-        }
-
-        inline fun <T> run(block: TestGlContext.() -> T): T {
-           check(isAvailable()) { "TestGlContext is not available" }
-           return TestGlContext().use {
-                it.makeCurrent()
-                val result = it.block()
-                it.swapBuffers()
-               result
-            }
-        }
-    }
-}
 
 class TestHelpers {
 
@@ -121,19 +82,3 @@ private external fun _nWriteArraysOfInts(interopPointer: InteropPointer): Native
 private external fun _nStringByIndex(index: Int): NativePointer
 
 internal fun nativeStringByIndex(index: Int): NativePointer = _nStringByIndex(index)
-
-@Suppress("OPTIONAL_DECLARATION_USAGE_IN_NON_COMMON_SOURCE") // See KT-44014
-@ExternalSymbolName("org_jetbrains_skiko_tests_TestHelpers__1nCreateTestGlContext")
-private external fun TestGlContext_nCreate(): NativePointer
-
-@Suppress("OPTIONAL_DECLARATION_USAGE_IN_NON_COMMON_SOURCE") // See KT-44014
-@ExternalSymbolName("org_jetbrains_skiko_tests_TestHelpers__1nGlContextGetFinalizer")
-private external fun TestGlContext_nGetFinalizer(): NativePointer
-
-@Suppress("OPTIONAL_DECLARATION_USAGE_IN_NON_COMMON_SOURCE") // See KT-44014
-@ExternalSymbolName("org_jetbrains_skiko_tests_TestHelpers__1nMakeGlContextCurrent")
-private external fun TestGlContext_nMakeCurrent(ptr: NativePointer)
-
-@Suppress("OPTIONAL_DECLARATION_USAGE_IN_NON_COMMON_SOURCE") // See KT-44014
-@ExternalSymbolName("org_jetbrains_skiko_tests_TestHelpers__1nGlContextSwapBuffers")
-private external fun TestGlContext_nSwapBuffers(ptr: NativePointer)
