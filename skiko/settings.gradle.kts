a660dac8b10e5e86053ad078645f35df307bc17a
Hubert Blaszczyk <hubert.blaszczyk@jetbrains.com>
2026-09-18 00:36:34 +0200

    Add gpu backend switch for iOS

--- skiko/settings.gradle.kts
+++ skiko/settings.gradle.kts
@@ -21,6 +21,18 @@ include("docs")
 include("import-generator")
 include("test-utils")
 include("skiko-skottie")
+val supportAnyNativeIos = System.getProperty("idea.active")?.toBoolean() == true || listOf(
+    "skiko.native.enabled",
+    "skiko.native.ios.enabled",
+    "skiko.native.ios.arm64.enabled",
+    "skiko.native.ios.simulatorArm64.enabled",
+    "skiko.native.ios.x64.enabled",
+).any { providers.gradleProperty(it).orNull == "true" }
+if (supportAnyNativeIos) {
+    include("skiko-gpu")
+    include("skiko-ganesh-gpu-provider")
+    include("skiko-graphite-gpu-provider")
+}
 include("skiko-graphite")
 include("skiko-ganesh")
 include("skiko-rendering")
