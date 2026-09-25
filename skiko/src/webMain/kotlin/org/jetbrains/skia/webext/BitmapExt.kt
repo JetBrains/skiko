500b44e730991d0021a26d585a6d49719a60a99a
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2026-09-25 15:39:22 +0200

    Stabilize Bitmap.installPixelsFromArrayBuffer (#1311)
    The goal of this PR is to stabilise the
    Bitmap.installPixelsFromArrayBuffer
    Reasoning is - it absolutely make sense as is and its existence on web
    platform only is justified.
    
    Alongside with stabilizing it's implementation was aligned with the
    Bitmap.installPixels counterpart

--- skiko/src/webMain/kotlin/org/jetbrains/skia/webext/BitmapExt.kt
+++ skiko/src/webMain/kotlin/org/jetbrains/skia/webext/BitmapExt.kt
@@ -5,11 +5,22 @@ import org.jetbrains.skia.ExternalSymbolName
 import org.jetbrains.skia.ImageInfo
 import org.jetbrains.skia.impl.Native
 import org.jetbrains.skia.impl.NativePointer
+import org.jetbrains.skia.impl.Stats
 import org.jetbrains.skia.impl.getPtr
-import org.jetbrains.skiko.ExperimentalSkikoApi
+import org.jetbrains.skia.impl.interopScope
+import org.jetbrains.skia.impl.reachabilityBarrier
 import org.khronos.webgl.ArrayBuffer
 
-@ExperimentalSkikoApi
+/**
+ * Installs pixels into [Bitmap] by copying data directly from a JS [ArrayBuffer].
+ *
+ * This function is specific to Web targets (Kotlin/JS and Kotlin/Wasm)
+ *
+ * Converting an [ArrayBuffer] into an intermediate Kotlin [ByteArray] incurs extra memory allocations
+ * on the Kotlin managed heap and requires multiple data-copying passes across runtime boundaries.
+ *
+ * For Common / Multiplatform consider the [Bitmap.installPixels], or using [org.jetbrains.skia.Data] / [org.jetbrains.skia.Image]
+ */
 suspend fun Bitmap.installPixelsFromArrayBuffer(
     info: ImageInfo,
     pixelsArrayBuffer: ArrayBuffer,
@@ -17,16 +28,26 @@ suspend fun Bitmap.installPixelsFromArrayBuffer(
 ): Boolean {
     val pixelsPtr = copyBufferToSkiko(pixelsArrayBuffer)
     if (pixelsPtr == Native.NullPointer) return false
-    return _nInstallPixelsFromPointer(
-        _ptr,
-        info.width,
-        info.height,
-        info.colorInfo.colorType.ordinal,
-        info.colorInfo.alphaType.ordinal,
-        getPtr(info.colorInfo.colorSpace),
-        pixelsPtr,
-        rowBytes
-    )
+    return try {
+        _imageInfo = null
+        Stats.onNativeCall()
+        interopScope {
+            _nInstallPixelsFromPointer(
+                _ptr,
+                info.width,
+                info.height,
+                info.colorInfo.colorType.ordinal,
+                info.colorInfo.alphaType.ordinal,
+                getPtr(info.colorInfo.colorSpace),
+                pixelsPtr,
+                rowBytes
+            )
+
+        }
+     } finally {
+        reachabilityBarrier(this)
+        reachabilityBarrier(info.colorInfo.colorSpace)
+     }
 }
 
 @ExternalSymbolName("org_jetbrains_skia_Bitmap__1nInstallPixelsFromPointer")
